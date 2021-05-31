a8ced4ab973c92fd76d490b8addc315b9fdecb41
LeoAndLis <lis.and.leo@gmail.com>
2021-05-31 20:46:07 +0300

    Added DNDProvider and removed Error component.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {DndProvider} from 'react-dnd';
+import {HTML5Backend} from 'react-dnd-html5-backend';
 
 import './App.scss';
 
@@ -7,26 +9,26 @@ import Footer from '../Footer';
 import SignIn from '../SignIn';
 import UploadProgramBlock from '../UploadProgramBlock';
 import Header from '../Header';
-import Error from '../Error';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app"> 
-        <Header/>
-        <main className="main">
-          <Route path='/sign-in' component={SignIn} exact/>
-          <Route path='/upload-program' component={UploadProgramBlock} exact/>
-          <Route path='/uploaded-programs'
-                 render={() => {
-                   return <UploadProgramBlock showUploaded />;
-                 }}
-                 exact/>
-        </main>
-        <Error/>
-        <Footer/>
-      </div>
-    </BrowserRouter>
+    <DndProvider backend={HTML5Backend}>
+      <BrowserRouter>
+        <div className="app">
+          <Header/>
+          <main className="main">
+            <Route path='/sign-in' component={SignIn} exact/>
+            <Route path='/upload-program' component={UploadProgramBlock} exact/>
+            <Route path='/uploaded-programs'
+                   render={() => {
+                     return <UploadProgramBlock showUploaded/>;
+                   }}
+                   exact/>
+          </main>
+          <Footer/>
+        </div>
+      </BrowserRouter>
+    </DndProvider>
   );
 }
 
