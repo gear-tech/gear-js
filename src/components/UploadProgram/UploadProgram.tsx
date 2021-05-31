133641f759bbaa44f576d58e17c9d601d292df23
Viktor <simpat007@yandex.ru>
2021-06-01 01:37:21 +0300

    add ProgramDetails

--- src/components/UploadProgram/UploadProgram.tsx
+++ src/components/UploadProgram/UploadProgram.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import ProgramDetails from '../ProgramDetails';
+
 import './UploadProgram.scss';
 
 const UploadProgram = () => {
+
   return (
-    <div className="drop-block">
-      <button className="drop-block__button" type="button">Upload program</button>
-      <div className="drop-block__info">
-        Click “Upload program” to browse or
-        drag and drop your .TBD files here
+    <>
+      <div className="drop-block">
+        <button className="drop-block__button" type="button">Upload program</button>
+          <div className="drop-block__info">
+            Click “Upload program” to browse or
+            drag and drop your .TBD files here
+          </div>
       </div>
-    </div>
+      <ProgramDetails/>
+    </>
   );
 }
 
