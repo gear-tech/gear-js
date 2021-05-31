7f6aa488de5dadc1dc4b66dcdad63f6fbda616a4
LeoAndLis <lis.and.leo@gmail.com>
2021-05-31 20:46:31 +0300

    Added some more items.

--- src/components/BlocksList/BlocksListUploaded.tsx
+++ src/components/BlocksList/BlocksListUploaded.tsx
@@ -27,6 +27,27 @@ const BlocksListUploaded = () => {
           <span className="programs-list__info">Uploaded at:<span className="programs-list__info-data">05-13-2021 15:24:17</span></span>
           <button className="programs-list__refresh-btn" type="button"/>
         </li>
+        <li className="programs-list__item">
+          <span className="programs-list__number">211</span>
+          <span className="programs-list__name">awesomeprogramname.tbd</span>
+          <span className="programs-list__info">Number of calls:<span className="programs-list__info-data">4</span></span>
+          <span className="programs-list__info">Uploaded at:<span className="programs-list__info-data">05-13-2021 15:24:17</span></span>
+          <button className="programs-list__refresh-btn" type="button"/>
+        </li>
+        <li className="programs-list__item">
+          <span className="programs-list__number">210</span>
+          <span className="programs-list__name">awesomeprogramname.tbd</span>
+          <span className="programs-list__info">Number of calls:<span className="programs-list__info-data">982</span></span>
+          <span className="programs-list__info">Uploaded at:<span className="programs-list__info-data">05-13-2021 15:24:17</span></span>
+          <button className="programs-list__refresh-btn" type="button"/>
+        </li>
+        <li className="programs-list__item">
+          <span className="programs-list__number">209</span>
+          <span className="programs-list__name">awesomeprogramname.tbd</span>
+          <span className="programs-list__info">Number of calls:<span className="programs-list__info-data">12</span></span>
+          <span className="programs-list__info">Uploaded at:<span className="programs-list__info-data">05-13-2021 15:24:17</span></span>
+          <button className="programs-list__refresh-btn" type="button"/>
+        </li>
       </ul>
     </div>
   );
