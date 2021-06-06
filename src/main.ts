a2e7eb1b5ec2d816d64fa7a75853037afacbb12d
Airat Zhanshuakov <dakanca@gmail.com>
2021-06-06 22:00:53 +0600

    feat: add api prefix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { BlocksModule } from './blocks/blocks.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+  app.setGlobalPrefix('api');
 
   console.log(`App succesfully run on the ${port} 🚀`);
   await app.listen(port);
