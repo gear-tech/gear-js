3c86e2287bc0d69af05e27cbd9102884788e93c0
Airat Zhanshuakov <dakanca@gmail.com>
2021-06-07 20:57:45 +0600

    init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Gear API')
+    .setDescription('The gear io API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
 
   console.log(`App succesfully run on the ${port} 🚀`);
   await app.listen(port);
