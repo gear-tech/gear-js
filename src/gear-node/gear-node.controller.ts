2cab5b053236e5e2e158605daec92b8fcd673aab
Dmitry Osipov <dmith.osipov@gmail.com>
2021-06-21 18:26:40 +0300

    change: balance transfer endpoint

--- src/gear-node/gear-node.controller.ts
+++ src/gear-node/gear-node.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { PublicKey } from 'src/decorators/public-key.decorator';
 import { GearNodeService } from './gear-node.service';
@@ -14,8 +14,8 @@ export class GearNodeController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get('balance-transfer')
-  async hasKey(@Body() body, @PublicKey() publicKey: string) {
+  @Post('balance-transfer')
+  async balanceTransfer(@Body() body, @PublicKey() publicKey: string) {
     await this.gearService.balanceTransfer(publicKey, body.value);
     return 'ok';
   }
