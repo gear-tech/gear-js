a489a925818bf987ba6a2502c8653184a5eaf1c2
Airat Zhanshuakov <dakanca@gmail.com>
2021-09-30 23:17:43 +0600

    move api init to private zone

--- website/frontend/src/index.tsx
+++ website/frontend/src/index.tsx
@@ -1,11 +1,9 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { nodeApi } from 'api/initApi';
 import { App } from './components/App/App';
 
 async function init() {
   try {
-    await nodeApi.init();
     ReactDOM.render(<App />, document.getElementById('root'));
   } catch (err) {
     console.log(err);
