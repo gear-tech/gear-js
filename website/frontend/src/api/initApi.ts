aabbc1dfeba3ab80b5df120a5adfebaa779a2c96
Airat Zhanshuakov <dakanca@gmail.com>
2021-09-30 23:25:23 +0600

    add getting api connect address from env

--- website/frontend/src/api/initApi.ts
+++ website/frontend/src/api/initApi.ts
@@ -1,18 +1,6 @@
 import { GearApi } from '@gear-js/api';
 import { API_CONNECTION_ADDRESS } from '../consts';
 
-let gear: Promise<GearApi> | null = null;
-
-// Connecting to RPC node
-export function initApi() {
-  if (!gear) {
-    gear = GearApi.create({
-      providerAddress: API_CONNECTION_ADDRESS,
-    });
-  }
-  return gear;
-}
-
 class NodeApi {
   get api(): GearApi | null {
     // eslint-disable-next-line no-underscore-dangle
@@ -33,4 +21,4 @@ class NodeApi {
   }
 }
 
-export const nodeApi = new NodeApi();
+export const nodeApi = new NodeApi(API_CONNECTION_ADDRESS);
