8f79c3aaafe906456102248d17436ea1e31548cd
EugeneWay <relax211@gmail.com>
2021-10-12 12:32:38 +0300

    add: copyToClickbord

--- website/frontend/src/components/blocks/Wallet/Wallet.tsx
+++ website/frontend/src/components/blocks/Wallet/Wallet.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useAlert } from 'react-alert';
 import { toShortAddress } from '../../../helpers';
 
 import './Wallet.scss';
@@ -6,14 +7,29 @@ import './Wallet.scss';
 export const Wallet = () => {
   const [address, setAddress] = useState('');
 
+  const alert = useAlert();
+
   useEffect(() => {
     const publicKey: any = localStorage.getItem('public_key');
     setAddress(publicKey);
   }, [address]);
 
+
+  const copyToClipboard = () => {
+    try {
+      navigator.clipboard.writeText(address);
+      alert.success(`Copied!`);
+    } catch (err) {
+      alert.error(`Copy error`);
+    }
+  };
+  
+  /* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */
+
   return (
     <div className="user-wallet__wrapper">
-      <div className="user-wallet__address">{toShortAddress(address)}</div>
+        
+        <div className="user-wallet__address" role="button" tabIndex={0} onClick={copyToClipboard}>{toShortAddress(address)}</div>  
     </div>
   );
 };
