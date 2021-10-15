6825e6705885b24910e498b1b08e817afa0f3ee9
Airat <dakanca@gmail.com>
2021-10-15 14:24:34 +0600

    add textarea to payload form (#84)
    * add textarea to payload form
    
    * Update website/frontend/src/components/pages/Programs/children/Message/children/MessageForm/MessageForm.tsx
    
    Co-authored-by: Dmitry Osipov <dmith.osipov@gmail.com>
    
    * add meta object to input
    
    * add meta object formatting
    
    Co-authored-by: Dmitry Osipov <dmith.osipov@gmail.com>

--- website/frontend/src/components/pages/Programs/children/Message/Message.tsx
+++ website/frontend/src/components/pages/Programs/children/Message/Message.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect, VFC } from 'react';
+import React, { useCallback, useEffect, useState, VFC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useAlert } from 'react-alert';
+import { Metadata } from '@gear-js/api';
 import { RootState } from 'store/reducers';
 import { sendMessageResetAction } from 'store/actions/actions';
 import { StatusPanel } from 'components/blocks/StatusPanel/StatusPanel';
 import { MessageForm } from 'components/pages/Programs/children/Message/children/MessageForm/MessageForm';
 import { PageHeader } from 'components/blocks/PageHeader/PageHeader';
 import './Message.scss';
-import { PAGE_TYPES } from 'consts';
+import { GEAR_STORAGE_KEY, PAGE_TYPES, RPC_METHODS } from 'consts';
+import ServerRPCRequestService from '../../../../../services/ServerRPCRequestService';
 
 type Props = {
   programHash: string;
@@ -16,6 +19,9 @@ type Props = {
 
 export const Message: VFC<Props> = ({ programHash, programName, handleClose }) => {
   const dispatch = useDispatch();
+  const alert = useAlert();
+
+  const [meta, setMeta] = useState<Metadata | null>(null);
 
   const { messageSendingError } = useSelector((state: RootState) => state.programs);
 
@@ -34,10 +40,32 @@ export const Message: VFC<Props> = ({ programHash, programName, handleClose }) =
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const getMeta = useCallback(async () => {
+    const apiRequest = new ServerRPCRequestService();
+
+    const { result } = await apiRequest.getResource(
+      RPC_METHODS.GET_METADATA,
+      {
+        programId: programHash,
+      },
+      { Authorization: `Bearer ${localStorage.getItem(GEAR_STORAGE_KEY)}` }
+    );
+
+    return result.meta as Metadata;
+  }, [programHash]);
+
+  useEffect(() => {
+    if (!meta) {
+      getMeta()
+        .then((res) => setMeta(res))
+        .catch((err) => alert.error(`${err}`));
+    }
+  }, [meta, alert, getMeta]);
+
   return (
     <div className="message-form">
       <PageHeader programName={programName} handleClose={handleClose} pageType={PAGE_TYPES.MESSAGE_FORM_PAGE} />
-      <MessageForm programHash={programHash} programName={programName} />
+      {meta && <MessageForm programHash={programHash} programName={programName} meta={meta} />}
       {statusPanelText && (
         <StatusPanel
           onClose={() => {
--- website/frontend/src/components/pages/Programs/children/Message/children/MessageForm/MessageForm.tsx
+++ website/frontend/src/components/pages/Programs/children/Message/children/MessageForm/MessageForm.tsx
@@ -1,14 +1,13 @@
-import React, { VFC } from 'react';
+import React, { useEffect, useState, VFC } from 'react';
 import { useDispatch } from 'react-redux';
 import { Field, Form, Formik } from 'formik';
+import { getTypeStructure, Metadata, parseHexTypes } from '@gear-js/api';
 import clsx from 'clsx';
 import { SendMessageToProgram } from 'services/ApiService';
 import { MessageModel } from 'types/program';
 import { sendMessageStartAction } from 'store/actions/actions';
 import { fileNameHandler } from 'helpers';
 import MessageIllustration from 'assets/images/message.svg';
-import { GEAR_STORAGE_KEY, RPC_METHODS } from 'consts';
-import ServerRPCRequestService from 'services/ServerRPCRequestService';
 import { useAlert } from 'react-alert';
 import { useApi } from '../../../../../../../hooks/useApi';
 import { Schema } from './Schema';
@@ -17,40 +16,39 @@ import './MessageForm.scss';
 type Props = {
   programHash: string;
   programName: string;
+  meta: Metadata;
 };
 
-export const MessageForm: VFC<Props> = ({ programHash, programName }) => {
+export const MessageForm: VFC<Props> = ({ programHash, programName, meta }) => {
   const [api] = useApi();
   const alert = useAlert();
-
+  const parsedMeta: Metadata = JSON.parse(meta as string);
   const dispatch = useDispatch();
-
-  const initialValues = {
+  const [ready, setReady] = useState(false);
+  const [initialValues, setInitialValues] = useState({
     gasLimit: 20000,
     value: 0,
     payload: '',
     destination: programHash,
-  };
+  });
+
+  useEffect(() => {
+    const displayedTypes = parseHexTypes(parsedMeta.types);
+    const inputType = getTypeStructure(parsedMeta.input!, displayedTypes);
+    console.log(initialValues.payload !== JSON.stringify(inputType, null, 4));
+    if (Object.keys(displayedTypes).length && JSON.stringify(inputType, null, 4) !== initialValues.payload) {
+      setInitialValues({ ...initialValues, payload: JSON.stringify(inputType, null, 4) });
+    }
+    setReady(true);
+  }, [initialValues, setInitialValues, parsedMeta]);
 
   const calculateGas = async (values: any, setFieldValue: any) => {
-    const apiRequest = new ServerRPCRequestService();
-
     if (values.payload.length === 0) {
       alert.error(`Error: payload can't be empty`);
       return;
     }
 
     try {
-      const {
-        result: { meta },
-      } = await apiRequest.getResource(
-        RPC_METHODS.GET_METADATA,
-        {
-          programId: programHash,
-        },
-        { Authorization: `Bearer ${localStorage.getItem(GEAR_STORAGE_KEY)}` }
-      );
-
       const estimatedGas = await api?.program.getGasSpent(programHash, values.payload, meta.input, meta);
       alert.info(`Estimated gas ${estimatedGas}`);
       setFieldValue('gasLimit', Number(`${estimatedGas}`));
@@ -60,115 +58,121 @@ export const MessageForm: VFC<Props> = ({ programHash, programName }) => {
     }
   };
 
-  return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={Schema}
-      validateOnBlur
-      onSubmit={(values: MessageModel, { resetForm }) => {
-        SendMessageToProgram(api, values, dispatch, alert);
-        dispatch(sendMessageStartAction());
-        resetForm();
-      }}
-    >
-      {({ errors, touched, values, setFieldValue }) => (
-        <Form id="message-form">
-          <div className="message-form--wrapper">
-            <div className="message-form--col">
-              <div className="message-form--info">
-                <span>File:</span>
-                <span>{fileNameHandler(programName)}</span>
-              </div>
-              <div className="message-form--info">
-                <label htmlFor="destination" className="message-form__field">
-                  Destination:
-                </label>
-                <div className="message-form__field-wrapper">
-                  <Field
-                    id="destination"
-                    name="destination"
-                    type="text"
-                    className={clsx('', errors.destination && touched.destination && 'message-form__input-error')}
-                  />
-                  {errors.destination && touched.destination ? (
-                    <div className="message-form__error">{errors.destination}</div>
-                  ) : null}
+  if (ready) {
+    return (
+      <Formik
+        initialValues={initialValues}
+        validationSchema={Schema}
+        validateOnBlur
+        onSubmit={(values: MessageModel, { resetForm }) => {
+          SendMessageToProgram(api, values, dispatch, alert);
+          dispatch(sendMessageStartAction());
+          resetForm();
+        }}
+      >
+        {({ errors, touched, values, setFieldValue }) => (
+          <Form id="message-form">
+            <div className="message-form--wrapper">
+              <div className="message-form--col">
+                <div className="message-form--info">
+                  <span>File:</span>
+                  <span>{fileNameHandler(programName)}</span>
                 </div>
-              </div>
+                <div className="message-form--info">
+                  <label htmlFor="destination" className="message-form__field">
+                    Destination:
+                  </label>
+                  <div className="message-form__field-wrapper">
+                    <Field
+                      id="destination"
+                      name="destination"
+                      type="text"
+                      className={clsx('', errors.destination && touched.destination && 'message-form__input-error')}
+                    />
+                    {errors.destination && touched.destination ? (
+                      <div className="message-form__error">{errors.destination}</div>
+                    ) : null}
+                  </div>
+                </div>
 
-              <div className="message-form--info">
-                <label htmlFor="payload" className="message-form__field">
-                  Payload:
-                </label>
-                <div className="message-form__field-wrapper">
-                  <Field
-                    id="payload"
-                    name="payload"
-                    type="text"
-                    className={clsx('', errors.payload && touched.payload && 'message-form__input-error')}
-                    placeholder="null"
-                  />
-                  {errors.payload && touched.payload ? (
-                    <div className="message-form__error">{errors.payload}</div>
-                  ) : null}
+                <div className="message-form--info">
+                  <label htmlFor="payload" className="message-form__field">
+                    Payload:
+                  </label>
+                  <div className="message-form__field-wrapper">
+                    <Field
+                      id="payload"
+                      name="payload"
+                      as="textarea"
+                      type="text"
+                      className={clsx('', errors.payload && touched.payload && 'message-form__input-error')}
+                      placeholder="// your payload here ..."
+                      rows={15}
+                    />
+                    {errors.payload && touched.payload ? (
+                      <div className="message-form__error">{errors.payload}</div>
+                    ) : null}
+                  </div>
                 </div>
-              </div>
 
-              <div className="message-form--info">
-                <label htmlFor="gasLimit" className="message-form__field">
-                  Gas limit:
-                </label>
-                <div className="message-form__field-wrapper">
-                  <Field
-                    id="gasLimit"
-                    name="gasLimit"
-                    type="number"
-                    className={clsx('', errors.gasLimit && touched.gasLimit && 'message-form__input-error')}
-                  />
-                  {errors.gasLimit && touched.gasLimit ? (
-                    <div className="message-form__error">{errors.gasLimit}</div>
-                  ) : null}
+                <div className="message-form--info">
+                  <label htmlFor="gasLimit" className="message-form__field">
+                    Gas limit:
+                  </label>
+                  <div className="message-form__field-wrapper">
+                    <Field
+                      id="gasLimit"
+                      name="gasLimit"
+                      type="number"
+                      className={clsx('', errors.gasLimit && touched.gasLimit && 'message-form__input-error')}
+                    />
+                    {errors.gasLimit && touched.gasLimit ? (
+                      <div className="message-form__error">{errors.gasLimit}</div>
+                    ) : null}
+                  </div>
                 </div>
-              </div>
 
-              <div className="message-form--info">
-                <label htmlFor="value" className="message-form__field">
-                  Value:
-                </label>
-                <div className="message-form__field-wrapper">
-                  <Field
-                    id="value"
-                    name="value"
-                    placeholder="20000"
-                    type="number"
-                    className={clsx('', errors.value && touched.value && 'message-form__input-error')}
-                  />
-                  {errors.value && touched.value ? <div className="message-form__error">{errors.value}</div> : null}
+                <div className="message-form--info">
+                  <label htmlFor="value" className="message-form__field">
+                    Value:
+                  </label>
+                  <div className="message-form__field-wrapper">
+                    <Field
+                      id="value"
+                      name="value"
+                      placeholder="20000"
+                      type="number"
+                      className={clsx('', errors.value && touched.value && 'message-form__input-error')}
+                    />
+                    {errors.value && touched.value ? <div className="message-form__error">{errors.value}</div> : null}
+                  </div>
                 </div>
-              </div>
-              <div className="message-form--btns">
-                <>
-                  <button
-                    className="message-form__button"
-                    type="button"
-                    onClick={() => {
-                      calculateGas(values, setFieldValue);
-                    }}
-                  >
-                    Calculate Gas
-                  </button>
-                  <button className="message-form__button" type="submit">
-                    <>
-                      <img src={MessageIllustration} alt="message" />
-                      Send request
-                    </>
-                  </button>
-                </>
+                <div className="message-form--btns">
+                  <>
+                    <button
+                      className="message-form__button"
+                      type="button"
+                      onClick={() => {
+                        calculateGas(values, setFieldValue);
+                      }}
+                    >
+                      Calculate Gas
+                    </button>
+                    <button className="message-form__button" type="submit">
+                      <>
+                        <img src={MessageIllustration} alt="message" />
+                        Send request
+                      </>
+                    </button>
+                  </>
+                </div>
               </div>
             </div>
-          </div>
-        </Form>
-      )}
-    </Formik>
-  );
+          </Form>
+        )}
+      </Formik>
+    );
+  }
+
+  return null;
 };
