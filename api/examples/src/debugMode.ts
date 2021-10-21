6002fa31bb663be5e0b489bff0dfaa82a224a9a7
Dmitry Osipov <dmith.osipov@gmail.com>
2021-10-21 17:10:55 +0300

    fix: debug example

---
+++ api/examples/src/debugMode.ts
@@ -0,0 +1,23 @@
+import { DebugMode, GearApi, GearKeyring } from '@gear-js/api';
+
+async function main() {
+  const api = await GearApi.create();
+  console.log(`Connected to ${await api.chain()}`);
+  const rootKeys = GearKeyring.fromSuri('//Alice', 'Alice default');
+  const debugMode = new DebugMode(api);
+
+  debugMode.enable();
+  const isEnabled = await debugMode.signAndSend(rootKeys);
+  console.log(isEnabled);
+
+  const unsub = await debugMode.snapshots(({ data }) => {
+    data.programs.forEach(({ id, static_pages, persistent_pages, code_hash, nonce }) => {
+      console.log(id.toHuman());
+    });
+    data.message_queue.forEach(({ id, source, dest, payload, gas_limit, value, reply }) => {
+      console.log(id.toHuman());
+    });
+  });
+}
+
+main();
