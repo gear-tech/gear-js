195f8918c8ddb09e7a538f593802e9b80649eb33
Dmitry Osipov <dmith.osipov@gmail.com>
2021-10-25 13:15:23 +0300

    Read mailbox (#110)
    * add: read mailbox
    
    * bump version to 0.6.1

--- api/api/src/index.ts
+++ api/api/src/index.ts
@@ -8,5 +8,6 @@ export { GearBalance } from './Balance';
 export { transformTypes } from './utils';
 export { getWasmMetadata } from './WasmMeta';
 export { DebugMode } from './DebugMode';
+export { GearMailbox } from './Mailbox';
 export * from './interfaces';
 export * from './types';
