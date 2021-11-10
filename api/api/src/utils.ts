48725c41a8f607a4e22323372d4072e3fecf411e
Dmitry Osipov <dmith.osipov@gmail.com>
2021-11-10 17:07:14 +0300

    fix: replace namespaces in CreateType module

--- api/api/src/utils.ts
+++ api/api/src/utils.ts
@@ -1,3 +1,54 @@
+import { stringCamelCase } from '@polkadot/util';
+import { Text } from '@polkadot/types';
+
 export function transformTypes(types: object): any {
   return Object.values(types).reduce((res, types): object => ({ ...res, ...types }), {});
+}
+
+export function toCamelCase(array: string[] | Text[]) {
+  let result = stringCamelCase(array.join());
+  result = result.slice(0, 1).toUpperCase() + result.slice(1, result.length);
+  return result;
+}
+
+export function isJSON(data: any) {
+  try {
+    JSON.parse(data);
+  } catch (error) {
+    try {
+      if (JSON.stringify(data)[0] !== '{') {
+        return false;
+      }
+    } catch (error) {
+      return false;
+    }
+    return true;
+  }
+  return true;
+}
+
+export function toJSON(data: any) {
+  try {
+    return JSON.parse(data);
+  } catch (error) {
+    return data;
+  }
+}
+
+export function splitByCommas(str: string) {
+  let counter = 0;
+  let result = [];
+  let lastTypeIndex = 0;
+  try {
+    Array.from(str).forEach((char, index) => {
+      if (char === ',' && counter === 0) {
+        result.push(str.slice(lastTypeIndex, index).trim());
+        lastTypeIndex = index + 1;
+      }
+      (char === '<' || char === '(') && counter++;
+      (char === '>' || char === ')') && counter--;
+    });
+    result.push(str.slice(lastTypeIndex).trim());
+  } catch (_) {}
+  return result;
 }
