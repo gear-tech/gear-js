f5d49f4bc25cf971a1de5ee4dd1e731f86237c8f
Dmitry Osipov <dmith.osipov@gmail.com>
2021-12-13 11:22:03 +0300

    fix: change request limit size

--- website/api-gateway/src/main.ts
+++ website/api-gateway/src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import express from 'express';
 import configuration from './config/configuration';
 const logger = new Logger('Main');
 
 async function bootstrap() {
   const port = configuration().server.port;
-
   console.log(configuration().kafka);
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }));
   app.setGlobalPrefix('api');
   logger.log(`App successfully run on the ${port} 🚀`);
   await app.listen(port);
