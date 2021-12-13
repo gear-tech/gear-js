8743ce6f3b4b41fc0d9f10d4ee111528ca4777a9
Dmitry Osipov <dmith.osipov@gmail.com>
2021-12-13 16:23:47 +0300

    fix: console.log payload

--- website/data-storage/src/consumer/consumer.controller.ts
+++ website/data-storage/src/consumer/consumer.controller.ts
@@ -10,7 +10,7 @@ export class ConsumerController {
 
   @MessagePattern('events')
   async addEvent(@Payload() payload) {
-    console.log(payload);
+    console.log(payload.value);
     const chain = payload.headers.chain;
     const key = payload.key;
     const value = payload.value;
@@ -23,7 +23,7 @@ export class ConsumerController {
 
   @MessagePattern('program.data')
   async programData(@Payload() payload) {
-    console.log(payload);
+    console.log(payload.value);
     const result = await this.consumerService.programData(payload.value);
     return JSON.stringify(result);
   }
@@ -37,28 +37,28 @@ export class ConsumerController {
 
   @MessagePattern('meta.add')
   async addMeta(@Payload() payload) {
-    console.log(payload);
+    console.log(payload.value);
     const result = await this.consumerService.addMeta(payload.value);
     return JSON.stringify(result);
   }
 
   @MessagePattern('meta.get')
   async getMeta(@Payload() payload) {
-    console.log(payload);
+    console.log(payload.value);
     const result = await this.consumerService.getMeta(payload.value);
     return JSON.stringify(result);
   }
 
   @MessagePattern('message.all')
   async allMessages(@Payload() payload) {
-    console.log(payload);
+    console.log(payload.value);
     const result = await this.consumerService.allMessages(payload.value);
     return JSON.stringify(result);
   }
 
   @MessagePattern('message.add.payload')
   async savePayload(@Payload() payload) {
-    console.log(payload);
+    console.log(payload.value);
     const result = await this.consumerService.addPayload(payload.value);
     return JSON.stringify(result);
   }
