8bd0e35dd8cbda29bcf797acf25857c9d3f78083
Dmitry Osipov <dmith.osipov@gmail.com>
2021-12-16 16:51:43 +0300

    fix: events-listener deploy

--- website/events-listener/src/config.js
+++ website/events-listener/src/config.js
@@ -3,8 +3,8 @@ dotenv.config();
 
 export default {
   kafka: {
-    clientId: process.env.CLIENT_ID,
-    brokers: process.env.BROKERS.split(','),
+    clientId: process.env.KAFKA_CLIENT_ID,
+    brokers: process.env.KAFKA_BROKERS.split(','),
   },
   api: {
     provider: process.env.WS_PROVIDER,
