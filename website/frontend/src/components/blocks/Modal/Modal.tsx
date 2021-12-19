c8f6baeb4faccc31be072c13068e4090c8a772ef
codev0 <dakanca@gmail.com>
2021-12-19 22:14:15 +0600

    improve Modal component

--- website/frontend/src/components/blocks/Modal/Modal.tsx
+++ website/frontend/src/components/blocks/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 import './Modal.scss';
 import { CloseIcon } from '../../../assets/Icons';
@@ -6,23 +6,33 @@ import { useBodyScrollLock } from '../../../hooks/useBodyScrollLock';
 
 type Props = {
   open: boolean;
-  content: any;
+  content: React.ReactNode;
   title?: string;
   handleClose: () => void;
+  isModal?: boolean;
 };
 
-export const Modal = ({ open, content, handleClose, ...props }: Props) => {
+export const Modal: FC<Props> = ({ open, content, handleClose, isModal, title }) => {
   const targetRef = useBodyScrollLock<HTMLDivElement>(open);
 
   return open ? (
-    <div className="modal__wrapper" ref={targetRef}>
+    <div
+      className="modal__wrapper"
+      ref={targetRef}
+      onClick={() => {
+        if (isModal) {
+          handleClose();
+        }
+      }}
+      aria-hidden="true"
+    >
       <div className="modal__box">
         <button className="modal__close" onClick={handleClose} type="button">
           <span className="modal__close-x">
             <CloseIcon color="#ffffff" />
           </span>
         </button>
-        {props.title && <h2 className="modal__title">{props.title}</h2>}
+        {title && <h2 className="modal__title">{title}</h2>}
         <div className="modal__content">
           <div className="modal__text">{content}</div>
         </div>
@@ -32,5 +42,6 @@ export const Modal = ({ open, content, handleClose, ...props }: Props) => {
 };
 
 Modal.defaultProps = {
-  title: false,
+  title: undefined,
+  isModal: true,
 };
