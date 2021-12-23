124c60af5748433926a187d488e16690a8f0cf42
Dmitry Osipov <dmith.osipov@gmail.com>
2021-12-23 17:17:37 +0300

    update @polkadot/api to 7.0.2

--- api/api/src/interfaces/augment-api-rpc.ts
+++ api/api/src/interfaces/augment-api-rpc.ts
@@ -1,8 +1,9 @@
 import type { Observable } from '@polkadot/types/types';
 import type { U64, Bytes } from '@polkadot/types';
 import { H256 } from '@polkadot/types/interfaces';
+import { AugmentedRpc } from '@polkadot/api/types';
 
-declare module '@polkadot/rpc-core/types.jsonrpc' {
+declare module '@polkadot/rpc-core/types/jsonrpc' {
   export interface RpcInterface {
     gear: {
       getGasSpent: AugmentedRpc<(id: H256 | string, payload: string | Bytes | Uint8Array) => Observable<U64>>;
