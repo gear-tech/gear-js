589627a5895be8cb10f8990c3c2276fb1ec5b3c7
Nikita Yutanov <nikitayutanov@gmail.com>
2022-01-06 15:07:12 +0500

    Add read state (#248)
    * Add read state button display
    
    * Add state read on button click
    
    * Add read state payload form
    
    * Add routing
    
    * Add state code display
    
    * Move to pages folder
    
    * Remove form submit if state doesn't have input parameters
    
    * Add loader
    
    * Fix useEffect callback deps
    
    * Fix loader and button display for state without input parameters
    
    * Fix form width
    
    * Add alert on empty form submit
    
    * Fix output variable name
    
    * Fix brackets warn
    
    * Fix meta property check

--- website/frontend/src/components/App/App.tsx
+++ website/frontend/src/components/App/App.tsx
@@ -13,6 +13,7 @@ import { Document } from 'components/pages/Document/Document';
 import { EditorPage } from 'features/Editor/EditorPage';
 import { NotificationsPage } from 'components/pages/Notifications/NotificationsPage';
 import { SimpleLoader } from 'components/blocks/SimpleLoader';
+import State from 'components/pages/State/State';
 
 import { routes } from 'routes';
 import { RootState } from 'store/reducers';
@@ -105,6 +106,9 @@ const AppComponent: FC = () => {
                 </Route>
                 <Route exact path={routes.program}>
                   <Program />
+                </Route>
+                <Route exact path={routes.state}>
+                  <State />
                 </Route>
                 <Route exact path={routes.editor}>
                   <EditorPage />
