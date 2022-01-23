fc4efec511daefd45a0ffb7e659499da61ff39c0
Nikita Yutanov <nikitayutanov@gmail.com>
2022-01-23 05:21:27 +0500

    Add last block and total issuance counters

--- website/frontend/src/components/pages/Explorer/Explorer.tsx
+++ website/frontend/src/components/pages/Explorer/Explorer.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { EventsList } from './EventsList/EventsList';
+import { ProgramSwitch } from 'components/blocks/ProgramSwitch/ProgramSwitch';
+import { SWITCH_PAGE_TYPES } from 'consts';
 import styles from './Explorer.module.scss';
 
 const Explorer = () => {
   return (
     <div className={styles.explorer}>
+      <ProgramSwitch pageType={SWITCH_PAGE_TYPES.EXPLORER} />
       <EventsList />
     </div>
   );
