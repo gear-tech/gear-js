f18ac2f70cbd9453c4e364cc099a89aae3044114
Dmitry Osipov <dmith.osipov@gmail.com>
2022-01-24 21:04:19 +0300

    remove transaction interface file

--- api/src/interfaces/transaction.ts
+++
@@ -1,8 +0,0 @@
-import { AddressOrPair, SignerOptions } from '@polkadot/api/types';
-import { Event } from '@polkadot/types/interfaces';
-import { ISubmittableResult } from '@polkadot/types/types';
-// export interface TransactionStatusCb {
-//   (event: Event): void | Promise<void>;
-// }
-
-export type TransactionStatusCb = (result: ISubmittableResult, extra: undefined) => void | Promise<void>;
