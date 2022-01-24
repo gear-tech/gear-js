59d8736d67e9fcd9e1ab336b2aad47773a27f0bf
Dmitry Osipov <dmith.osipov@gmail.com>
2022-01-24 17:05:54 +0300

    fix: transaction signAndSend will not return modified event data

--- api/src/interfaces/transaction.ts
+++ api/src/interfaces/transaction.ts
@@ -1,5 +1,8 @@
 import { AddressOrPair, SignerOptions } from '@polkadot/api/types';
+import { Event } from '@polkadot/types/interfaces';
+import { ISubmittableResult } from '@polkadot/types/types';
+// export interface TransactionStatusCb {
+//   (event: Event): void | Promise<void>;
+// }
 
-export interface TransactionStatusCb {
-  (data: any): void | Promise<void>;
-}
+export type TransactionStatusCb = (result: ISubmittableResult, extra: undefined) => void | Promise<void>;
--- api/src/types/Transaction.ts
+++ api/src/types/Transaction.ts
@@ -1,85 +1,50 @@
-import { AddressOrPair, SignerOptions, SubmittableExtrinsic } from '@polkadot/api/types';
 import { CreateType } from '../CreateType';
 import { GearApi } from '../GearApi';
 import { TransactionStatusCb } from '../interfaces';
-import { TransactionError } from '../errors';
-import { MessageInfoData } from './EventData';
 import { isFunction } from '@polkadot/util';
+import { AddressOrPair, SignerOptions, SubmittableExtrinsic } from '@polkadot/api/types';
+import { ISubmittableResult } from '@polkadot/types/types';
+import { Hash } from '@polkadot/types/interfaces';
+import { TransactionError } from '../errors';
 
 export class GearTransaction {
   protected api: GearApi;
   protected createType: CreateType;
-  protected method: string;
-  submitted: SubmittableExtrinsic<'promise'>;
+  submitted: SubmittableExtrinsic<'promise', ISubmittableResult>;
 
-  constructor(gearApi: GearApi, method: string) {
+  constructor(gearApi: GearApi) {
     this.api = gearApi;
     this.createType = new CreateType(gearApi);
-    this.method = method;
   }
 
-  signAndSend(account: AddressOrPair, callback: TransactionStatusCb): Promise<0>;
+  signAndSend(account: AddressOrPair, callback: TransactionStatusCb): Promise<() => void>;
 
-  signAndSend(account: AddressOrPair, options: Partial<SignerOptions>): Promise<0>;
+  signAndSend(account: AddressOrPair, options?: Partial<SignerOptions>): Promise<Hash>;
 
-  signAndSend(account: AddressOrPair, options: Partial<SignerOptions>, callback: TransactionStatusCb): Promise<0>;
+  signAndSend(
+    account: AddressOrPair,
+    options: Partial<SignerOptions>,
+    callback: TransactionStatusCb,
+  ): Promise<() => void>;
 
-  public signAndSend(
+  public async signAndSend(
     account: AddressOrPair,
     optionsOrCallback?: Partial<SignerOptions> | TransactionStatusCb,
-    optionalCallback?: (data: any) => void,
-  ): Promise<0> {
+    optionalCallback?: TransactionStatusCb,
+  ): Promise<Hash | (() => void)> {
     const [options, callback] = isFunction(optionsOrCallback)
       ? [undefined, optionsOrCallback]
       : [optionsOrCallback, optionalCallback];
 
-    return new Promise(async (resolve, reject) => {
-      try {
-        let blockHash: string;
-        await this.submitted.signAndSend(account, options, ({ events = [], status }) => {
-          if (status.isInBlock) {
-            blockHash = status.asInBlock.toHex();
-          } else if (status.isFinalized) {
-            blockHash = status.asFinalized.toHex();
-            resolve(0);
-          } else if (status.isInvalid) {
-            reject(new TransactionError(`Transaction error. Status: isInvalid`));
-          }
-
-          // Check transaction errors
-          events
-            .filter(({ event }) => this.api.events.system.ExtrinsicFailed.is(event))
-            .forEach(
-              ({
-                event: {
-                  data: [error],
-                },
-              }) => {
-                reject(new TransactionError(`${error.toString()}`));
-              },
-            );
-
-          events.forEach(({ event: { data, method } }) => {
-            if (method === this.method) {
-              const eventData = new MessageInfoData(data);
-              callback({
-                method,
-                status: status.type,
-                blockHash,
-                programId: eventData.programId.toHex(),
-                messageId: eventData.messageId.toHex(),
-              });
-            }
-          });
-        });
-      } catch (error) {
-        const errorCode = +error.message.split(':')[0];
-        if (errorCode === 1010) {
-          reject(new TransactionError('Account balance too low'));
-        } else {
-          reject(new TransactionError(error.message));
-        }
+    try {
+      return await this.submitted.signAndSend(account, options, callback);
+    } catch (error) {
+      const errorCode = +error.message.split(':')[0];
+      if (errorCode === 1010) {
+        throw new TransactionError('Account balance too low');
+      } else {
+        throw new TransactionError(error.message);
       }
-    });
+    }
   }
 }
