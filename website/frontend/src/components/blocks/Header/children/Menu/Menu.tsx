9b5eeab5e38fadafc029c533ce699681ebad09a5
Nikita Yutanov <nikitayutanov@gmail.com>
2022-01-25 21:47:19 +0500

    Fix header links hover (#293)

--- website/frontend/src/components/blocks/Header/children/Menu/Menu.tsx
+++ website/frontend/src/components/blocks/Header/children/Menu/Menu.tsx
@@ -14,14 +14,20 @@ const Menu = ({ openSidebar }: Props) => {
 
   return (
     <ul className={styles.menu}>
-      <li className={styles.item} onClick={openSidebar}>
-        {isApiReady ? localStorage.chain : 'Loading...'}
+      <li>
+        <span className={styles.link} onClick={openSidebar}>
+          {isApiReady ? localStorage.chain : 'Loading...'}
+        </span>
       </li>
-      <li className={styles.item}>
-        <Link to={routes.explorer}>Explorer</Link>
+      <li>
+        <Link to={routes.explorer} className={styles.link}>
+          Explorer
+        </Link>
       </li>
-      <li className={styles.item}>
-        <Link to={routes.editor}>&lt;/&gt; IDE</Link>
+      <li>
+        <Link to={routes.editor} className={styles.link}>
+          &lt;/&gt; IDE
+        </Link>
       </li>
     </ul>
   );
