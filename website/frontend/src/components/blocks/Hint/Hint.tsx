0146c31fea8feaff5ae7735edb706b5f2170ccd7
aderrod94 <igor.berg094@gmail.com>
2022-01-28 13:53:57 +0400

    change to react alert

--- website/frontend/src/components/blocks/Hint/Hint.tsx
+++ website/frontend/src/components/blocks/Hint/Hint.tsx
@@ -1,5 +1,8 @@
-import React, { FC, useRef, useEffect, useState } from 'react';
+import React, { FC } from 'react';
+import { useDispatch } from 'react-redux';
 import { HelpCircle } from 'react-feather';
+import { AddAlert } from 'store/actions/actions';
+import { EventTypes } from 'types/events';
 import styles from './Hint.module.scss';
 
 type Params = {
@@ -7,29 +10,15 @@ type Params = {
 };
 
 export const Hint: FC<Params> = ({ children }) => {
-  const timerId = useRef<any>(null);
-  const [isShow, setIsShow] = useState<Boolean>(false);
-
-  useEffect(() => {
-    if (isShow) {
-      timerId.current = setTimeout(() => {
-        setIsShow(false);
-      }, 3000);
-    }
-  }, [isShow]);
+  const dispatch = useDispatch();
 
   const handleClick = () => {
-    if (isShow) {
-      clearInterval(timerId.current);
-    }
-
-    setIsShow(!isShow);
+    dispatch(AddAlert({ type: EventTypes.ERROR, message: `${children}` }));
   };
 
   return (
     <div className={styles.hint}>
       <HelpCircle className={styles.icon} size="16" onClick={handleClick} />
-      {isShow && <p className={styles.text}>{children}</p>}
     </div>
   );
 };
