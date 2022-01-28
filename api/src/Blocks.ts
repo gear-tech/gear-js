c327f0b7eb1e75c7b466643e60136482068b74dc
Dmitry Osipov <dmith.osipov@gmail.com>
2022-01-28 14:40:53 +0300

    add error for discarded blocks

--- api/src/Blocks.ts
+++ api/src/Blocks.ts
@@ -1,8 +1,9 @@
 import { GearApi } from './GearApi';
-import { AnyTuple } from '@polkadot/types/types';
+import { AnyTuple, AnyNumber } from '@polkadot/types/types';
 import { GenericExtrinsic, Vec } from '@polkadot/types';
-import { SignedBlock } from '@polkadot/types/interfaces';
+import { SignedBlock, BlockNumber, BlockHash } from '@polkadot/types/interfaces';
 import { FrameSystemEventRecord } from '@polkadot/types/lookup';
+import { GetBlockError } from './errors/blocks.errors';
 
 export class GearBlock {
   protected api: GearApi;
@@ -10,13 +11,31 @@ export class GearBlock {
     this.api = api;
   }
 
+  /**
+   * Get
+   * @param hash
+   * @returns
+   */
   async get(hash: `0x${string}` | Uint8Array): Promise<SignedBlock> {
-    return await this.api.rpc.chain.getBlock(hash);
+    try {
+      return await this.api.rpc.chain.getBlock(hash);
+    } catch (error) {
+      throw new GetBlockError(error.message, hash);
+    }
   }
 
   /**
-   *
-   * @param blockHash hash of particular block in which the events are located
+   * Get blockHash by number
+   * @param number number of block
+   * @returns blockHash
+   */
+  async getBlockHash(number: AnyNumber | BlockNumber): Promise<BlockHash> {
+    return await this.api.rpc.chain.getBlockHash(number);
+  }
+
+  /**
+   * Get all extrinsic of particular block
+   * @param blockHash hash of particular block
    * @returns Vec of extrinsics
    */
   async getExtrinsics(blockHash: `0x${string}` | Uint8Array): Promise<Vec<GenericExtrinsic<AnyTuple>>> {
@@ -24,8 +43,8 @@ export class GearBlock {
   }
 
   /**
-   *
-   * @param blockHash hash of particular block in which the events are located
+   * Get all events of particular block
+   * @param blockHash hash of particular block
    * @returns Vec of events
    */
   async getEvents(blockHash: `0x${string}` | Uint8Array): Promise<Vec<FrameSystemEventRecord>> {
