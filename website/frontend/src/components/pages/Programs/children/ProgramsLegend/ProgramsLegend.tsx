c87bdeff7816df8dd91f8c059851ab5e534ce212
aderrod94 <igor.berg094@gmail.com>
2022-01-28 13:16:23 +0400

    fix

--- website/frontend/src/components/pages/Programs/children/ProgramsLegend/ProgramsLegend.tsx
+++ website/frontend/src/components/pages/Programs/children/ProgramsLegend/ProgramsLegend.tsx
@@ -1,12 +1,11 @@
 import React, { FC } from 'react';
-import clsx from 'clsx';
 import codeIcon from 'assets/images/code_icon.svg';
 import timestampIcon from 'assets/images/timestamp_icon.svg';
 import menuIcon from 'assets/images/menu_icon.svg';
 import styles from './ProgramsLegend.module.scss';
 
 export const ProgramsLegend: FC = () => (
-  <div className={clsx(styles.legend)}>
+  <div className={styles.legend}>
     <div className={styles.legendItem}>
       <img className={styles.legendIcon} src={codeIcon} alt="program name" />
       <p className={styles.legendCaption}>Program name</p>
