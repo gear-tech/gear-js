c123409b363253f94fcff429d19bd3265b39551e
Nikita Yutanov <nikitayutanov@gmail.com>
2022-02-03 17:04:52 +0500

    Add search input

--- website/frontend/src/components/pages/Explorer/Explorer.tsx
+++ website/frontend/src/components/pages/Explorer/Explorer.tsx
@@ -3,6 +3,7 @@ import { EventsList } from './EventsList/EventsList';
 import { EventsProps } from 'types/events-list';
 import { BackButton } from 'common/components/BackButton/BackButton';
 import { BlocksSummary } from 'components/BlocksSummary/BlocksSummary';
+import { Search } from './Search/Search';
 import styles from './Explorer.module.scss';
 
 const Explorer = ({ events }: EventsProps) => {
@@ -10,6 +11,7 @@ const Explorer = ({ events }: EventsProps) => {
     <div className={styles.explorer}>
       <header className={styles.header}>
         <BackButton />
+        <Search />
         <BlocksSummary />
       </header>
       <EventsList events={events} />
---
+++ website/frontend/src/components/pages/Explorer/Search/Search.tsx
@@ -0,0 +1,42 @@
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
+import styles from './Search.module.scss';
+
+const Search = () => {
+  const history = useHistory();
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(value);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const trimmedSearchQuery = searchQuery.trim();
+
+    if (trimmedSearchQuery) {
+      const href = `/explorer/${trimmedSearchQuery}`;
+      history.push(href);
+      setSearchQuery('');
+    }
+  };
+
+  return (
+    <form className={styles.search} onSubmit={handleSubmit}>
+      <div className={styles.wrapper}>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Search block hash or number to query"
+          value={searchQuery}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit" className={styles.button}>
+        Search
+      </button>
+    </form>
+  );
+};
+
+export { Search };
