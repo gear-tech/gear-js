bfa278d8f36c4da813c0ee92037c2aa99ff4f4ce
Dmitry Osipov <dmith.osipov@gmail.com>
2022-02-07 17:26:42 +0300

    return programId and salt of submitted program

--- api/src/Program.ts
+++ api/src/Program.ts
@@ -16,21 +16,21 @@ export class GearProgram extends GearTransaction {
   submit(
     program: {
       code: Buffer;
-      salt?: string;
+      salt?: `0x${string}`;
       initPayload?: string | any;
       gasLimit: u64 | AnyNumber;
       value?: BalanceOf | AnyNumber;
     },
     meta?: Metadata,
     messageType?: string,
-  ): ProgramId {
+  ): { programId: ProgramId; salt: `0x${string}` } {
     const salt = program.salt || randomAsHex(20);
     const code = this.createType.create('bytes', Array.from(program.code)) as Bytes;
     let payload: string = createPayload(this.createType, messageType || meta?.init_input, program.initPayload, meta);
     try {
       this.submitted = this.api.tx.gear.submitProgram(code, salt, payload, program.gasLimit, program.value || 0);
       const programId = this.generateProgramId(code, salt);
-      return programId.toHex();
+      return { programId: programId.toHex(), salt };
     } catch (error) {
       throw new SubmitProgramError();
     }
