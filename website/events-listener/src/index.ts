06d6167d9638e46ade3220bd6f79d2f00d9e6c02
Denis Makarenko <xhjkl@users.noreply.github.com>
2022-02-08 17:41:19 +0300

    event listener: restart if needed

--- website/events-listener/src/index.ts
+++ website/events-listener/src/index.ts
@@ -4,21 +4,45 @@ import { listen } from './events';
 import { KafkaProducer } from './producer';
 import { logger } from './logger';
 import config from './config';
+import { restartIfNeeded, setRestartNeeded } from './lifecycle';
 
 const log = logger('Main');
 
+let shouldRun = true;
+
+process.on('SIGTERM', () => {
+  shouldRun = false;
+});
+
+process.on('SIGINT', () => {
+  shouldRun = false;
+});
+
 const main = async () => {
-  log.info('App is running...');
-  const api = await GearApi.create({ providerAddress: config.api.provider, throwOnConnect: true });
-  const chain = await api.chain();
-  const genesis = api.genesisHash.toHex();
-  log.info(`Connected to ${chain} with genesis ${genesis}`);
-  const producer = new KafkaProducer();
-  await producer.createTopic('events');
-  await producer.connect();
-  listen(api, genesis, ({ key, value }) => {
-    producer.send(key, value, genesis);
-  });
+  while (shouldRun) {
+    log.info('Starting...');
+    const api = await GearApi.create({ providerAddress: config.api.provider, throwOnConnect: true });
+    api.on('error', () => {
+      setRestartNeeded();
+    });
+
+    const chain = await api.chain();
+    const genesis = api.genesisHash.toHex();
+
+    log.info(`Connected to ${chain} with genesis ${genesis}`);
+
+    const producer = new KafkaProducer();
+    await producer.createTopic('events');
+    await producer.connect();
+
+    listen(api, genesis, ({ key, value }) => {
+      producer.send(key, value, genesis);
+    });
+
+    log.info('Started.');
+
+    await restartIfNeeded;
+  }
 };
 
 main().catch((error) => {
