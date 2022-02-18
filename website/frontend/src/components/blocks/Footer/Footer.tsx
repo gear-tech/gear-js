b220569bc90481015a87fca34ae1231f4e97e541
Nikita Yutanov <nikitayutanov@gmail.com>
2022-02-18 12:41:48 +0300

    Refactor styles

--- website/frontend/src/components/blocks/Footer/Footer.tsx
+++ website/frontend/src/components/blocks/Footer/Footer.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import clsx from 'clsx';
-import { routes } from 'routes';
 import styles from './Footer.module.scss';
 
 const Footer = () => {
-  const { pathname } = useLocation();
-  const isLight = pathname !== routes.main || pathname !== routes.uploadedPrograms;
-  const className = clsx(styles.footer, isLight && styles.light);
-
-  return (
-    <footer className={className}>
-      <span className={styles.content}>2022. All rights reserved.</span>
-    </footer>
-  );
+  return <footer className={styles.footer}>2022. All rights reserved.</footer>;
 };
 
 export { Footer };
