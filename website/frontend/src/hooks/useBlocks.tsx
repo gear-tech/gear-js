83da0ecee89ec5261b1d1a571082ef83e701a478
Nikita Yutanov <nikitayutanov@gmail.com>
2022-02-22 21:41:29 +0300

    Fix block timestamp and block page's events border

--- website/frontend/src/hooks/useBlocks.tsx
+++ website/frontend/src/hooks/useBlocks.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@polkadot/types/interfaces';
+import { IU8a } from '@polkadot/types/types';
 import { useDispatch } from 'react-redux';
 import { fetchBlockAction } from 'store/actions/actions';
 import { useApi } from './useApi';
@@ -8,8 +9,21 @@ export function useBlocks() {
   const [api] = useApi();
   const dispatch = useDispatch();
 
-  const setBlock = ({ hash, number }: Header) => {
-    dispatch(fetchBlockAction({ hash: hash.toHex(), number: number.toNumber() }));
+  const getTime = async (hash: IU8a) => {
+    const timestamp = await api.blocks.getBlockTimestamp(hash);
+    const date = new Date(timestamp.toNumber());
+
+    return date.toLocaleTimeString();
+  };
+
+  const setBlock = async ({ hash, number }: Header) => {
+    dispatch(
+      fetchBlockAction({
+        hash: hash.toHex(),
+        number: number.toNumber(),
+        time: await getTime(hash),
+      })
+    );
   };
 
   const subscribeToBlocks = () => api.gearEvents.subscribeToNewBlocks(setBlock);
