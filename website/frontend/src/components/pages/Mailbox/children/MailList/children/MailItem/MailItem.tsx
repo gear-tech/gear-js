61ea179272e40a8f0e6584e638fa0d1cd9e9813e
aderrod94 <igor.berg094@gmail.com>
2022-02-23 11:24:27 +0300

    add claim logic

--- website/frontend/src/components/pages/Mailbox/children/MailList/children/MailItem/MailItem.tsx
+++ website/frontend/src/components/pages/Mailbox/children/MailList/children/MailItem/MailItem.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { web3FromSource } from '@polkadot/extension-dapp';
 import { Link } from 'react-router-dom';
 import { useApi } from 'hooks/useApi';
+import { RootState } from 'store/reducers';
 import MessageIcon from 'assets/images/message.svg';
 import ClaimIcon from 'assets/images/claim.svg';
+import { AddAlert } from 'store/actions/actions';
 import { getPreformattedText } from 'helpers';
+import { EventTypes } from 'types/events';
 import styles from './MailItem.module.scss';
 
 type Props = {
@@ -12,11 +17,24 @@ type Props = {
 
 export const MailItem: FC<Props> = ({ elem }) => {
   const [api] = useApi();
+  const currentAccount = useSelector((state: RootState) => state.account.account);
+  const dispatch = useDispatch();
 
   const handleClaim = async () => {
-    const a = await api.claimValueFromMailbox.submit(elem.id);
-    const b = a.toHuman();
-    console.log(b);
+    if (currentAccount) {
+      try {
+        const injector = await web3FromSource(currentAccount.meta.source);
+
+        await api.claimValueFromMailbox.submit(elem.id);
+        await api.claimValueFromMailbox.signAndSend(currentAccount.address, { signer: injector.signer }, (data) => {
+          dispatch(AddAlert({ type: EventTypes.SUCCESS, message: `Status: ${data.status}` }));
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      dispatch(AddAlert({ type: EventTypes.ERROR, message: `Wallet not connected` }));
+    }
   };
 
   return (
