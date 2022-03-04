7900c05a3f7effba6faa223946f24e2eea9eabbc
Nikita Yutanov <nikitayutanov@gmail.com>
2022-03-04 16:34:59 +0300

    Add logo tests

--- website/frontend/src/components/blocks/Header/children/Logo/Logo.tsx
+++ website/frontend/src/components/blocks/Header/children/Logo/Logo.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { LogoIcon } from 'assets/Icons';
 import { Link } from 'react-router-dom';
 import { routes } from 'routes';
+import { ReactComponent as SVG } from './images/logo.svg';
 
 const Logo = () => (
   <Link to={routes.main} className="img-wrapper">
-    <LogoIcon color="#fff" />
+    <SVG data-testid="svg" />
   </Link>
 );
 
