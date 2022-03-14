e16648f8ee6fd682a1e9b05fcad267edd23719f3
aderrod94 <igor.berg094@gmail.com>
2022-03-14 15:37:07 +0400

    refactoring component

---
+++ website/frontend/src/components/pages/Mailbox/children/MailItem/MailItem.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useApi } from 'hooks/useApi';
+import { RootState } from 'store/reducers';
+import MessageIcon from 'assets/images/message.svg';
+import ClaimIcon from 'assets/images/claim.svg';
+import { AddAlert } from 'store/actions/actions';
+import { getPreformattedText } from 'helpers';
+import { claimValue } from 'components/pages/Mailbox/helpers';
+import { EventTypes } from 'types/alerts';
+import { Mail } from '../../types';
+import styles from './MailItem.module.scss';
+
+type Props = {
+  elem: Mail;
+};
+
+export const MailItem: FC<Props> = ({ elem }) => {
+  const [api] = useApi();
+  const dispatch = useDispatch();
+  const currentAccount = useSelector((state: RootState) => state.account.account);
+
+  const handleClaim = async () => {
+    if (currentAccount) {
+      claimValue(api, currentAccount, elem, dispatch);
+    } else {
+      dispatch(AddAlert({ type: EventTypes.ERROR, message: `Wallet not connected` }));
+    }
+  };
+
+  return (
+    <div className={styles.mailItem}>
+      <pre className={styles.body}>{getPreformattedText(elem)}</pre>
+      <div className={styles.buttons}>
+        <Link to={`/reply-message/${elem.id}`} className={styles.button}>
+          <img className={styles.image} src={MessageIcon} alt="send reply icon" />
+          <span className={styles.buttonText}>Send reply</span>
+        </Link>
+        <button className={styles.button} onClick={handleClaim}>
+          <img className={styles.image} src={ClaimIcon} alt="send reply icon" />
+          <span className={styles.buttonText}>Claim value</span>
+        </button>
+      </div>
+    </div>
+  );
+};
