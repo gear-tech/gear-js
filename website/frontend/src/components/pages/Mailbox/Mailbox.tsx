27720e4689ea0f23e66ccf615f00cacdc79a87d6
aderrod94 <igor.berg094@gmail.com>
2022-03-14 12:30:12 +0400

    update component

--- website/frontend/src/components/pages/Mailbox/Mailbox.tsx
+++ website/frontend/src/components/pages/Mailbox/Mailbox.tsx
@@ -1,16 +1,38 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { useApi } from 'hooks/useApi';
+import { Hex } from '@gear-js/api';
 import { Box } from 'layout/Box/Box';
-import { MailBoxContextProvider } from './context/context';
-import { AccountId } from './children/AccountId/AccountId';
-import { MailList } from './children/MailList/MailList';
+import { MailItem } from './children/MailItem/MailItem';
+import { Mail } from './types';
+import { getMails } from './helpers';
+import { LOCAL_STORAGE } from 'consts';
+import styles from './Mailbox.module.scss';
 
 export const Mailbox: FC = () => {
+  const [api] = useApi();
+  const [mails, setMails] = useState<Mail[]>([]);
+
+  useEffect(() => {
+    const publicKey = localStorage.getItem(LOCAL_STORAGE.PUBLIC_KEY_RAW) as Hex;
+
+    getMails(api, publicKey, setMails);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <MailBoxContextProvider>
-      <Box>
-        <AccountId />
-        <MailList />
-      </Box>
-    </MailBoxContextProvider>
+    <Box>
+      <div className={styles.item}>
+        <h2 className={styles.heading}>Mailbox:</h2>
+        {mails && mails.length ? (
+          <div className={styles.list}>
+            {mails.map((elem: Mail) => {
+              return <MailItem key={elem.id} elem={elem} />;
+            })}
+          </div>
+        ) : (
+          <p className={styles.empty}>No messages</p>
+        )}
+      </div>
+    </Box>
   );
 };
--- website/frontend/src/components/pages/Mailbox/types.ts
+++ website/frontend/src/components/pages/Mailbox/types.ts
@@ -1,12 +1,10 @@
+import { Hex } from '@gear-js/api';
+
 export type Mail = {
-  destination: string;
-  id: string;
+  dest: Hex;
+  id: Hex;
   payload: string | object;
   reply: [];
   source: string;
   value: string;
-};
-
-export type Mails = {
-  mails: Mail[];
 };
