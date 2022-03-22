6e508964fc3718d65b9560e79d9525f9ac28a16f
Nikita Yutanov <nikitayutanov@gmail.com>
2022-03-22 21:33:04 +0300

    Install and config rollup-plugin-postcss

--- utils/gear-ui/rollup.config.js
+++ utils/gear-ui/rollup.config.js
@@ -2,8 +2,9 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
 
-const packageJson = require('package.json');
+const packageJson = require('./package.json');
 
 export default [
   {
@@ -20,11 +21,12 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: 'tsconfig.json' })],
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss({ modules: true })],
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
+    external: [/\.(css|scss)$/],
   },
 ];
---
+++ utils/gear-ui/src/global.d.ts
@@ -0,0 +1,4 @@
+declare module '*.module.scss' {
+  const styles: { [key: string]: string };
+  export default styles;
+}
