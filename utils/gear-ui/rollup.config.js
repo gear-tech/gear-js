e8273c62ea577b37b7be1991cc4c52bed20cb483
Nikita Yutanov <nikitayutanov@gmail.com>
2022-03-25 13:48:13 +0300

    Drop npmignore and source maps

--- utils/gear-ui/rollup.config.js
+++ utils/gear-ui/rollup.config.js
@@ -13,16 +13,8 @@ export default [
   {
     input: 'src/index.ts',
     output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
+      { file: packageJson.main, format: 'cjs' },
+      { file: packageJson.module, format: 'esm' },
     ],
     plugins: [
       resolve(),
