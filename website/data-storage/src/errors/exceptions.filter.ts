f1c1e9e6c04c43ef7504e1d64f4b0b165aa5dac5
Dmitry Osipov <dmith.osipov@gmail.com>
2022-03-30 14:11:19 +0300

    add logs data-storage requests

--- website/data-storage/src/errors/exceptions.filter.ts
+++ website/data-storage/src/errors/exceptions.filter.ts
@@ -2,6 +2,7 @@ export function CatchGearErrros(target: Object, propertyKey: string, descriptor:
   const originalMethod = descriptor.value;
   descriptor.value = async function SafeWrapper() {
     try {
+      console.log(arguments);
       return { result: await originalMethod.apply(this, arguments) };
     } catch (ex) {
       console.log(ex);
