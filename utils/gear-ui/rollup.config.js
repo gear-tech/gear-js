008bbd2da4563cc815a7de4b9c3e89f27b03c88c
Nikita Yutanov <nikitayutanov@gmail.com>
2022-04-06 01:05:47 +0300

    Fix wrong peer deps and add image build

--- utils/gear-ui/rollup.config.js
+++ utils/gear-ui/rollup.config.js
@@ -5,6 +5,7 @@ import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import image from '@rollup/plugin-image';
 
 const packageJson = require('./package.json');
 
@@ -16,18 +17,18 @@ export default [
       { file: packageJson.module, format: 'esm' },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
       postcss(),
+      image(),
       terser(),
-      peerDepsExternal(),
     ],
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
-    external: [/\.(css|scss)$/],
   },
 ];
