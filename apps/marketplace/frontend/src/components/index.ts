dbe0150a89518a0412f99b025b1c8f00f754b613
Nikita Yutanov <nikitayutanov@gmail.com>
2022-04-12 14:53:36 +0300

    Add filter component

--- apps/marketplace/frontend/src/components/index.ts
+++ apps/marketplace/frontend/src/components/index.ts
@@ -1,4 +1,5 @@
 import Header from './header';
 import Footer from './footer';
+import Filter from './filter';
 
-export { Header, Footer };
+export { Header, Footer, Filter };
--- apps/marketplace/frontend/src/pages/home/Home.tsx
+++ apps/marketplace/frontend/src/pages/home/Home.tsx
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+import { Filter } from 'components';
+import styles from './Home.module.scss';
+
+const buttons = ['All', 'Buy now', 'On auction', 'New', 'Has offers'];
+
 function Home() {
-  return <div>PAGE CONTENT</div>;
+  const [filter, setFilter] = useState('All');
+
+  return (
+    <header className={styles.header}>
+      <h2 className={styles.heading}>NFT Marketplace</h2>
+      <Filter list={buttons} value={filter} onChange={setFilter} />
+    </header>
+  );
 }
 
 export default Home;
