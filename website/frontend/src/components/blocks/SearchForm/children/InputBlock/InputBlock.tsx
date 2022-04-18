5b7e39416d59bdd10fca06199979028b01c9882a
aderrod94 <igor.berg094@gmail.com>
2022-04-18 15:24:53 +0400

    fix Input from ui-kit

--- website/frontend/src/components/blocks/SearchForm/children/InputBlock/InputBlock.tsx
+++ website/frontend/src/components/blocks/SearchForm/children/InputBlock/InputBlock.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import { Input } from '@gear-js/ui';
 import { FieldProps } from 'formik';
-import { SearchIcon } from 'assets/Icons';
+import searchIcon from 'assets/images/search.svg';
 import styles from './InputBlock.module.scss';
 
 type Props = {
   props: FieldProps;
 };
 
-const InputBlock = (props: Props) => {
-  const iconColor = '#BBBBBB';
-
-  return (
-    <div className={styles.inputBlock}>
-      <div className={styles.icon}>
-        <SearchIcon color={iconColor} />
-      </div>
-      <Input className={styles.inputWrapper} type="text" {...props} />
-    </div>
-  );
-};
+const InputBlock = (props: Props) => <Input icon={searchIcon} className={styles.inputWrapper} type="text" {...props} />;
 
 export { InputBlock };
