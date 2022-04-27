a5c448c2b9c17a98c177a296d42497a85bb083d4
Dmitry Osipov <dmith.osipov@gmail.com>
2022-04-27 19:32:23 +0300

    Fix verifying captcha on backend (#619)

--- website/api-gateway/src/utils.ts
+++ website/api-gateway/src/utils.ts
@@ -1,5 +1,7 @@
 import { verify } from 'hcaptcha';
 import configuration from './config/configuration';
+import errors from '@gear-js/jsonrpc-errors';
+import { IRpcRequest, IRpcResponse } from './json-rpc/interface';
 const config = configuration();
 
 const SECRET = config.server.captchaSecret;
@@ -10,4 +12,17 @@ export async function verifyCaptcha(token: string) {
     return true;
   }
   return false;
+}
+
+export function getResponse(procedure: IRpcRequest, error?: any, result?: any): IRpcResponse {
+  const response: IRpcResponse = {
+    jsonrpc: '2.0',
+    id: procedure.id,
+  };
+  if (error) {
+    response['error'] = { message: errors[error].message, code: errors[error].code };
+  } else if (result) {
+    response['result'] = result;
+  }
+  return response;
 }
