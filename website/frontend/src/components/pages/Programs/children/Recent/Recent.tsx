666625c8c596138eecb61fba9055f1f93824e81e
Timur Rozhkov <hiddendesu@gmail.com>
2022-05-06 20:00:25 +0500

    fix displayed page data after account switch

--- website/frontend/src/components/pages/Programs/children/Recent/Recent.tsx
+++ website/frontend/src/components/pages/Programs/children/Recent/Recent.tsx
@@ -1,20 +1,23 @@
-import React, { useEffect, useState, VFC } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { ProgramModel } from 'types/program';
-
-import { INITIAL_LIMIT_BY_PAGE, LOCAL_STORAGE } from 'consts';
-
-import { ProgramsLegend } from 'components/pages/Programs/children/ProgramsLegend/ProgramsLegend';
-import { Pagination } from 'components/Pagination/Pagination';
+import { GearKeyring } from '@gear-js/api';
 
 import styles from './Recent.module.scss';
 import { UserProgram } from '../UserProgram/UserProgram';
+import { ProgramsLegend } from '../ProgramsLegend/ProgramsLegend';
 
-import { SearchForm } from '../../../../blocks/SearchForm/SearchForm';
+import { useAccount, useChangeEffect } from 'hooks';
+import { ProgramModel } from 'types/program';
+import { UserPrograms } from 'types/common';
 import { getUserPrograms } from 'services';
+import { INITIAL_LIMIT_BY_PAGE } from 'consts';
+import { Pagination } from 'components/Pagination/Pagination';
+import { SearchForm } from 'components/blocks/SearchForm/SearchForm';
 
-export const Recent: VFC = () => {
+export const Recent = () => {
   const location = useLocation();
+  const { account } = useAccount();
+
   const searchParams = new URLSearchParams(location.search);
   const pageFromUrl = searchParams.has('page') ? Number(searchParams.get('page')) : 1;
 
@@ -22,40 +25,47 @@ export const Recent: VFC = () => {
   const [programsCount, setProgramsCount] = useState(0);
 
   const [term, setTerm] = useState('');
-  const [currentPage, setCurrentPage] = useState(pageFromUrl);
+  const [page, setPage] = useState(pageFromUrl);
 
-  const onPageChange = (page: number) => setCurrentPage(page);
+  const handleSearch = (currentTerm: string) => {
+    setTerm(currentTerm);
+    setPage(1);
+  };
+
+  const handleRemoveQuery = () => {
+    setTerm('');
+    setPage(1);
+  };
+
+  useChangeEffect(() => {
+    setTerm('');
+    setPage(1);
+  }, [account]);
 
   useEffect(() => {
-    const programParams = {
-      owner: localStorage.getItem(LOCAL_STORAGE.PUBLIC_KEY_RAW),
-      limit: INITIAL_LIMIT_BY_PAGE,
-      offset: (currentPage - 1) * INITIAL_LIMIT_BY_PAGE,
-      term,
-    };
+    if (account) {
+      const params: UserPrograms = {
+        term,
+        owner: GearKeyring.decodeAddress(account.address),
+        limit: INITIAL_LIMIT_BY_PAGE,
+        offset: (page - 1) * INITIAL_LIMIT_BY_PAGE,
+      };
 
-    getUserPrograms(programParams).then(({ result }) => {
-      setPrograms(result.programs);
-      setProgramsCount(result.count);
-    });
-  }, [currentPage, term]);
+      getUserPrograms(params).then(({ result }) => {
+        setPrograms(result.programs);
+        setProgramsCount(result.count);
+      });
+    }
+  }, [page, term, account]);
 
   return (
     <div className={styles.blockList}>
       <div className={styles.paginationWrapper}>
         <span>Total results: {programsCount || 0}</span>
-        <Pagination page={currentPage} count={programsCount || 1} onPageChange={onPageChange} />
+        <Pagination page={page} count={programsCount || 1} onPageChange={setPage} />
       </div>
       <div>
-        <SearchForm
-          handleRemoveQuery={() => {
-            setTerm('');
-          }}
-          handleSearch={(val: string) => {
-            setTerm(val);
-          }}
-          placeholder="Find program"
-        />
+        <SearchForm placeholder="Find program" handleSearch={handleSearch} handleRemoveQuery={handleRemoveQuery} />
         <br />
       </div>
       <ProgramsLegend />
@@ -66,7 +76,7 @@ export const Recent: VFC = () => {
       </div>
       {programsCount > 0 && (
         <div className={styles.paginationBottom}>
-          <Pagination page={currentPage} count={programsCount} onPageChange={onPageChange} />
+          <Pagination page={page} count={programsCount} onPageChange={setPage} />
         </div>
       )}
     </div>
