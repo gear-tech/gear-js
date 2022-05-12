80dd91d72fc86376984ff312a8823c4943d56bb1
Dmitry Osipov <dmith.osipov@gmail.com>
2022-05-12 17:36:47 +0300

    tests for program... methods

--- website/tests/src/e2e/main.test.ts
+++ website/tests/src/e2e/main.test.ts
@@ -1,6 +1,6 @@
 import { GearApi, Hex } from '@gear-js/api';
 import { waitReady } from '@polkadot/wasm-crypto';
-import { getAllPrograms, uploadMeta } from './programs';
+import { getAllPrograms, getMeta, getProgramData, uploadMeta } from './programs';
 import base from '../config/base';
 import { processPrepare } from '../prepare';
 import { IPrepared, IPreparedProgram } from '../interfaces';
@@ -13,28 +13,38 @@ let api: GearApi;
 jest.setTimeout(30000);
 
 beforeAll(async () => {
-    api = await GearApi.create({ providerAddress: base.gear.wsProvider });
-    genesis = api.genesisHash.toHex();
-    prepared = await processPrepare(api);
-    await waitReady();
+  api = await GearApi.create({ providerAddress: base.gear.wsProvider });
+  genesis = api.genesisHash.toHex();
+  prepared = await processPrepare(api);
+  await waitReady();
 });
 
 afterAll(async () => {
-    await api.disconnect();
-    await sleep();
+  await api.disconnect();
+  await sleep();
 });
 
 describe('Main', () => {
-    test('getAllProgram', async () => {
-        expect(getAllPrograms(genesis, Object.keys(prepared.programs) as Hex[])).resolves.not.toThrowError();
-    });
+  test('program.all request', async () => {
+    expect(await getAllPrograms(genesis, Object.keys(prepared.programs) as Hex[])).toBeTruthy();
+  });
 
-    test('upload meta', async () => {
-        for (let id_ of Object.keys(prepared.programs)) {
-            const program = prepared.programs[id_] as IPreparedProgram;
-            if (program.spec.pathToMeta) {
-                expect(await uploadMeta(genesis, program)).resolves.not.toThrowError();
-            }
-        }
-    });
+  test('program.meta.add request', async () => {
+    for (let id_ of Object.keys(prepared.programs)) {
+      const program = prepared.programs[id_] as IPreparedProgram;
+      expect(await uploadMeta(genesis, program)).toBeTruthy();
+    }
+  });
+
+  test('program.meta.get request', async () => {
+    for (let id_ of Object.keys(prepared.programs)) {
+      expect(await getMeta(genesis, id_)).toBeTruthy();
+    }
+  });
+
+  test('program.data method', async () => {
+    for (let id_ of Object.keys(prepared.programs)) {
+      expect(await getProgramData(genesis, id_)).toBeTruthy();
+    }
+  });
 });
--- website/tests/src/e2e/programs.ts
+++ website/tests/src/e2e/programs.ts
@@ -7,53 +7,63 @@ import { IPreparedProgram, Passed } from '../interfaces';
 import accounts from '../config/accounts';
 
 export async function getAllPrograms(genesis: string, expected: Hex[]): Promise<Passed> {
-    const response = await request('program.all', { genesis });
-    expect(response).to.have.own.property('result');
-    expect(response.result.count).to.eq(expected.length);
+  const response = await request('program.all', { genesis });
+  expect(response).to.have.own.property('result');
+  expect(response.result.count).to.eq(expected.length);
 
-    response.result.programs
-        .map((program: any) => program.id)
-        .forEach((programId: Hex) => {
-            expect(expected).to.contains(programId);
-        });
-    return true;
+  response.result.programs
+    .map((program: any) => program.id)
+    .forEach((programId: Hex) => {
+      expect(expected).to.contains(programId);
+    });
+  return true;
 }
 
 export async function getProgramData(genesis: string, programId: string): Promise<Passed> {
-    const response = await request('program.data', { genesis, programId });
-    expect(response).to.have.own.property('result');
-    expect(response).to.have.all.keys('id', 'owner', 'name', 'uploadedAt', 'meta', 'title', 'initStatus');
-    return true;
+  const response = await request('program.data', { genesis, programId });
+  expect(response).to.have.own.property('result');
+  expect(response.result).to.have.all.keys(
+    'id',
+    'blockHash',
+    'genesis',
+    'owner',
+    'name',
+    'timestamp',
+    'meta',
+    'title',
+    'initStatus',
+  );
+  expect(response.result.meta).to.have.all.keys('meta');
+  return true;
 }
 
 export async function uploadMeta(genesis: string, program: IPreparedProgram): Promise<Passed> {
-    const accs = await accounts();
-    const metaFile = readFileSync(program.spec.pathToMeta);
-    const meta = JSON.stringify(await getWasmMetadata(metaFile));
-    const data = {
-        genesis,
-        programId: program.id,
-        meta,
-        metaFile: metaFile.toString('base64'),
-        name: program.spec.name,
-        title: `Test ${program.spec.name}`,
-        signature: u8aToHex(accs[program.spec.account].sign(meta)),
-    };
-    console.log(data);
-    const response = await request('program.meta.add', data);
-    expect(response).to.eq('Metadata added');
-    return true;
+  const accs = await accounts();
+  const metaFile = program.spec.pathToMeta ? readFileSync(program.spec.pathToMeta) : null;
+  const meta = metaFile ? JSON.stringify(await getWasmMetadata(metaFile)) : null;
+  const data = {
+    genesis,
+    programId: program.id,
+    meta,
+    metaFile: metaFile ? metaFile.toString('base64') : null,
+    name: program.spec.name,
+    title: `Test ${program.spec.name}`,
+    signature: u8aToHex(accs[program.spec.account].sign(meta)),
+  };
+  const response = await request('program.meta.add', data);
+  expect(response).to.have.property('result');
+  expect(response.result).to.have.property('status');
+  expect(response.result.status).to.eq('Metadata added');
+  return true;
 }
 
-export async function getMeta(genesis: string, programsIds: string[]): Promise<Passed> {
-    for (let id of programsIds) {
-        const data = {
-            genesis,
-            programId: id,
-        };
-        const response = await request('program.meta.get', data);
-        expect(response).not.to.have.property('result');
-        expect(response).to.have.all.keys('program', 'meta', 'metaFile');
-    }
-    return true;
+export async function getMeta(genesis: string, programId: string): Promise<Passed> {
+  const data = {
+    genesis,
+    programId,
+  };
+  const response = await request('program.meta.get', data);
+  expect(response).to.have.property('result');
+  expect(response.result).to.have.all.keys('program', 'meta', 'metaFile');
+  return true;
 }
