4bfcb73255a5b5ce209d09f06f10f03bf203ce7d
Dmitry Osipov <dmith.osipov@gmail.com>
2022-05-16 13:25:30 +0300

    fix: read env

--- website/tests/src/config/base.ts
+++ website/tests/src/config/base.ts
@@ -14,11 +14,11 @@ export default {
     api: checkEnv('API_ENDPOINT'),
   },
   db: {
-    host: checkEnv('DB_HOST'),
-    port: checkEnv('DB_PORT'),
-    username: checkEnv('DB_USERNAME'),
-    password: checkEnv('DB_PASSWORD'),
-    name: checkEnv('DB_NAME'),
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
   },
   isLocal: process.env.E2E_IS_LOCAL || false,
 };
