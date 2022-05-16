0a73013f338cc8d3016b40f80221171683fe23df
Dmitry Osipov <dmith.osipov@gmail.com>
2022-05-16 13:11:08 +0300

    fix workflow

--- website/tests/src/config/base.ts
+++ website/tests/src/config/base.ts
@@ -3,21 +3,22 @@ import { config } from 'dotenv';
 config();
 
 function checkEnv(name: string): string {
-    const env = process.env[name];
-    assert.notStrictEqual(env, undefined, `${name} is not specified`);
-    return env as string;
+  const env = process.env[name];
+  assert.notStrictEqual(env, undefined, `${name} is not specified`);
+  return env as string;
 }
 
 export default {
-    gear: {
-        wsProvider: checkEnv('WS_PROVIDER'),
-        api: checkEnv('API_ENDPOINT'),
-    },
-    db: {
-        host: checkEnv('DB_HOST'),
-        port: checkEnv('DB_PORT'),
-        username: checkEnv('DB_USERNAME'),
-        password: checkEnv('DB_PASSWORD'),
-        name: checkEnv('DB_NAME'),
-    },
+  gear: {
+    wsProvider: checkEnv('WS_PROVIDER'),
+    api: checkEnv('API_ENDPOINT'),
+  },
+  db: {
+    host: checkEnv('DB_HOST'),
+    port: checkEnv('DB_PORT'),
+    username: checkEnv('DB_USERNAME'),
+    password: checkEnv('DB_PASSWORD'),
+    name: checkEnv('DB_NAME'),
+  },
+  isLocal: process.env.E2E_IS_LOCAL || false,
 };
--- website/tests/test-setup.js
+++ website/tests/test-setup.js
@@ -4,20 +4,22 @@ const config = require('./src/config/base').default;
 const { exec } = require('child_process');
 
 module.exports = async () => {
-  const dataSource = new DataSource({
-    type: 'postgres',
-    host: config.db.host,
-    port: config.db.port,
-    username: config.db.username,
-    password: config.db.password,
-    database: config.db.name,
-    synchronize: true,
-    entities: [Program, Message, Meta],
-  });
-  await dataSource.initialize();
-  await dataSource.getRepository(Program).delete({});
-  await dataSource.getRepository(Message).delete({});
-  await dataSource.getRepository(Meta).delete({});
+  if (config.isLocal) {
+    const dataSource = new DataSource({
+      type: 'postgres',
+      host: config.db.host,
+      port: config.db.port,
+      username: config.db.username,
+      password: config.db.password,
+      database: config.db.name,
+      synchronize: true,
+      entities: [Program, Message, Meta],
+    });
+    await dataSource.initialize();
+    await dataSource.getRepository(Program).delete({});
+    await dataSource.getRepository(Message).delete({});
+    await dataSource.getRepository(Meta).delete({});
+  }
   await new Promise((resolve, reject) =>
     exec('./download-examples.sh', (error) => {
       if (error) {
