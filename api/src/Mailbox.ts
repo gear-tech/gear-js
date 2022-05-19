b457e8a8e4075e1b10bb07fc9c4bcbc5abd56c7d
Dmitry Osipov <dmith.osipov@gmail.com>
2022-05-19 16:19:54 +0300

    resolve comments

--- api/src/Mailbox.ts
+++ api/src/Mailbox.ts
@@ -29,14 +29,14 @@ export class GearMailbox {
    */
   async read(accountId: Hex | AccountId32 | string, messageId?: Hex | H256): Promise<MailboxType> {
     if (messageId) {
-      const mailbox = await this.api.query.gearMessenger['mailbox'](accountId, messageId);
+      const mailbox = await this.api.query.gearMessenger.mailbox(accountId, messageId);
       return mailbox.toHuman() as MailboxType;
     } else {
-      const keys = await this.api.query.gearMessenger['mailbox'].keys(accountId);
+      const keys = await this.api.query.gearMessenger.mailbox.keys(accountId);
       if (keys.length === 0) {
         return [];
       }
-      const keyPrefixes = this.api.query.gearMessenger['mailbox'].keyPrefix(accountId);
+      const keyPrefixes = this.api.query.gearMessenger.mailbox.keyPrefix(accountId);
       const keysPaged = await this.api.rpc.state.getKeysPaged(keyPrefixes, 1000, keyPrefixes);
       const mailbox = (await this.api.rpc.state.queryStorageAt(keysPaged)) as Option<StoredMessage>[];
       return mailbox.map((option, index) => {
