82f238626a0abd294822f020a3cb3abd81417cc5
Dmitry Osipov <dmith.osipov@gmail.com>
2022-05-24 13:52:20 +0300

    Update mailbox (#658)

--- api/CHANGELOG.md
+++ api/CHANGELOG.md
@@ -1,6 +1,18 @@
+## 0.19.0
+
+_05/20/2022_
+
+https://github.com/gear-tech/gear-js/pull/658
+
+### Changes
+
+- Update pallet name with mailbox
+
+---
+
 ## 0.18.3
 
-_16/11/2022_
+_05/16/2022_
 
 https://github.com/gear-tech/gear-js/pull/648
 
@@ -12,7 +24,7 @@ https://github.com/gear-tech/gear-js/pull/648
 
 ## 0.18.2
 
-_16/11/2022_
+_05/16/2022_
 
 https://github.com/gear-tech/gear-js/pull/647
 
--- api/src/Mailbox.ts
+++ api/src/Mailbox.ts
@@ -29,14 +29,14 @@ export class GearMailbox {
    */
   async read(accountId: Hex | AccountId32 | string, messageId?: Hex | H256): Promise<MailboxType> {
     if (messageId) {
-      const mailbox = await this.api.query.gear['mailbox'](accountId, messageId);
+      const mailbox = await this.api.query.gearMessenger.mailbox(accountId, messageId);
       return mailbox.toHuman() as MailboxType;
     } else {
-      const keys = await this.api.query.gear['mailbox'].keys(accountId);
+      const keys = await this.api.query.gearMessenger.mailbox.keys(accountId);
       if (keys.length === 0) {
         return [];
       }
-      const keyPrefixes = this.api.query.gear['mailbox'].keyPrefix(accountId);
+      const keyPrefixes = this.api.query.gearMessenger.mailbox.keyPrefix(accountId);
       const keysPaged = await this.api.rpc.state.getKeysPaged(keyPrefixes, 1000, keyPrefixes);
       const mailbox = (await this.api.rpc.state.queryStorageAt(keysPaged)) as Option<StoredMessage>[];
       return mailbox.map((option, index) => {
@@ -49,32 +49,4 @@ export class GearMailbox {
       });
     }
   }
-
-  /**
-   * Subscribe to user mailbox changes
-   * @param accountId AccountId in hex or base58 format
-   * @param callback callback with mailbox data
-   * @examples
-   * ```javascript
-   * const unsub = await gearApi.mailbox.subscribe('0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d', (data) => {
-   *    console.log(data.toHuman());
-   * })
-   * ```
-   */
-  // subscribe(
-  //   accountId: Hex | AccountId32 | string,
-  //   callback: (data: Option<BTreeMap<H256, QueuedMessage>>) => void,
-  // ): UnsubscribePromise {
-  //   this.subscription = this.api.query.gear.mailbox(accountId, '', callback);
-  //   return this.subscription;
-  // }
-
-  /**
-   * Unsubscribe from user mailbox changes
-   */
-  // unsubscribe() {
-  //   this.subscription.then((fn) => {
-  //     fn();
-  //   });
-  // }
 }
