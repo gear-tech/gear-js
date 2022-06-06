249e116cf28fe1e3b19a334c5e5c6c03dd3857d5
Nikita Yutanov <nikitayutanov@gmail.com>
2022-06-06 18:08:32 +0300

    Separate template and cga

--- utils/create-gear-app/index.js
+++ utils/create-gear-app/index.js
@@ -2,10 +2,11 @@
 
 'use strict';
 
+const spawn = require('cross-spawn');
+
 const args = process.argv.slice(2);
 const [folder] = args;
 
-const spawn = require('cross-spawn');
-const npxArgs = ['create-react-app', folder || 'gear-app', '--template', 'create-gear-app'];
+const npxArgs = ['create-react-app', folder || 'gear-app', '--template', '@gear-js/cra-template-gear-app'];
 
 spawn('npx', npxArgs, { stdio: 'inherit' });
