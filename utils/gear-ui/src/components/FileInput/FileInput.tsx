320742c506487249f4f8d9aded730ec80c17e4fa
Nikita Yutanov <nikitayutanov@gmail.com>
2022-06-08 23:50:33 +0300

    Drop file input value

--- utils/gear-ui/src/components/FileInput/FileInput.tsx
+++ utils/gear-ui/src/components/FileInput/FileInput.tsx
@@ -1,74 +1,71 @@
-import { Button } from '../Button/Button';
 import clsx from 'clsx';
 import {
   InputHTMLAttributes,
   useRef,
   MouseEvent,
-  useEffect,
   forwardRef,
   ForwardedRef,
   useImperativeHandle,
+  useState,
+  ChangeEvent,
 } from 'react';
+import { Button } from '@gear-js/ui';
 import trashSVG from './images/trash.svg';
 import styles from './FileInput.module.scss';
 
-interface Props extends Omit<InputHTMLAttributes<HTMLInputElement>, 'value'> {
-  value: File | undefined;
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
 }
 
-type Ref = ForwardedRef<HTMLInputElement>;
+const FileInput = forwardRef(
+  ({ label, className, onChange, ...attrs }: Props, forwardedRef: ForwardedRef<HTMLInputElement>) => {
+    const [name, setName] = useState('');
+    const ref = useRef<HTMLInputElement>(null);
+    const labelClassName = clsx(styles.label, className);
 
-const FileInput = forwardRef(({ label, className, value, ...attrs }: Props, forwardedRef: Ref) => {
-  const ref = useRef<HTMLInputElement>(null);
-  const labelClassName = clsx(styles.label, className);
+    // TODO: figure out what's wrong
+    // @ts-ignore
+    useImperativeHandle(forwardedRef, () => ref.current);
 
-  // TODO: figure out what's wrong
-  // @ts-ignore
-  useImperativeHandle(forwardedRef, () => ref.current);
+    const handleButtonClick = () => {
+      ref.current?.click();
+    };
 
-  const handleButtonClick = () => {
-    ref.current?.click();
-  };
+    const resetValue = () => {
+      if (ref.current) {
+        ref.current.value = '';
+      }
+    };
 
-  const resetValue = () => {
-    if (ref.current) ref.current.value = '';
-  };
+    const handleRemoveButtonClick = (e: MouseEvent<HTMLButtonElement>) => {
+      if (ref.current) {
+        resetValue();
+        const changeEvent = new Event('change', { bubbles: true });
+        ref.current.dispatchEvent(changeEvent);
+      }
+    };
 
-  const handleRemoveButtonClick = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setName(e.target.files?.[0]?.name || '');
 
-    if (ref.current) {
-      resetValue();
-      const changeEvent = new Event('change', { bubbles: true });
-      ref.current.dispatchEvent(changeEvent);
-    }
-  };
+      if (onChange) onChange(e);
+    };
 
-  useEffect(() => {
-    if (!value) resetValue();
-  }, [value]);
-
-  return (
-    <label className={labelClassName}>
-      {label && <span className={styles.text}>{label}</span>}
-      <input type="file" className={styles.input} ref={ref} {...attrs} />
-      {value ? (
-        <span className={styles.file}>
-          <Button
-            text={value.name}
-            color="transparent"
-            size="small"
-            className={styles.name}
-            onClick={handleButtonClick}
-          />
-          <Button icon={trashSVG} color="transparent" onClick={handleRemoveButtonClick} />
-        </span>
-      ) : (
-        <Button text="Select file" color="secondary" size="small" onClick={handleButtonClick} />
-      )}
-    </label>
-  );
-});
+    return (
+      <label className={labelClassName}>
+        {label && <span className={styles.text}>{label}</span>}
+        <input type="file" className={styles.input} ref={ref} onChange={handleChange} {...attrs} />
+        {name ? (
+          <span className={styles.file}>
+            <Button text={name} color="transparent" size="small" className={styles.name} onClick={handleButtonClick} />
+            <Button icon={trashSVG} color="transparent" onClick={handleRemoveButtonClick} />
+          </span>
+        ) : (
+          <Button text="Select file" color="secondary" size="small" onClick={handleButtonClick} />
+        )}
+      </label>
+    );
+  },
+);
 
 export { FileInput, Props as FileInputProps, styles as fileInputStyles };
