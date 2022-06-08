297e2615a1ca763c3ae9882b1e30c09555a986d1
Almazatun <atunstyle@gmail.com>
2022-06-08 22:32:47 +0600

    Update README file and delete unused files

--- website/api-gateway/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ApiGatewayModule } from './api-gateway/api-gateway.module';
-import { HealthcheckModule } from './healthcheck/healthcheck.module';
-
-@Module({
-  imports: [HealthcheckModule, ApiGatewayModule],
-})
-export class AppModule {}
