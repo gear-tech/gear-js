10b8726b6e7a22c8681dc5ad6ba6e56d537319dc
Almazatun <atunstyle@gmail.com>
2022-06-10 21:58:17 +0600

    Update jsonrpc errors in test file

--- website/data-storage/test/errors.spec.ts
+++ website/data-storage/test/errors.spec.ts
@@ -1,17 +1,18 @@
-import errors from '@gear-js/jsonrpc-errors';
+import { JSONRPC_ERRORS } from '@gear-js/common';
+
 import { MessageNotFound, MetadataNotFound, ProgramNotFound, SignatureNotVerified } from '../src/errors';
 
 describe('Errors', () => {
   it('MessageNotFound', () => {
-    expect(MessageNotFound.name).toBe(errors.MessageNotFound.name);
+    expect(MessageNotFound.name).toBe(JSONRPC_ERRORS.MessageNotFound.name);
   });
   it('MetadataNotFound', () => {
-    expect(MetadataNotFound.name).toBe(errors.MetadataNotFound.name);
+    expect(MetadataNotFound.name).toBe(JSONRPC_ERRORS.MetadataNotFound.name);
   });
   it('ProgramNotFound', () => {
-    expect(ProgramNotFound.name).toBe(errors.ProgramNotFound.name);
+    expect(ProgramNotFound.name).toBe(JSONRPC_ERRORS.ProgramNotFound.name);
   });
   it('SignNotVerified', () => {
-    expect(SignatureNotVerified.name).toBe(errors.SignatureNotVerified.name);
+    expect(SignatureNotVerified.name).toBe(JSONRPC_ERRORS.SignatureNotVerified.name);
   });
 });
