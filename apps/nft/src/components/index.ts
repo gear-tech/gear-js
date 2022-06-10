4b5bdaec9d54c90036a8c9b2d2415f1ea4d7c3f1
Nikita Yutanov <nikitayutanov@gmail.com>
2022-06-10 03:26:37 +0300

    Add loader

--- apps/nft/src/components/index.ts
+++ apps/nft/src/components/index.ts
@@ -1,6 +1,6 @@
 import { Header, Footer } from './layout';
 import { AddressModal, ConfirmationModal } from './modals';
-import { ApiLoader } from './api-loader';
+import { ApiLoader, Loader } from './loaders';
 import { Alert } from './alert';
 
-export { Header, Footer, AddressModal, ConfirmationModal, ApiLoader, Alert };
+export { Header, Footer, AddressModal, ConfirmationModal, ApiLoader, Loader, Alert };
