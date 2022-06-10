ef15edc479f8ab08ac3d0ffbb5cefefef36a217f
Nikita Yutanov <nikitayutanov@gmail.com>
2022-06-10 22:00:22 +0300

    Add loader on nft read

--- apps/nft/src/pages/nft/NFT.tsx
+++ apps/nft/src/pages/nft/NFT.tsx
@@ -1,27 +1,14 @@
-import { useAccount } from '@gear-js/react-hooks';
 import { useEffect, useState } from 'react';
 import { TokenDetails } from 'types';
 import { getIpfsAddress } from 'utils';
-import { Button } from '@gear-js/ui';
-import { ConfirmationModal, AddressModal } from 'components';
-import clsx from 'clsx';
+import { ConfirmationModal, AddressModal, Loader } from 'components';
 import { Hex } from '@gear-js/api';
 import { useNFT, useSendNFTMessage } from 'hooks';
-import { Card } from './card';
-import { Addresses } from './addresses';
-import { Attributes } from './attributes';
-import styles from './NFT.module.scss';
+import { Content } from './content';
 
 function NFT() {
-  const { account } = useAccount();
-
   const nft = useNFT();
-  const { id, name, ownerId, description, media, reference, approvedAccountIds } = nft || {};
-
-  const heading = `${name} #${id}`;
-  const src = media ? getIpfsAddress(media) : '';
-  const isAnyApprovedAccount = !!approvedAccountIds?.length;
-  const isOwner = account?.decodedAddress === ownerId;
+  const { id, reference } = nft || {};
 
   const sendMessage = useSendNFTMessage();
 
@@ -56,31 +43,22 @@ function NFT() {
 
   return (
     <>
-      <h2 className={styles.heading}>{heading}</h2>
-      <div className={styles.main}>
-        <section>
-          <div className={styles.imgCard}>
-            <img src={src} alt={name} className={styles.image} />
-          </div>
-          {isOwner && (
-            <div className={styles.buttons}>
-              <Button text="Transfer" color="secondary" onClick={openTransferModal} block />
-              <Button text="Approve" onClick={openApproveModal} block />
-            </div>
-          )}
-        </section>
-        <section>
-          <div className={clsx(styles.main, styles.details)}>
-            {ownerId && <Card heading="Owner" text={ownerId} />}
-            {rarity && <Card heading="Rarity" text={rarity} />}
-            {description && <Card heading="Description" text={description} />}
-            {attributes && <Attributes attributes={attributes} />}
-          </div>
-          {isAnyApprovedAccount && (
-            <Addresses list={approvedAccountIds} onAddressClick={openRevokeModal} isOwner={isOwner} />
-          )}
-        </section>
-      </div>
+      {nft ? (
+        <Content
+          heading={`${nft.name} #${nft.id}`}
+          image={getIpfsAddress(nft.media)}
+          ownerId={nft.ownerId}
+          description={nft.description}
+          approvedAccounts={nft.approvedAccountIds}
+          rarity={rarity}
+          attributes={attributes}
+          onTransferButtonClick={openTransferModal}
+          onApproveButtonClick={openApproveModal}
+          onRevokeButtonClick={openRevokeModal}
+        />
+      ) : (
+        <Loader />
+      )}
       {isTransferModalOpen && <AddressModal heading="Transfer token" close={closeModal} onSubmit={transfer} />}
       {isApproveModalOpen && <AddressModal heading="Approve token" close={closeModal} onSubmit={approve} />}
       {revokedAddress && <ConfirmationModal heading="Revoke approval?" close={closeModal} onSubmit={revoke} />}
