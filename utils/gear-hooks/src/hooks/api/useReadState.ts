582a8d127b5e7a1681408bd07a1a06b4b367a079
Nikita Yutanov <nikitayutanov@gmail.com>
2022-06-14 20:05:13 +0300

    Add read state and metadata error handlers

--- utils/gear-hooks/src/hooks/api/useReadState.ts
+++ utils/gear-hooks/src/hooks/api/useReadState.ts
@@ -1,20 +1,24 @@
 import { ProgramId } from '@gear-js/api';
 import { AnyJson } from '@polkadot/types/types';
 import { useEffect, useState, useContext } from 'react';
-import { ApiContext } from 'context';
+import { AlertContext, ApiContext } from 'context';
 import { useConditionalMetaBuffer } from './useMetadata';
 
 function useReadState(programId: ProgramId, metaSourceOrBuffer: string | Buffer | undefined, payload?: AnyJson) {
-  const [state, setState] = useState<AnyJson>();
   const { api } = useContext(ApiContext); // сircular dependency fix
+  const alert = useContext(AlertContext);
+
   const metaBuffer = useConditionalMetaBuffer(metaSourceOrBuffer);
+
+  const [state, setState] = useState<AnyJson>();
 
   useEffect(() => {
     if (metaBuffer && payload) {
       api.programState
         .read(programId, metaBuffer, payload)
         .then((codecState) => codecState.toHuman())
-        .then(setState);
+        .then(setState)
+        .catch(({ message }: Error) => alert.error(message));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [metaBuffer, payload]);
