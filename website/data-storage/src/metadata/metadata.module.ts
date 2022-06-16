7cf85eeecfe6502505a1283053ff916d8fc37b6a
Almazatun <atunstyle@gmail.com>
2022-06-16 22:35:42 +0600

    Add metadata repository and refactor program service

--- website/data-storage/src/metadata/metadata.module.ts
+++ website/data-storage/src/metadata/metadata.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { ProgramsModule } from 'src/programs/programs.module';
+import { ProgramModule } from 'src/program/program.module';
 import { Meta } from '../entities/meta.entity';
 import { MetadataService } from './metadata.service';
 import { Program } from '../entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Program, Meta]), Meta, ProgramsModule],
+  imports: [TypeOrmModule.forFeature([Program, Meta]), Meta, ProgramModule],
   providers: [MetadataService],
   exports: [MetadataService],
 })
