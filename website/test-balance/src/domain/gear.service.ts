ee9cef9f6b54f9e1b90a8db222b1a35968b75119
Almazatun <atunstyle@gmail.com>
2022-06-22 18:43:10 +0600

    Update transferBalance method and refactor

--- website/test-balance/src/domain/gear.service.ts
+++ website/test-balance/src/domain/gear.service.ts
@@ -1,11 +1,10 @@
-import { GearApi, GearKeyring } from '@gear-js/api';
+import { GearApi, GearKeyring, TransferData } from '@gear-js/api';
 import { initLogger } from '@gear-js/common';
-
-import config from '../config/config';
-
 import { BN } from '@polkadot/util';
 import { KeyringPair } from '@polkadot/keyring/types';
 import { DbService } from '../database/db';
+
+import config from '../config/config';
 
 const log = initLogger('GearService');
 
@@ -21,7 +20,7 @@ export class GearService {
     this.dbService = dbService;
   }
 
-  async connect() {
+  public async connect() {
     this.api = await GearApi.create({ providerAddress: config.gear.providerAddress });
     this.api.on('error', () => {
       GearApi.create({ providerAddress: config.gear.providerAddress }).then(
@@ -34,24 +33,52 @@ export class GearService {
         },
       );
     });
-    log.info(`Connected to ${await this.api.chain()} with genesis ${this.genesisHash}`);
-    this.account = await GearKeyring.fromSeed(config.gear.accountSeed);
-    this.rootAccount =
-      config.gear.rootAccountSeed === '//Alice'
-        ? await GearKeyring.fromSuri('//Alice')
-        : await GearKeyring.fromSeed(config.gear.rootAccountSeed);
+    log.info(`Connected to ${await this.api.chain()} with genesis ${this.getGenesisHash}`);
+
+    const [account, rootAccountByGearKeyring] = await Promise.all([
+      GearKeyring.fromSeed(config.gear.accountSeed),
+      this.setRootAccountSeedByGearKeyring(),
+    ]);
+
+    this.account = account;
+    this.rootAccount = rootAccountByGearKeyring;
     this.accountBalance = new BN(config.gear.accountBalance);
     this.balanceToTransfer = new BN(config.gear.balanceToTransfer);
-    if (await this.accountBalanceIsSmall()) {
-      this.transferBalance(this.account.address, this.rootAccount, this.accountBalance);
+    if (await this.isSmallAccountBalance()) {
+      await this.transferBalance(this.account.address, this.rootAccount, this.accountBalance);
     }
   }
 
-  get genesisHash(): string {
+  public async transferBalance(to: string, from: KeyringPair, balance = this.accountBalance) {
+    await Promise.all([this.api.balance.transfer(to, balance), this.transferData(from)]).catch((error) =>
+      log.error(error),
+    );
+
+    if (to !== this.account.address) {
+      await this.dbService.setTransferDate(to, this.getGenesisHash);
+    }
+    return { status: 'ok', transferredBalance: balance.toString() };
+  }
+
+  public get getGenesisHash(): string {
     return this.api.genesisHash.toHex();
   }
 
-  async accountBalanceIsSmall(): Promise<boolean> {
+  private async transferData(from: KeyringPair): Promise<TransferData> {
+    return new Promise((resolve, reject) => {
+      this.api.balance.signAndSend(from, ({ events }) => {
+        events.forEach(({ event: { method, data } }) => {
+          if (method === 'Transfer') {
+            resolve(data as TransferData);
+          } else if (method === 'ExtrinsicFailed') {
+            reject(data);
+          }
+        });
+      });
+    });
+  }
+
+  private async isSmallAccountBalance(): Promise<boolean> {
     const balance = await this.api.balance.findOut(this.account.address);
     if (balance.lt(this.accountBalance)) {
       return true;
@@ -59,11 +86,12 @@ export class GearService {
     return false;
   }
 
-  async transferBalance(to: string, from = this.account, value = this.balanceToTransfer) {
-    await this.api.balance.transferBalance(from, to, value);
-    if (to !== this.account.address) {
-      await this.dbService.setTransferDate(to, this.genesisHash);
+  private async setRootAccountSeedByGearKeyring(): Promise<KeyringPair> {
+    const envVar = config.gear.rootAccountSeed;
+    if (envVar === '//Alice') {
+      return GearKeyring.fromSuri('//Alice');
+    } else {
+      return GearKeyring.fromSeed(config.gear.rootAccountSeed);
     }
-    return { status: 'ok', transferedBalance: value.toString() };
   }
 }
