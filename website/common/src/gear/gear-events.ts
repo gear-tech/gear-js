57e6597a4e669137c59d3e11c180f3403ab58c97
Almazatun <atunstyle@gmail.com>
2022-06-23 16:46:29 +0600

    Add gear events

---
+++ website/common/src/gear/gear-events.ts
@@ -0,0 +1,12 @@
+export enum GEAR_EVENT {
+  MESSAGE_ENQUEUED = 'MessageEnqueued',
+  USER_MESSAGE_SENT = 'UserMessageSent',
+  USER_MESSAGE_READ = 'UserMessageRead',
+  MESSAGES_DISPATCHED = 'MessagesDispatched',
+  MESSAGE_WAITED = 'MessageWaited',
+  MESSAGE_WAKEN = 'MessageWaken',
+  CODE_CHANGED = 'CodeChanged',
+  PROGRAM_CHANGED = 'ProgramChanged',
+  DEBUG_DATA_SNAPSHOT = 'DebugDataSnapshot',
+  DEBUG_MODE = 'DebugMode',
+}
