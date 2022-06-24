db8d41d1600b78b8b45063eb3250be2db69431b9
Almazatun <atunstyle@gmail.com>
2022-06-24 15:35:20 +0600

    Delete unnecessary gear events

--- website/common/src/gear/gear-events.ts
+++ website/common/src/gear/gear-events.ts
@@ -1,12 +1,8 @@
 export enum GEAR_EVENT {
   MESSAGE_ENQUEUED = 'MessageEnqueued',
   USER_MESSAGE_SENT = 'UserMessageSent',
-  USER_MESSAGE_READ = 'UserMessageRead',
   MESSAGES_DISPATCHED = 'MessagesDispatched',
-  MESSAGE_WAITED = 'MessageWaited',
-  MESSAGE_WAKEN = 'MessageWaken',
   CODE_CHANGED = 'CodeChanged',
   PROGRAM_CHANGED = 'ProgramChanged',
-  DEBUG_DATA_SNAPSHOT = 'DebugDataSnapshot',
-  DEBUG_MODE = 'DebugMode',
+  DATABASE_WIPED = 'DatabaseWiped',
 }
