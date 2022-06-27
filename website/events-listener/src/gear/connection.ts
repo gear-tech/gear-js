94b3e4366b0f735c76dd14f57ac5b022c854ccfc
Dmitry Osipov <dmith.osipov@gmail.com>
2022-06-27 14:01:57 +0200

    fix: loss connection in events-listener

---
+++ website/events-listener/src/gear/connection.ts
@@ -0,0 +1,37 @@
+import { GearApi } from '@gear-js/api';
+import config from '../config/configuration';
+import { eventListenerLogger } from '../common/event-listener.logger';
+import { changeStatus } from '../routes/healthcheck/healthcheck.router';
+import { KafkaProducer } from '../kafka/producer';
+import { listen } from './listener';
+
+export async function connectToGearNode(producer: KafkaProducer) {
+  let api: GearApi = new GearApi({
+    providerAddress: config.api.provider,
+    throwOnConnect: true,
+  });
+  api.isReadyOrError.catch(console.error);
+  try {
+    await api.isReady;
+  } catch (error) {
+    console.log('api.isReady', error);
+  }
+  changeStatus('ws');
+
+  const chain = await api.chain();
+  const genesis = api.genesisHash.toHex();
+
+  eventListenerLogger.info(`Connected to ${chain} with genesis ${genesis}`);
+
+  let unsub = await listen(api, genesis, ({ key, value }) => {
+    producer.send(key, value, genesis);
+  });
+
+  return new Promise((resolve) => {
+    api.on('error', (error) => {
+      unsub();
+      changeStatus('ws');
+      resolve(error);
+    });
+  });
+}
---
+++ website/events-listener/src/gear/index.ts
@@ -0,0 +1,1 @@
+export { connectToGearNode } from './connection';
