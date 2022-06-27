5057b149f044fddca0614e8280bfab0639ba1c2e
Dmitry Osipov <dmith.osipov@gmail.com>
2022-06-27 18:50:47 +0300

    Fix loss connection in events-listener (#769)

--- website/events-listener/src/main.ts
+++ website/events-listener/src/main.ts
@@ -1,12 +1,9 @@
-import { GearApi } from '@gear-js/api';
 import express from 'express';
-
 import { KafkaProducer } from './kafka/producer';
 import config from './config/configuration';
-import { restartIfNeeded, setRestartNeeded } from './lifecycle';
 import { eventListenerLogger } from './common/event-listener.logger';
 import { changeStatus, healthcheckRouter } from './routes/healthcheck/healthcheck.router';
-import { listen } from './gear-events';
+import { connectToGearNode } from './gear';
 
 const app = express();
 
@@ -15,31 +12,23 @@ const port = config.healthcheck.port;
 app.use('/health', healthcheckRouter);
 
 const startApp = async () => {
-  const api = await GearApi.create({ providerAddress: config.api.provider, throwOnConnect: true });
-  changeStatus('ws');
-  api.on('error', () => {
-    setRestartNeeded();
-  });
-
-  const chain = await api.chain();
-  const genesis = api.genesisHash.toHex();
-
-  eventListenerLogger.info(`Connected to ${chain} with genesis ${genesis}`);
-
   const producer = new KafkaProducer();
   await producer.createTopic('events');
   await producer.connect();
   changeStatus('kafka');
 
-  listen(api, genesis, ({ key, value }) => {
-    producer.send(key, value, genesis);
-  });
-
   app.listen(port, () => {
     eventListenerLogger.info(`Healthckech server is running on port ${port} 🚀`);
   });
 
-  await restartIfNeeded;
+  // It's neccessary to retain connection during runtimeUpgrade
+  while (true) {
+    await connectToGearNode(producer);
+    console.log('Reconnecting...');
+  }
 };
 
-startApp();
+startApp().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
