d853d37a01fceac265d6ecf7b10597652298c5ef
Nikita Yutanov <nikitayutanov@gmail.com>
2022-06-29 17:41:27 +0300

    Add alert component, heading styles and resets (#773)
    * Add button color variables
    
    * Add noWrap button prop
    
    * Add headings mixins
    
    * Add alert component
    
    * Fix checkbox default margin reset
    
    * Fix read only styles on disabled textarea
    
    * Separate resets to a classes
    
    * Add resets and move fonts to typography module

---
+++ utils/gear-ui/src/components/Alert/Alert.types.ts
@@ -0,0 +1,22 @@
+import { CSSProperties, ReactNode } from 'react';
+
+type Options = {
+  type: 'info' | 'error' | 'loading' | 'success';
+  style?: CSSProperties;
+  title?: string;
+  timeout?: number;
+  isClosed?: boolean;
+};
+
+type Alert = {
+  id: string;
+  content: ReactNode;
+  options: Options;
+};
+
+type Props = {
+  alert: Alert;
+  close: () => void;
+};
+
+export { Props };
