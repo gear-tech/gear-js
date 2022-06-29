e6844a26df0ea5067b7b7820e30f2b178b0773a5
Almazatun <atunstyle@gmail.com>
2022-06-29 17:08:54 +0600

    Update migrations field

--- website/data-storage/src/data-source.ts
+++ website/data-storage/src/data-source.ts
@@ -1,4 +1,5 @@
 import { DataSource } from 'typeorm';
+import { join } from 'path';
 
 import { Message, Meta, Program } from './entities';
 import config from './config/configuration';
@@ -16,7 +17,7 @@ export const AppDataSource = new DataSource({
   password: config().database.password,
   database: config().database.name,
   entities,
-  migrations: ['dist/database/migrations/*.js'],
+  migrations: [join(__dirname, 'database/migrations/*.js')],
   synchronize: false,
   logging: true,
   // Run migrations automatically,
