2232d7afb835f9f4006e585a02e2d573dcbae168
Timur Rozhkov <hiddendesu@gmail.com>
2022-07-01 13:27:13 +0300

    refactor claim message hook

--- website/frontend/src/hooks/useClaimMessage.ts
+++ website/frontend/src/hooks/useClaimMessage.ts
@@ -1,9 +1,10 @@
 import { useCallback } from 'react';
-import { Hex } from '@gear-js/api';
+import { Hex, UserMessageRead } from '@gear-js/api';
 import { web3FromSource } from '@polkadot/extension-dapp';
 
 // eslint-disable-next-line import/no-cycle
 import { useApi, useAccount, useAlert } from '.';
+import { getExtrinsicFailedMessage } from 'helpers';
 import { DEFAULT_ERROR_OPTIONS, DEFAULT_SUCCESS_OPTIONS } from 'context/alert/const';
 
 const useClaimMessage = () => {
@@ -28,13 +29,7 @@ const useClaimMessage = () => {
 
         const { signer } = await web3FromSource(meta.source);
 
-        await api.claimValueFromMailbox.signAndSend(address, { signer }, ({ status }) => {
-          if (status.isBroadcast) {
-            alert.update(alertId, 'Broadcast');
-
-            return;
-          }
-
+        await api.claimValueFromMailbox.signAndSend(address, { signer }, ({ status, events }) => {
           if (status.isReady) {
             alert.update(alertId, 'Ready');
 
@@ -43,6 +38,30 @@ const useClaimMessage = () => {
 
           if (status.isInBlock) {
             alert.update(alertId, 'InBlock');
+
+            events.forEach(({ event }) => {
+              const { method, section, data } = event as UserMessageRead;
+
+              const title = `${section}.${method}`;
+
+              if (method === 'UserMessageRead') {
+                const reason = data.reason.toHuman() as { [key: string]: string };
+                const reasonKey = Object.keys(reason)[0];
+                const reasonValue = reason[reasonKey];
+
+                const message = `${data.id.toHuman()}\n ${reasonKey}: ${reasonValue}`;
+
+                alert.success(message, { title });
+
+                return;
+              }
+
+              if (method === 'ExtrinsicFailed') {
+                alert.error(getExtrinsicFailedMessage(api, event), { title });
+
+                return;
+              }
+            });
 
             return;
           }
