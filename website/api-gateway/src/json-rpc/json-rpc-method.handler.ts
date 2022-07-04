e7b365d9ab35da10359268d62cd24a0d36a854bf
Almazatun <atunstyle@gmail.com>
2022-07-04 15:52:19 +0600

    Move rpcMethods

--- website/api-gateway/src/json-rpc/json-rpc-method.handler.ts
+++ website/api-gateway/src/json-rpc/json-rpc-method.handler.ts
@@ -12,22 +12,22 @@ import {
   testBalance,
 } from '../kafka/kafka-events';
 import { KafkaParams } from '../kafka/types';
-import { RpcMapping } from './types';
+import { RpcMethods } from './types';
+
+const rpcMethods: RpcMethods = {
+  [KAFKA_TOPICS.PROGRAM_DATA]: programData,
+  [KAFKA_TOPICS.PROGRAM_ALL]: programAll,
+  [KAFKA_TOPICS.PROGRAM_META_ADD]: programMetaAdd,
+  [KAFKA_TOPICS.PROGRAM_META_GET]: programMetaGet,
+  [KAFKA_TOPICS.PROGRAM_ALL_USER]: programAllUsers,
+  [KAFKA_TOPICS.MESSAGE_ALL]: messageAll,
+  [KAFKA_TOPICS.MESSAGE_DATA]: messageData,
+  [KAFKA_TOPICS.MESSAGE_ADD_PAYLOAD]: messageAddPayload,
+  [KAFKA_TOPICS.TEST_BALANCE_GET]: testBalance,
+};
 
 export function jsonRpcMethodHandler(method: KAFKA_TOPICS | string, params: KafkaParams): Promise<any> {
-  const getRpcMethod: RpcMapping = {
-    [KAFKA_TOPICS.PROGRAM_DATA]: programData,
-    [KAFKA_TOPICS.PROGRAM_ALL]: programAll,
-    [KAFKA_TOPICS.PROGRAM_META_ADD]: programMetaAdd,
-    [KAFKA_TOPICS.PROGRAM_META_GET]: programMetaGet,
-    [KAFKA_TOPICS.PROGRAM_ALL_USER]: programAllUsers,
-    [KAFKA_TOPICS.MESSAGE_ALL]: messageAll,
-    [KAFKA_TOPICS.MESSAGE_DATA]: messageData,
-    [KAFKA_TOPICS.MESSAGE_ADD_PAYLOAD]: messageAddPayload,
-    [KAFKA_TOPICS.TEST_BALANCE_GET]: testBalance,
-  };
-
-  const rpcMethod = getRpcMethod[method];
+  const rpcMethod = rpcMethods[method];
 
   return rpcMethod(params);
 }
