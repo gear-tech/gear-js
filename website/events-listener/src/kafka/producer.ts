1b2785fd9c515ab5c3202b4e1407528d04d1518a
Almaz Pasonov <atunstyle@gmail.com>
2022-07-04 18:33:24 +0600

    Update and refactor kafka in @gear-js/event-listener (#785)

--- website/events-listener/src/kafka/producer.ts
+++ website/events-listener/src/kafka/producer.ts
@@ -1,71 +1,63 @@
-import { kafkaLogger } from '@gear-js/common';
-
-import { Kafka, Producer } from 'kafkajs';
-import config from '../config/configuration';
+import { Admin, Producer } from 'kafkajs';
 import { eventListenerLogger } from '../common/event-listener.logger';
+import { initKafka } from './init-kafka';
 
-export class KafkaProducer {
-  readonly kafka: Kafka;
-  readonly producer: Producer;
+const producer: Producer = initKafka.producer();
 
-  constructor() {
-    this.kafka = new Kafka({
-      clientId: config.kafka.clientId,
-      brokers: config.kafka.brokers,
-      logCreator: kafkaLogger,
-      sasl: {
-        mechanism: 'plain',
-        username: config.kafka.sasl.username,
-        password: config.kafka.sasl.password,
-      },
-    });
-    this.producer = this.kafka.producer();
-  }
+async function connect(): Promise<void> {
+  await producer.connect();
+  eventListenerLogger.info('Producer is connected');
+}
 
-  async createTopic(topic: string) {
-    const admin = this.kafka.admin();
-    try {
-      await admin.connect();
-      eventListenerLogger.info('Kafka producer: Admin is connected');
-    } catch (error) {
-      eventListenerLogger.error(error);
-      eventListenerLogger.error('Admin is not connected');
-      throw error;
-    }
-    try {
-      const topics = await admin.listTopics();
-      if (!topics.includes(topic)) {
-        await admin.createTopics({
-          waitForLeaders: true,
-          topics: [
-            {
-              topic,
-            },
-          ],
-        });
-        eventListenerLogger.info(`Kafka producer: Topic ${topic} created`);
-      } else {
-        eventListenerLogger.warn(`Kafka producer: Topic ${topic} already existed`);
-      }
-    } catch (error) {
-      eventListenerLogger.error(`Kafka producer: ${error}`);
-      await admin.disconnect();
-      eventListenerLogger.info(`Admin is disconnected`);
-      throw error;
-    }
-    await admin.disconnect();
-    eventListenerLogger.info(`Admin is disconnected`);
-  }
+async function send(key: string, value: string, genesis: string): Promise<void> {
+  await producer.send({
+    topic: 'events',
+    messages: [{ key, value: JSON.stringify(value), headers: { genesis } }],
+  });
+}
 
-  async connect() {
-    await this.producer.connect();
-    eventListenerLogger.info('Producer is connected');
-  }
+async function createTopic(topic: string): Promise<void> {
+  const admin = initKafka.admin();
+  await connectKafkaAdmin(admin);
+  await createKafkaTopic(admin, topic);
+  await admin.disconnect();
+  eventListenerLogger.info(`Admin is disconnected`);
+}
 
-  async send(key: string, value: string, genesis: string) {
-    await this.producer.send({
-      topic: 'events',
-      messages: [{ key, value: JSON.stringify(value), headers: { genesis } }],
-    });
+async function connectKafkaAdmin(admin: Admin): Promise<void> {
+  try {
+    await admin.connect();
+    eventListenerLogger.info('Kafka producer: Admin is connected');
+  } catch (error) {
+    eventListenerLogger.error(error);
+    eventListenerLogger.error('Admin is not connected');
+    throw error;
   }
 }
+
+async function createKafkaTopic(admin: Admin, topic: string): Promise<void> {
+  try {
+    const kafkaTopics = await admin.listTopics();
+    if (!isTopicAlreadyExist(kafkaTopics, topic)) {
+      await admin.createTopics({
+        waitForLeaders: true,
+        topics: [{ topic }],
+      });
+      eventListenerLogger.info(`Kafka producer: Topic ${topic} created`);
+    } else {
+      eventListenerLogger.info(`Kafka producer: Topic ${topic} already existed`);
+    }
+  } catch (error) {
+    eventListenerLogger.error(`Kafka producer: ${error}`);
+    throw error;
+  }
+
+  await admin.disconnect();
+  eventListenerLogger.info(`Admin is disconnected`);
+}
+
+function isTopicAlreadyExist(topics: string[], topic: string): boolean {
+  return topics.includes(topic);
+}
+
+export const kafkaProducer = { connect, send, createTopic };
