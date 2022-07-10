a8f5d64852b91cd8a20ec32ecd14ca0942d3d795
Dmitry Osipov <dmith.osipov@gmail.com>
2022-07-10 12:02:59 +0300

    Method to check that addres belongs to program (#811)

---
+++ api/src/utils/prefixes.ts
@@ -0,0 +1,4 @@
+export const GPROG = 'g::prog::';
+export const GPAGES = 'g::pages::';
+export const GPROG_HEX = Buffer.from(GPROG).toString('hex');
+export const GPAGES_HEX = Buffer.from('g::pages::').toString('hex');
