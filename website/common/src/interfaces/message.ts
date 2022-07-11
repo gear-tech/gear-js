a5fa31a933300614e43db5b6a400260b28cfc775
Almazatun <atunstyle@gmail.com>
2022-07-11 17:52:19 +0600

    Add MESSAGE_TYPE, MESSAGE_READ_STATUS

--- website/common/src/interfaces/message.ts
+++ website/common/src/interfaces/message.ts
@@ -21,4 +21,15 @@ interface IMessagesDispatchedData {
   statuses: { [key: string]: 'Success' | 'Failed' };
 }
 
-export { IMessage, IMessageEnqueuedData, IMessagesDispatchedData };
+enum MESSAGE_TYPE {
+  USER_MESS_SENT = 'UserMessageSent',
+  ENQUEUED = 'Enqueued',
+}
+
+enum MESSAGE_READ_STATUS {
+  OUT_OF_RENT = 'OutOfRent',
+  CLAIMED = 'Claimed',
+  REPLIED = 'Replied',
+}
+
+export { IMessage, IMessageEnqueuedData, IMessagesDispatchedData, MESSAGE_TYPE, MESSAGE_READ_STATUS };
