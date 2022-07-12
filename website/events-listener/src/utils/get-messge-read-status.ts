f52434bf2193de7d237f4cc5a9cc3a7709e8e5bf
Almazatun <atunstyle@gmail.com>
2022-07-12 14:08:21 +0600

    Refactor getMessageReadStatus and resolve comments

--- website/events-listener/src/utils/get-messge-read-status.ts
+++ website/events-listener/src/utils/get-messge-read-status.ts
@@ -2,18 +2,16 @@ import { UserMessageReadData } from '@gear-js/api';
 import { MESSAGE_READ_STATUS } from '@gear-js/common';
 
 function getMessageReadStatus(data: UserMessageReadData): MESSAGE_READ_STATUS | null {
-  let status: MESSAGE_READ_STATUS | null = null;
-
   if (data.reason.isSystem && data.reason.asSystem.isOutOfRent) {
-    status = MESSAGE_READ_STATUS.OUT_OF_RENT;
+    return MESSAGE_READ_STATUS.OUT_OF_RENT;
   }
   if (data.reason.isRuntime && data.reason.asRuntime.isMessageClaimed) {
-    status = MESSAGE_READ_STATUS.CLAIMED;
+    return MESSAGE_READ_STATUS.CLAIMED;
   }
   if (data.reason.isRuntime && data.reason.asRuntime.isMessageReplied) {
-    status = MESSAGE_READ_STATUS.REPLIED;
+    return MESSAGE_READ_STATUS.REPLIED;
   }
-  return status;
+  return null;
 }
 
 export { getMessageReadStatus };
