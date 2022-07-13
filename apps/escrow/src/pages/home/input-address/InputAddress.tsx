a8f7354a8f7e12074d81d883d1f480ba7a54deaa
Nikita Yutanov <nikitayutanov@gmail.com>
2022-07-13 13:17:00 +0300

    Add program existence check on id submit

--- apps/escrow/src/pages/home/input-address/InputAddress.tsx
+++ apps/escrow/src/pages/home/input-address/InputAddress.tsx
@@ -1,4 +1,5 @@
 import { Hex } from '@gear-js/api';
+import { useApi } from '@gear-js/react-hooks';
 import { Input, Button } from '@gear-js/ui';
 import { useForm } from '@mantine/form';
 import { isHex } from '@polkadot/util';
@@ -13,11 +14,25 @@ const initialValues = { address: '' as Hex };
 const validate = { address: (value: string) => (!isHex(value) ? 'Address should be hex' : null) };
 
 function InputAddress({ label, onSubmit }: Props) {
+  const { api } = useApi();
+
   const form = useForm({ initialValues, validate });
-  const { getInputProps, errors } = form;
+  const { getInputProps, errors, setFieldError } = form;
+
+  const handleProgramSubmit = async (address: Hex) => {
+    const isProgram = await api.program.is(address);
+
+    if (isProgram) {
+      onSubmit(address);
+    } else {
+      setFieldError('address', 'Program is not found in the storage');
+    }
+  };
+
+  const submitHandler = label === 'Program address' ? handleProgramSubmit : onSubmit;
 
   return (
-    <form onSubmit={form.onSubmit(({ address }) => onSubmit(address))}>
+    <form onSubmit={form.onSubmit(({ address }) => submitHandler(address))}>
       <div>
         <Input label={label} {...getInputProps('address')} />
         <p className={styles.error}>{errors.address}</p>
