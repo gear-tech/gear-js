3bd861aa04b8f379f79587208f11d26725a65e83
Almazatun <atunstyle@gmail.com>
2022-07-15 13:15:27 +0600

    Add gearService

--- website/events-listener/src/gear/listener.ts
+++ website/events-listener/src/gear/listener.ts
@@ -4,13 +4,13 @@ import { GEAR_EVENT } from '@gear-js/common';
 
 import { eventListenerLogger } from '../common/event-listener.logger';
 import { handleEvent } from './event-handlers';
-import { updateSignedBlock } from './service';
+import { gearService } from '../services/gear.service';
 
 export const listen = (api: GearApi, genesis: string, callback: (arg: { key: string; value: any }) => void) => {
   return api.query.system.events(async (events) => {
     const blockHash = events.createdAtHash!.toHex();
 
-    const [timestamp, signedBlock, extrinsicStatus] = await Promise.all([
+    const [signerBlockTimestamp, signedBlock, extrinsicStatus] = await Promise.all([
       api.blocks.getBlockTimestamp(blockHash!),
       api.blocks.get(blockHash),
       api.createType('ExtrinsicStatus', { finalized: blockHash }),
@@ -19,19 +19,21 @@ export const listen = (api: GearApi, genesis: string, callback: (arg: { key: str
     const base = {
       genesis,
       blockHash,
-      timestamp: timestamp.toNumber(),
+      timestamp: signerBlockTimestamp.toNumber(),
     };
 
-    events.forEach(async ({ event: { data, method } }) => {
+    for (const {
+      event: { data, method },
+    } of events) {
       try {
-        const eventData = await handleEvent(method as GEAR_EVENT, data as GenericEventData);
+        const eventData = handleEvent(method as GEAR_EVENT, data as GenericEventData);
         eventData !== null && callback({ key: eventData.key, value: { ...eventData.value, ...base } });
       } catch (error) {
         eventListenerLogger.error({ method, data: data.toHuman() });
         eventListenerLogger.error(error);
       }
-    });
+    }
 
-    await updateSignedBlock<typeof extrinsicStatus>(signedBlock, events, extrinsicStatus);
+    await gearService.updateMessagePayload<typeof extrinsicStatus>(signedBlock, events, extrinsicStatus);
   });
 };
