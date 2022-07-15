bc7140bf79339715c9e81a6ba55e6f701570fc1f
Almazatun <atunstyle@gmail.com>
2022-07-15 19:15:20 +0600

    Update KAFKA_TOPICS

--- website/common/src/enums/kafka-topics.enum.ts
+++ website/common/src/enums/kafka-topics.enum.ts
@@ -1,5 +1,4 @@
 enum KAFKA_TOPICS {
-  EVENTS = 'events',
   PROGRAM_DATA = 'program.data',
   PROGRAM_META_ADD = 'program.meta.add',
   PROGRAM_META_GET = 'program.meta.get',
@@ -7,7 +6,6 @@ enum KAFKA_TOPICS {
   PROGRAM_ALL_USER = 'program.all.user',
   MESSAGE_ALL = 'message.all',
   MESSAGE_DATA = 'message.data',
-  MESSAGE_UPDATE_DATA = 'message.update.data',
   TEST_BALANCE_GET = 'testBalance.get',
   CODE_ALL = 'code.all',
   CODE_DATA = 'code.data',
--- website/events-listener/src/gear/types/index.ts
+++
@@ -1,7 +0,0 @@
-import { GearApi } from '@gear-js/api';
-
-let api: GearApi;
-
-type GearSignedBlock = Awaited<ReturnType<typeof api.blocks.get>>;
-
-export { GearSignedBlock };
