793fdc0c3f48f7e5211b8ea2384dfef2ed0c9dcf
Dmitry Osipov <dmith.osipov@gmail.com>
2022-07-18 18:58:06 +0300

    read state using new allocation logic

--- api/src/Code.ts
+++ api/src/Code.ts
@@ -4,7 +4,7 @@ import { Bytes, Option } from '@polkadot/types';
 
 import { GearTransaction } from './Transaction';
 import { generateCodeHash, validateCodeId } from './utils';
-import { CodeMetadata, Hex } from './types';
+import { CodeMetadata, CodeStorage, Hex } from './types';
 export class GearCode extends GearTransaction {
   /**
    * Submit code without initialization
@@ -23,5 +23,14 @@ export class GearCode extends GearTransaction {
   async exists(codeId: string) {
     const codeMetadata = await this.api.query.gearProgram.metadataStorage(codeId) as Option<CodeMetadata>;
     return codeMetadata.isSome;
+  }
+
+  async storage(codeId: Hex): Promise<CodeStorage> {
+    return this.api.query.gearProgram.codeStorage(codeId) as unknown as CodeStorage;
+  }
+
+  async staticPages(codeId: Hex): Promise<number | null> {
+    const storage = await this.storage(codeId);
+    return storage.isSome ? storage.unwrap().staticPages.toNumber() : null;
   }
 }
