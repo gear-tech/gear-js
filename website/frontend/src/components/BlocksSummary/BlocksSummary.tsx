a86fdaea84fc13e186b5098b6e4cbebf6115c1fe
Timur <hiddendesu@gmail.com>
2022-07-19 20:37:37 +0300

    Add tooltip for total issues (#837)

--- website/frontend/src/components/BlocksSummary/BlocksSummary.tsx
+++ website/frontend/src/components/BlocksSummary/BlocksSummary.tsx
@@ -1,63 +1,71 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useApi } from '@gear-js/react-hooks';
 
 import styles from './BlocksSummary.module.scss';
 
 import { useBlocks } from 'hooks';
+import { Tooltip } from 'components/common/Tooltip';
+import { ReactComponent as QuestionSVG } from 'assets/images/circleQuestion.svg';
 
 const BlocksSummary = () => {
   const { api } = useApi();
   const blocks = useBlocks();
 
+  const prevBlockHash = useRef('');
+
   const [timeInstance, setTimeInstance] = useState(0);
   const [totalIssuance, setTotalIssuance] = useState('');
-  const [prevBlockHash, setPrevBlockHash] = useState('');
 
-  const seconds = timeInstance.toFixed(1).slice(0, 1);
-  const milliseconds = timeInstance.toFixed(1).slice(-1);
-  const time = `${seconds}.${milliseconds}`;
+  useEffect(() => {
+    api.totalIssuance().then((result) => setTotalIssuance(result.slice(0, 5)));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const decreasedTime = timeInstance + 0.1;
-      setTimeInstance(decreasedTime);
+      setTimeInstance((prevState) => prevState + 0.1);
     }, 100);
-
-    if (blocks && blocks.length) {
-      if (blocks[0].hash !== prevBlockHash) {
-        setTimeInstance(0);
-      }
-      setPrevBlockHash(blocks[0].hash);
-    }
 
     return () => {
       clearInterval(intervalId);
     };
-  }, [setTimeInstance, timeInstance, setPrevBlockHash, prevBlockHash, blocks]);
+  }, []);
 
   useEffect(() => {
-    const getTotal = async () => {
-      if (api) {
-        const totalBalance = await api.totalIssuance();
-        setTotalIssuance(totalBalance);
-      }
-    };
-    getTotal();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [totalIssuance]);
+    if (!blocks || !blocks.length) {
+      return;
+    }
+
+    const lastBlockHash = blocks[0].hash;
+
+    if (lastBlockHash !== prevBlockHash.current) {
+      setTimeInstance(0);
+    }
+
+    prevBlockHash.current = lastBlockHash;
+  }, [blocks]);
 
   return (
     <div className={styles.summary}>
       <div className={styles.section}>
         <p>Last block</p>
         <p className={styles.data}>
-          <span className={styles.number}>{time}</span> s
+          <span className={styles.number}>{timeInstance.toFixed(1)}</span> s
         </p>
       </div>
       <div className={styles.section}>
         <p>Total issuance</p>
         <p className={styles.data}>
-          <span className={styles.number}>{totalIssuance.slice(0, 5)}</span> Munit
+          <span className={styles.number}>{totalIssuance}</span>
+          <span>MUnit</span>
+          <Tooltip
+            width={245}
+            content="Total issuance balance MUnits"
+            className={styles.question}
+            contentClassName={styles.questionTooltip}
+          >
+            <QuestionSVG />
+          </Tooltip>
         </p>
       </div>
     </div>
---
+++ website/frontend/src/components/common/Tooltip/Tooltip.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import clsx from 'clsx';
+
+import styles from './Tooltip.module.scss';
+
+type Props = {
+  width?: number;
+  content: ReactNode;
+  children: ReactNode;
+  className?: string;
+  contentClassName?: string;
+};
+
+const Tooltip = ({ width = 160, content, children, className, contentClassName }: Props) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const hideTooltip = () => setIsVisible(false);
+  const showTooltip = () => setIsVisible(true);
+
+  return (
+    <div
+      className={clsx(styles.tooltip, className)}
+      onBlur={hideTooltip}
+      onFocus={showTooltip}
+      onMouseOver={showTooltip}
+      onMouseLeave={hideTooltip}
+    >
+      {children}
+      <CSSTransition in={isVisible} timeout={200} unmountOnExit>
+        <div style={{ width }} className={clsx(styles.content, contentClassName)}>
+          {content}
+        </div>
+      </CSSTransition>
+    </div>
+  );
+};
+
+export { Tooltip };
