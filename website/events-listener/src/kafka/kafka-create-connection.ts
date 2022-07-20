219bc2a533af0eac20b110e8eac8771e812445bb
Almazatun <atunstyle@gmail.com>
2022-07-20 12:10:17 +0600

    Update kafka topics

--- website/events-listener/src/kafka/kafka-create-connection.ts
+++ website/events-listener/src/kafka/kafka-create-connection.ts
@@ -2,7 +2,7 @@ import { API_METHODS } from '@gear-js/common';
 
 import { kafkaProducer } from './producer';
 
-const topics: string[] = [API_METHODS.EVENTS, API_METHODS.MESSAGE_UPDATE_DATA];
+const topics: string[] = [API_METHODS.EVENTS, API_METHODS.MESSAGES_UPDATE_DATA];
 
 async function kafkaCreateConnection(): Promise<void> {
   await kafkaProducer.createTopics(topics);
