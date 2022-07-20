52ba45f0f59285a347a6d5354021e420828ed2b8
Dmitry Osipov <dmith.osipov@gmail.com>
2022-07-20 14:35:35 +0300

    Update `reply` field in data of `UserMessageSent` event (#848)

--- api/src/errors/message.errors.ts
+++ api/src/errors/message.errors.ts
@@ -2,13 +2,13 @@ export class SendMessageError extends Error {
   name = 'SendMessageError';
 
   constructor(message?: string) {
-    super(message || "Can't send message. Params are invalid");
+    super(message || 'Can\'t send message. Params are invalid');
   }
 }
 
 export class SendReplyError extends Error {
   name = 'SendReplyError';
   constructor(message?: string) {
-    super(message || "Can't send reply. Params are invalid");
+    super(message || 'Can\'t send reply. Params are invalid');
   }
 }
