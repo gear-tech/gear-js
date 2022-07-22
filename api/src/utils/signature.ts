166d45a8b9b84233ca3f055aa27bc56366a21089
Dmitry Osipov <dmith.osipov@gmail.com>
2022-07-22 11:21:40 +0300

    resolve comment

--- api/src/utils/signature.ts
+++ api/src/utils/signature.ts
@@ -1,9 +1,5 @@
 import { signatureVerify } from '@polkadot/util-crypto';
 
 export function signatureIsValid(publicKey: string, signature: string, message: string) {
-  if (signatureVerify(message, signature, publicKey).isValid) {
-    return true;
-  } else {
-    return false;
-  }
+  return signatureVerify(message, signature, publicKey).isValid;
 }
