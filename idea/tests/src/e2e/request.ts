c2c55513856b8108e18ddd9a31338aa0b5820ab6
Dmitry Osipov <dmith.osipov@gmail.com>
2022-07-25 14:23:30 +0300

    Refactor repository structure (#874)

---
+++ idea/tests/src/e2e/request.ts
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch';
+import base from '../config/base';
+
+export default async function (method: string, params: any) {
+  const response = await fetch(base.gear.api, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      jsonrpc: '2.0',
+      id: Math.floor(Math.random() * 100),
+      method,
+      params,
+    }),
+  });
+  const responseData: any = await response.json();
+  return responseData;
+}
