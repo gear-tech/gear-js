aee865d675107e4d5598294cabb9d3736f3943c7
Almazatun <atunstyle@gmail.com>
2022-07-30 00:22:55 +0600

    update kafkaCreateConnection and consumer features

--- idea/test-balance/src/kafka/consumer.ts
+++ idea/test-balance/src/kafka/consumer.ts
@@ -1,5 +1,5 @@
 import { Consumer, KafkaMessage } from 'kafkajs';
-import { initLogger, JSONRPC_ERRORS } from '@gear-js/common';
+import { initLogger, JSONRPC_ERRORS, KAFKA_TOPICS } from '@gear-js/common';
 
 import config from '../config/configuration';
 import { initKafka } from './init-kafka';
@@ -11,9 +11,9 @@ const consumer: Consumer = initKafka.consumer({ groupId: config.kafka.groupId })
 
 const logger = initLogger('KafkaConsumer');
 
-async function subscribeConsumerTopic(topic: string): Promise<void> {
-  await consumer.subscribe({ topic });
-  logger.info(`Subscribe to ${topic} topic`);
+async function subscribeConsumerTopics(topics: string[]): Promise<void> {
+  const promises = topics.map((topic) => consumer.subscribe({ topic }));
+  await Promise.all(promises);
 }
 
 async function connect(): Promise<void> {
@@ -28,9 +28,7 @@ async function run(): Promise<void> {
   });
 }
 
-async function messageProcessing(message: KafkaMessage) {
-  let result;
-
+async function messageProcessing(message: KafkaMessage): Promise<void | { error: string }> {
   const { payload, error } = await getPayloadFromMessage(message);
 
   if (error) {
@@ -38,24 +36,43 @@ async function messageProcessing(message: KafkaMessage) {
   }
 
   if (payload.genesis === gearService.getGenesisHash()) {
-    try {
-      const isPossibleToTransfer = await transferService.isPossibleToTransfer(payload.address, payload.genesis);
-      if (isPossibleToTransfer) {
-        const transferBalance = await gearService.transferBalance(payload.address);
-        result = { result: transferBalance };
-      } else {
-        result = { error: JSONRPC_ERRORS.TransferLimitReached.name };
-      }
-    } catch (error) {
-      logger.error(error.message, error.stack);
-      result = { error: JSONRPC_ERRORS.InternalError.name };
-    }
-    await sendReply(message, JSON.stringify(result));
+    await getGenesisHashByPayload(message, payload);
+    return;
   }
+
+  await apiGenesisHash();
 }
 
-async function sendReply(message: any, value: string) {
-  await kafkaProducer.send(message, value);
+async function getGenesisHashByPayload(message: KafkaMessage, payload: any): Promise<void> {
+  let result;
+
+  try {
+    const isPossibleToTransfer = await transferService.isPossibleToTransfer(payload.address, payload.genesis);
+    if (isPossibleToTransfer) {
+      const transferBalance = await gearService.transferBalance(payload.address);
+      result = { result: transferBalance };
+    } else {
+      result = { error: JSONRPC_ERRORS.TransferLimitReached.name };
+    }
+  } catch (error) {
+    logger.error(error.message, error.stack);
+    result = { error: JSONRPC_ERRORS.InternalError.name };
+  }
+
+  await sendReply(message, JSON.stringify(result));
+}
+
+async function sendReply(message: KafkaMessage, value: string): Promise<void> {
+  const topic = message.headers.kafka_replyTopic.toString();
+  const correlationId = message.headers.kafka_correlationId.toString();
+
+  await kafkaProducer.send(topic, value, correlationId);
+}
+
+async function apiGenesisHash(): Promise<void> {
+  const genesisHash = gearService.getGenesisHash();
+
+  await kafkaProducer.send(`${KAFKA_TOPICS.TEST_BALANCE_GET_API}.reply`, genesisHash);
 }
 
 async function getPayloadFromMessage(message: KafkaMessage): Promise<{ error: string; payload: any }> {
@@ -72,4 +89,4 @@ async function getPayloadFromMessage(message: KafkaMessage): Promise<{ error: st
   return result;
 }
 
-export const kafkaConsumer = { subscribeConsumerTopic, connect, run };
+export const kafkaConsumer = { subscribeConsumerTopics, connect, run };
