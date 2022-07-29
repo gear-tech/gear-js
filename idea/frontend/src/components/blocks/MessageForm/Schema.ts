74c641e2a27e2e428a567da0c1321f2113c06c9c
Timur Rozhkov <hiddendesu@gmail.com>
2022-07-29 13:43:22 +0300

    refactor validation schema

--- idea/frontend/src/components/blocks/MessageForm/Schema.ts
+++ idea/frontend/src/components/blocks/MessageForm/Schema.ts
@@ -1,11 +1,15 @@
 import * as yup from 'yup';
 import { Metadata } from '@gear-js/api';
 
-export const getValidationSchema = (type?: string, metadata?: Metadata) =>
+export const getValidationSchema = (deposit: number, type?: string, metadata?: Metadata) =>
   yup.object().shape({
-    value: yup.number().required('This field is required').min(0, 'Initial value should be more or equal than 0'),
+    value: yup
+      .number()
+      .required('This field is required')
+      .min(0, `Initial value should be more ${deposit} or equal than 0`)
+      .moreThan(deposit, `Initial value should be more ${deposit} or equal than 0`),
     // @ts-ignore
     payload: yup.mixed().default('').testPayload(type, metadata),
-    gasLimit: yup.number().required('This field is required').min(1, 'Initial value should be more than 0'),
+    gasLimit: yup.number().required('This field is required').min(1, 'Gas limit be more than 0'),
     payloadType: yup.string().required('This field is required'),
   });
--- idea/frontend/src/pages/Programs/children/Upload/children/UploadForm/Schema.ts
+++ idea/frontend/src/pages/Programs/children/Upload/children/UploadForm/Schema.ts
@@ -1,12 +1,16 @@
 import * as yup from 'yup';
 import { Metadata } from '@gear-js/api';
 
-export const getValidationSchema = (type?: string, metadata?: Metadata) =>
+export const getValidationSchema = (deposit: number, type?: string, metadata?: Metadata) =>
   yup.object().shape({
-    value: yup.number().required('This field is required').min(0, 'Initial value should be more or equal than 0'),
+    value: yup
+      .number()
+      .required('This field is required')
+      .min(0, `Initial value should be more ${deposit} or equal than 0`)
+      .moreThan(deposit, `Initial value should be more ${deposit} or equal than 0`),
     // @ts-ignore
     payload: yup.mixed().default('').testPayload(type, metadata),
-    gasLimit: yup.number().min(0, 'Initial value should be more than 0'),
+    gasLimit: yup.number().required('This field is required').min(1, 'Gas limit should be more than 0'),
     programName: yup.string().max(50, 'Name value should be less than 50'),
     payloadType: yup.string().required('This field is required'),
   });
