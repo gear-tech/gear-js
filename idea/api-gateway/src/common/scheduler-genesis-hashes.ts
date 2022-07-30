9dbe52d00e828e0cfc05fd2a5a3cca43cdd28b13
Almazatun <atunstyle@gmail.com>
2022-07-30 23:36:21 +0600

    update schedulerGenesisHashes

--- idea/api-gateway/src/common/scheduler-genesis-hashes.ts
+++ idea/api-gateway/src/common/scheduler-genesis-hashes.ts
@@ -4,25 +4,35 @@ import { KAFKA_TOPICS } from '@gear-js/common';
 import { kafkaProducer } from '../kafka/producer';
 import { genesisHashMap } from './genesis-hash-map';
 
+let cron: CronJob;
 
 function schedulerGenesisHashes(){
   return {
     start() {
-      new CronJob(getCronRunTime(), async function (){
+      cron = new CronJob(getCronRunTime(), async function (){
         genesisHashMap.clear();
         await kafkaProducer.sendByTopic(KAFKA_TOPICS.TEST_BALANCE_SERVICES, 'testBalanceServices');
-      }, null, true, '');
+      });
+
+      if (process.env.TEST_ENV){
+        const TEN_SECONDS = 10_000;
+        setTimeout(() => {
+          cron.stop();
+        }, TEN_SECONDS);
+      }
+
+      cron.start();
     }
   };
 }
 
 function getCronRunTime(): string {
   if (process.env.TEST_ENV){
-    // every 10 seconds
-    return '*/10 * * * * *';
+    // every 50 seconds
+    return '*/50 * * * * *';
   }
 
-  // every three hours
+  // every 3 hours
   return '0 */3 * * *';
 }
 
