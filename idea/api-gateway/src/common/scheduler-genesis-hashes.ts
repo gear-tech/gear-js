7329d6c3d5b73b8232318ce7bc4403d387920957
Almazatun <atunstyle@gmail.com>
2022-08-01 12:45:04 +0600

    update schedulerGenesisHashes

--- idea/api-gateway/src/common/scheduler-genesis-hashes.ts
+++ idea/api-gateway/src/common/scheduler-genesis-hashes.ts
@@ -4,34 +4,27 @@ import { KAFKA_TOPICS } from '@gear-js/common';
 import { kafkaProducer } from '../kafka/producer';
 import { genesisHashMap } from './genesis-hash-map';
 
-let cron: CronJob;
-
 function schedulerGenesisHashes(){
   return {
     start() {
-      cron = new CronJob(getCronRunTime(), async function (){
-        genesisHashMap.clear();
-        await kafkaProducer.sendByTopic(KAFKA_TOPICS.TEST_BALANCE_SERVICES, 'testBalanceServices');
-      });
+      if (!process.env.TEST_ENV) {
+        new CronJob(getCronRunTime(), async function () {
+          genesisHashMap.clear();
+          await kafkaProducer.sendByTopic(KAFKA_TOPICS.TEST_BALANCE_SERVICES, 'testBalanceServices');
+        }, null, true, '');
+      }
 
       if (process.env.TEST_ENV){
         const SEVENTY_SECONDS = 70 * 1000;
-        setTimeout(() => {
-          cron.stop();
+        setTimeout(async () => {
+          await kafkaProducer.sendByTopic(KAFKA_TOPICS.TEST_BALANCE_SERVICES, 'testBalanceServices');
         }, SEVENTY_SECONDS);
       }
-
-      cron.start();
     }
   };
 }
 
 function getCronRunTime(): string {
-  if (process.env.TEST_ENV){
-    // every 1 min
-    return '* * * * *';
-  }
-
   // every 3 hours
   return '0 */3 * * *';
 }
