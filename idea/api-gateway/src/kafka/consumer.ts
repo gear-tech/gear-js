8842991c86dc1146d88a43ce40a1c14478a8fc6c
Almazatun <atunstyle@gmail.com>
2022-08-03 10:49:07 +0600

    add messageProcessing

--- idea/api-gateway/src/kafka/consumer.ts
+++ idea/api-gateway/src/kafka/consumer.ts
@@ -1,4 +1,5 @@
 import { KAFKA_TOPICS } from '@gear-js/common';
+import { KafkaMessage } from 'kafkajs';
 
 import config from '../config/configuration';
 import { initKafka } from './init-kafka';
@@ -15,15 +16,7 @@ async function connect(): Promise<void> {
 async function run(): Promise<void> {
   await consumer.run({
     eachMessage: async ({ message, topic }) => {
-      if (topic !== `${KAFKA_TOPICS.TEST_BALANCE_GENESIS_HASH_API}.reply`) {
-        const correlationId = message.headers.kafka_correlationId.toString();
-        const resultFromService = kafkaEventMap.get(correlationId);
-        if (resultFromService) await resultFromService(JSON.parse(message.value.toString()));
-        deleteKafkaEvent(correlationId);
-      } else {
-        const genesisHash = message.value.toString();
-        genesisHashesCollection.add(genesisHash);
-      }
+      await messageProcessing(message, topic);
     },
   });
 }
@@ -36,6 +29,18 @@ async function subscribeConsumerTopics(topics: string[]): Promise<void> {
     }),
   );
   await Promise.all(promises);
+}
+
+async function messageProcessing(message: KafkaMessage, topic: string): Promise<void> {
+  if (topic !== `${KAFKA_TOPICS.TEST_BALANCE_GENESIS_HASH_API}.reply`) {
+    const correlationId = message.headers.kafka_correlationId.toString();
+    const resultFromService = kafkaEventMap.get(correlationId);
+    if (resultFromService) await resultFromService(JSON.parse(message.value.toString()));
+    deleteKafkaEvent(correlationId);
+  } else {
+    const genesisHash = message.value.toString();
+    genesisHashesCollection.add(genesisHash);
+  }
 }
 
 export const kafkaConsumer = { subscribeConsumerTopics, connect, run };
