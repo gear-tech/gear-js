e68839e4caf261e092bf55a7bf7069916cfd919b
Timur <hiddendesu@gmail.com>
2022-08-15 19:01:21 +0300

    Refactor the gas validation (#923)

--- idea/frontend/src/components/blocks/MessageForm/Schema.ts
+++ idea/frontend/src/components/blocks/MessageForm/Schema.ts
@@ -13,7 +13,7 @@ export const getValidationSchema = ({ type, deposit, metadata, maxGasLimit }: Pa
     gasLimit: yup
       .number()
       .required('This field is required')
-      .min(1, 'Gas limit should be more than 0')
+      .min(0, 'Gas limit should be more or equal than 0')
       .max(maxGasLimit, `Gas limit should be less than ${maxGasLimit}`),
     payloadType: yup.string().required('This field is required'),
   });
