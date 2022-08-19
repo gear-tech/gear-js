7cc7aa426619bf8d4b996b4a31cfbe9818f30c21
Nikita Yutanov <nikitayutanov@gmail.com>
2022-08-19 20:20:35 +0300

    Bump apps api

--- apps/escrow/src/hooks/program/useUploadProgram.ts
+++ apps/escrow/src/hooks/program/useUploadProgram.ts
@@ -102,7 +102,14 @@ function useUploadProgram(onSuccess: (programId: Hex) => void) {
       const initPayload = { ftProgramId };
       const value = 0;
 
-      const gas = await api.program.calculateGas.init(decodedAddress, buffer, initPayload, value, false, metadata);
+      const gas = await api.program.calculateGas.initUpload(
+        decodedAddress,
+        buffer,
+        initPayload,
+        value,
+        false,
+        metadata,
+      );
       const gasLimit = gas.min_limit;
 
       const alertId = alert.loading('SignIn', { title: 'gear.submitProgram' });
