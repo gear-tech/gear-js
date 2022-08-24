5443d3f29391c41879971fffde6d0b10aa2d08c9
Timur <hiddendesu@gmail.com>
2022-08-24 12:37:57 +0300

    Show node version (#956)

--- idea/frontend/src/components/blocks/Header/Header.tsx
+++ idea/frontend/src/components/blocks/Header/Header.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import styles from './Header.module.scss';
 import { Wallet } from '../Wallet/Wallet';
-import { useAccount } from '@gear-js/react-hooks';
+import { useApi, useAccount } from '@gear-js/react-hooks';
 
 import { Logo } from './children/Logo/Logo';
 import { Menu } from './children/Menu/Menu';
@@ -13,8 +13,10 @@ import { useSidebarNodes } from 'hooks';
 
 const Header = () => {
   const { account } = useAccount();
+  const { api, isApiReady } = useApi();
   const sidebarNodes = useSidebarNodes();
 
+  const [nodeVersion, setNodeVersion] = useState('');
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const openSidebar = () => {
@@ -25,12 +27,35 @@ const Header = () => {
     setIsSidebarOpen(false);
   };
 
+  const chain = api?.runtimeChain.toHuman();
+  const specName = api?.runtimeVersion.specName.toHuman();
+  const specVersion = api?.runtimeVersion.specVersion.toHuman();
+
+  useEffect(() => {
+    if (api) {
+      api.nodeVersion().then((result) => {
+        const version = result.split('-')[1];
+
+        if (version && version !== 'unknown') {
+          setNodeVersion(version);
+        }
+      });
+    }
+  }, [api]);
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
         <Logo />
         <hr className={styles.separator} />
-        <Menu openSidebar={openSidebar} />
+        <Menu
+          chain={chain}
+          name={specName}
+          version={specVersion}
+          isApiReady={isApiReady}
+          nodeVersion={nodeVersion}
+          openSidebar={openSidebar}
+        />
       </nav>
       <div className={styles.rightSide}>
         {account && <TestBalance address={account.address} />}
