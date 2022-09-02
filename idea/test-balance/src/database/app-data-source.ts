7f8fc305d240c7b865bec5d6589c8cc087f479e4
Almaz Pasonov <atunstyle@gmail.com>
2022-09-02 16:18:37 +0600

    Update `@gear-js/test-balance` db connection (#966)

---
+++ idea/test-balance/src/database/app-data-source.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+
+import config from '../config/configuration';
+import { TransferBalance } from './entities/transfer.entity';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: config.db.host,
+  port: Number(config.db.port) || 5432,
+  username: config.db.user,
+  password: config.db.password,
+  database: config.db.name,
+  synchronize: true,
+  logging: true,
+  entities: [TransferBalance],
+  migrations: [],
+});
