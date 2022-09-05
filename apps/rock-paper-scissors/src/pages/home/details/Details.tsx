cb3a606d13ae990b1ab1385be7f319c0354ace04
Nikita Yutanov <nikitayutanov@gmail.com>
2022-09-06 00:03:14 +0300

    Init details component

---
+++ apps/rock-paper-scissors/src/pages/home/details/Details.tsx
@@ -0,0 +1,12 @@
+import { BackButton } from 'components';
+import styles from './Details.module.scss';
+
+function Details() {
+  return (
+    <div className={styles.container}>
+      <BackButton />
+    </div>
+  );
+}
+
+export { Details };
