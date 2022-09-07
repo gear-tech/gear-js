71bb4144413006fde3b2e2285303f0bdbc029acc
Timur Rozhkov <hiddendesu@gmail.com>
2022-09-08 01:14:01 +0300

    refactor recent blocks

--- idea/new-frontend/src/features/recentBlocks/helpers/index.ts
+++ idea/new-frontend/src/features/recentBlocks/helpers/index.ts
@@ -1,3 +1,13 @@
+import { CSSProperties } from 'react';
+
 const getRandomPercent = () => Math.ceil((Math.random() || 0.01) * 100);
 
-export { getRandomPercent };
+const getMinWidth = (value?: number): CSSProperties => {
+  const length = value ? String(value).length : 5;
+
+  return {
+    minWidth: `${((length + 1) * 10.5) / 16}rem`,
+  };
+};
+
+export { getMinWidth, getRandomPercent };
--- idea/new-frontend/src/features/recentBlocks/ui/RecentBlocks.tsx
+++ idea/new-frontend/src/features/recentBlocks/ui/RecentBlocks.tsx
@@ -9,6 +9,7 @@ import { AnimationTimeout } from 'shared/config';
 import { ReactComponent as ArrowSVG } from 'shared/assets/images/actions/arrowRight.svg';
 
 import styles from './RecentBlocks.module.scss';
+import { getMinWidth } from '../helpers';
 import { Graph } from './graph';
 import { RecentBlocksList } from './recentBlocksList';
 
@@ -51,30 +52,40 @@ const RecentBlocks = () => {
   const time = `${timeInstance.toFixed(1)} s`;
   const blockNumber = `#${block?.number ?? '00000'}`;
 
-  const buttonClasses = clsx(buttonStyles.button, buttonStyles.transparent, buttonStyles.noText, styles.blocksBtn);
+  const buttonClasses = clsx(
+    buttonStyles.button,
+    buttonStyles.transparent,
+    buttonStyles.noText,
+    styles.blocksBtn,
+    isOpen && styles.rotated,
+  );
 
   return (
-    <CSSTransition in={isOpen} timeout={AnimationTimeout.Default}>
-      <section ref={sectionRef} className={styles.recentBlocks}>
-        <div className={styles.content}>
-          <Graph blocks={blocks} className={styles.graph} />
-          <div className={styles.blockInfo}>
-            <h2 className={styles.title}>Recent block</h2>
-            <p className={styles.indicators}>
-              <span className={styles.value}>{blockNumber}</span>
-              <span className={styles.point} />
-              <span className={styles.time}>{time}</span>
-            </p>
+    <div className={styles.recentBlocksWrapper}>
+      <CSSTransition in={isOpen} timeout={AnimationTimeout.Default}>
+        <section ref={sectionRef} className={styles.recentBlocks}>
+          <div className={styles.content}>
+            <Graph blocks={blocks} className={styles.graph} />
+            <div className={styles.blockInfo}>
+              <h2 className={styles.title}>Recent block</h2>
+              <p className={styles.indicators}>
+                <span style={getMinWidth(block?.number)} className={styles.value}>
+                  {blockNumber}
+                </span>
+                <span className={styles.point} />
+                <span className={styles.time}>{time}</span>
+              </p>
+            </div>
+            <button type="button" className={buttonClasses} onClick={toggleList}>
+              <ArrowSVG />
+            </button>
           </div>
-          <button type="button" className={buttonClasses} onClick={toggleList}>
-            <ArrowSVG />
-          </button>
-        </div>
-        <CSSTransition in={isOpen} timeout={AnimationTimeout.Default} mountOnEnter unmountOnExit>
-          <RecentBlocksList blocks={blocks} className={styles.recentBlocksList} />
-        </CSSTransition>
-      </section>
-    </CSSTransition>
+          <CSSTransition in={isOpen} timeout={AnimationTimeout.Default}>
+            <RecentBlocksList blocks={blocks} className={styles.recentBlocksList} />
+          </CSSTransition>
+        </section>
+      </CSSTransition>
+    </div>
   );
 };
 
