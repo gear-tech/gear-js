2cf47cc29e9ef0a1e484843802f64d172e305211
Almazatun <atunstyle@gmail.com>
2022-09-23 16:50:55 +0300

    add getNewServicePartition

--- idea/api-gateway/src/common/services-partition-map.ts
+++ idea/api-gateway/src/common/services-partition-map.ts
@@ -1,1 +1,38 @@
-export const servicesPartitionMap = new Map<string, string>();
+import { initKafka } from '../kafka/init-kafka';
+
+const servicesPartitionMap = new Map<string, string>();
+
+async function getNewServicePartition (topic: string): Promise<number> {
+  let sumPartitionsInApiGatewayService = 0;
+
+  const admin = initKafka.admin();
+  const topicOffsets = await admin.fetchTopicOffsets(topic);
+
+  const sumPartitionsInKafka = topicOffsets.reduce((acc, topicData) => {
+    return acc + topicData.partition;
+  }, 0 as number);
+
+  for(const el of servicesPartitionMap){
+    const [_, partition] = el;
+    sumPartitionsInApiGatewayService += Number(partition);
+  }
+
+  if(sumPartitionsInApiGatewayService < sumPartitionsInKafka && sumPartitionsInApiGatewayService !== 0) {
+    return sumPartitionsInApiGatewayService + 1;
+  }
+
+  if (sumPartitionsInApiGatewayService === sumPartitionsInKafka){
+    const topics = await admin.listTopics();
+
+    const partitionNewService = sumPartitionsInKafka + 1;
+    const topicPartitions = topics.map(topic => ({ topic, count: partitionNewService }));
+
+    await admin.createPartitions({ topicPartitions });
+
+    return partitionNewService;
+  }
+
+  return 1;
+}
+
+export { servicesPartitionMap, getNewServicePartition };
