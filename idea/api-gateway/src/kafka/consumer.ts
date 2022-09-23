d349003ef9e745b19fa7740418ff12878497c7af
Almazatun <atunstyle@gmail.com>
2022-09-23 16:51:33 +0300

    update kafka consumer

--- idea/api-gateway/src/kafka/consumer.ts
+++ idea/api-gateway/src/kafka/consumer.ts
@@ -1,13 +1,11 @@
-import { API_METHODS, KAFKA_TOPICS } from '@gear-js/common';
+import { KAFKA_TOPICS } from '@gear-js/common';
 import { KafkaMessage } from 'kafkajs';
 
 import config from '../config/configuration';
 import { initKafka } from './init-kafka';
 import { deleteKafkaEvent, kafkaEventMap } from './kafka-event-map';
 import { genesisHashesCollection } from '../common/genesis-hashes-collection';
-import { kafkaProducer } from './producer';
-import { servicesPartitionMap } from '../common/services-partition-map';
-import { getTopicsForPartition } from '../common/helpers';
+import { sendServicePartition, setServicePartition } from '../common/helpers';
 
 const configKafka = config().kafka;
 
@@ -63,46 +61,6 @@ async function messageProcessing(message: KafkaMessage, topic: string): Promise<
     const genesisHash = message.value.toString();
     genesisHashesCollection.add(genesisHash);
   }
-}
-
-async function sendServicePartition(message: KafkaMessage, topic: string): Promise<void>{
-  const serviceGenesis = JSON.parse(message.value.toString());
-  const correlationId = message.headers.kafka_correlationId.toString();
-  const partitionServiceByGenesis = servicesPartitionMap.get(serviceGenesis);
-
-  if(partitionServiceByGenesis) {
-    await kafkaProducer.sendByTopic(KAFKA_TOPICS.SERVICE_PARTITION_GET, partitionServiceByGenesis, correlationId);
-    return;
-  }
-
-  const admin = initKafka.admin();
-  const topicOffsets = await admin.fetchTopicOffsets(topic);
-
-  const partitionService = topicOffsets.reduce((acc, topicData) => {
-    return acc + topicData.partition;
-  }, 0 as number) + 1;
-
-  const apiMethods: string[] = [...Object.values(API_METHODS)];
-
-  const topicPartitions = getTopicsForPartition(apiMethods).map(topic => {
-    return { topic, count: partitionService };
-  });
-
-  await admin.createPartitions({ topicPartitions });
-
-  await servicesPartitionMap.set(serviceGenesis, String(partitionService));
-  await kafkaProducer.sendByTopic(KAFKA_TOPICS.SERVICE_PARTITION_GET, String(partitionService), correlationId);
-}
-
-async function setServicePartition(message: KafkaMessage): Promise<void> {
-  const { partition, genesis } = JSON.parse(message.value.toString());
-
-  const admin = initKafka.admin();
-  const topicOffsets = await admin.fetchTopicOffsets(`${KAFKA_TOPICS.SERVICE_PARTITION_GET}.reply`);
-
-  const topicOffset = topicOffsets.find(topicData => topicData.partition === partition);
-
-  servicesPartitionMap.set(genesis, String(topicOffset.partition));
 }
 
 export const kafkaConsumer = { subscribeConsumerTopics, connect, run };
