33b36ad44c4f33a4e89a5a4c4ef743dad032b8d3
Almazatun <atunstyle@gmail.com>
2022-09-30 16:47:57 +0600

    update getOptAndMetaWasm

--- apps/upload-dapp-chain/src/common/helpers/get-opt-and-meta-wasm.ts
+++ apps/upload-dapp-chain/src/common/helpers/get-opt-and-meta-wasm.ts
@@ -1,14 +1,8 @@
 import fetch, { Response } from "node-fetch";
 
-// eslint-disable-next-line consistent-return
 export function getOptAndMetaWasm(optWasmDownloadUrl: string, metaWasmDownloadUrl:string): Promise<Response[]> {
-  try {
-    return Promise.all([
-      fetch(optWasmDownloadUrl),
-      fetch(metaWasmDownloadUrl),
-    ]);
-  } catch (error) {
-    console.log("________>getOptAndMetaWasm");
-    console.log(error);
-  }
+  return Promise.all([
+    fetch(optWasmDownloadUrl),
+    fetch(metaWasmDownloadUrl),
+  ]);
 }
