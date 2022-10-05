2614ae30bf14c0cc03b4ec5c978f8971d1378fec
Almazatun <atunstyle@gmail.com>
2022-10-05 12:01:59 +0600

    simple refactor

--- idea/data-storage/src/consumer/consumer.controller.ts
+++ idea/data-storage/src/consumer/consumer.controller.ts
@@ -26,63 +26,63 @@ export class ConsumerController {
   @KafkaMessagePartition
   async programData(@Payload() payload: KafkaPayload<FindProgramParams>): Promise<string> {
     const result = await this.consumerService.programData(payload.value);
-    return JSON.stringify({ ...result, partition: payload.partition });
+    return JSON.stringify(result);
   }
 
   @MessagePattern(KAFKA_TOPICS.PROGRAM_ALL)
   @KafkaMessagePartition
   async allPrograms(@Payload() payload: KafkaPayload<GetAllProgramsParams>): Promise<string> {
     const result = await this.consumerService.allPrograms(payload.value);
-    return JSON.stringify({ ...result, partition: payload.partition });
+    return JSON.stringify(result);
   }
 
   @MessagePattern(KAFKA_TOPICS.PROGRAM_ALL_USER)
   @KafkaMessagePartition
   async allUserPrograms(@Payload() payload: KafkaPayload<GetAllUserProgramsParams>): Promise<string> {
     const result = await this.consumerService.allUserPrograms(payload.value);
-    return JSON.stringify({ ...result, partition: payload.partition });
+    return JSON.stringify(result);
   }
 
   @MessagePattern(KAFKA_TOPICS.PROGRAM_META_ADD)
   @KafkaMessagePartition
   async addMeta(@Payload() payload: KafkaPayload<AddMetaParams>): Promise<string> {
     const result = await this.consumerService.addMeta(payload.value);
-    return JSON.stringify({ ...result, partition: payload.partition });
+    return JSON.stringify(result);
   }
 
   @MessagePattern(KAFKA_TOPICS.PROGRAM_META_GET)
   @KafkaMessagePartition
   async getMeta(@Payload() payload: KafkaPayload<GetMetaParams>): Promise<string> {
     const result = await this.consumerService.getMeta(payload.value);
-    return JSON.stringify({ ...result, partition: payload.partition });
+    return JSON.stringify(result);
   }
 
   @MessagePattern(KAFKA_TOPICS.MESSAGE_ALL)
   @KafkaMessagePartition
   async allMessages(@Payload() payload: KafkaPayload<GetMessagesParams>): Promise<string> {
     const result = await this.consumerService.allMessages(payload.value);
-    return JSON.stringify({ ...result, partition: payload.partition });
+    return JSON.stringify(result);
   }
 
   @MessagePattern(KAFKA_TOPICS.MESSAGE_DATA)
   @KafkaMessagePartition
   async messageData(@Payload() payload: KafkaPayload<FindMessageParams>): Promise<string> {
     const result = await this.consumerService.message(payload.value);
-    return JSON.stringify({ ...result, partition: payload.partition });
+    return JSON.stringify(result);
   }
 
   @MessagePattern(KAFKA_TOPICS.CODE_DATA)
   @KafkaMessagePartition
   async codeData(@Payload() payload: KafkaPayload<GetCodeParams>): Promise<string> {
     const result = await this.consumerService.code(payload.value);
-    return JSON.stringify({ ...result, partition: payload.partition });
+    return JSON.stringify(result);
   }
 
   @MessagePattern(KAFKA_TOPICS.CODE_ALL)
   @KafkaMessagePartition
   async allCode(@Payload() payload: KafkaPayload<GetAllCodeParams>): Promise<string> {
     const result = await this.consumerService.allCode(payload.value);
-    return JSON.stringify({ ...result, partition: payload.partition });
+    return JSON.stringify(result);
   }
 
   @MessagePattern(KAFKA_TOPICS.SERVICE_PARTITION_GET)
