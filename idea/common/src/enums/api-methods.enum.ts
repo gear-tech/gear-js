d82a7e800494a977c93ef4a39fcb0dd48202557d
Almazatun <atunstyle@gmail.com>
2022-10-08 22:40:41 +0600

    delete program.all.user from API_METHODS

--- idea/common/src/enums/api-methods.enum.ts
+++ idea/common/src/enums/api-methods.enum.ts
@@ -3,7 +3,6 @@ enum API_METHODS {
   PROGRAM_META_ADD = 'program.meta.add',
   PROGRAM_META_GET = 'program.meta.get',
   PROGRAM_ALL = 'program.all',
-  PROGRAM_ALL_USER = 'program.all.user',
   MESSAGE_ALL = 'message.all',
   MESSAGE_DATA = 'message.data',
   TEST_BALANCE_GET = 'testBalance.get',
