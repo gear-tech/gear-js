a93bc20dc53d8bafed7b23279c87308367cf5a10
Almazatun <atunstyle@gmail.com>
2022-10-21 16:44:41 +0600

    add validateJsonRpcRequest middleware and update files

--- idea/api-gateway/src/middleware/check-genesis.middleware.ts
+++ idea/api-gateway/src/middleware/check-genesis.middleware.ts
@@ -2,25 +2,24 @@ import { Response, Request, NextFunction } from 'express';
 import { IRpcRequest, JSONRPC_ERRORS } from '@gear-js/common';
 
 import { getResponse } from '../utils';
-import { servicesPartitionMap } from '../common/services-partition-map';
 
 export async function checkGenesisMiddleware(req: Request, res: Response, next: NextFunction) {
   const body: IRpcRequest = req.body;
 
   if(Array.isArray(body)) {
     const isExistGenesis = body.every((value) => 'genesis' in value
-      && value.genesis
-      && servicesPartitionMap.has(value.genesis));
+      && value.genesis);
 
     if (!isExistGenesis) {
       res.send(getResponse(body, JSONRPC_ERRORS.InvalidParams.name));
       return;
     }
   } else {
-    if (!('genesis' in body.params && body.params.genesis && servicesPartitionMap.has(body.params.genesis))) {
+    if (!('genesis' in body.params && body.params.genesis)) {
       res.send(getResponse(body, JSONRPC_ERRORS.InvalidParams.name));
       return;
     }
   }
+
   next();
 }
