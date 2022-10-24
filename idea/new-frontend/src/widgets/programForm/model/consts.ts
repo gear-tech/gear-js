fac6fafec519bca48d2d5f04ed6b3a84d064871e
Nikita Yutanov <nikitayutanov@gmail.com>
2022-10-24 19:27:50 +0300

    Update default payload value to 0x

--- idea/new-frontend/src/widgets/programForm/model/consts.ts
+++ idea/new-frontend/src/widgets/programForm/model/consts.ts
@@ -2,7 +2,7 @@ import { FormValues } from './types';
 
 const INITIAL_VALUES: FormValues = {
   value: 0,
-  payload: '0x00',
+  payload: '0x',
   gasLimit: 0,
   programName: '',
   payloadType: 'Bytes',
