4bb435c104153f7bbadb74897c66f004bae4a656
Almazatun <atunstyle@gmail.com>
2022-11-14 21:46:19 +0300

    delete kafka package from api-gateway

--- idea/common/src/enums/amqp-methods.enum.ts
+++ idea/common/src/enums/amqp-methods.enum.ts
@@ -1,4 +1,4 @@
-enum KAFKA_TOPICS {
+enum AMQP_METHODS {
   PROGRAM_DATA = 'program.data',
   PROGRAM_META_ADD = 'program.meta.add',
   PROGRAM_META_GET = 'program.meta.get',
@@ -6,15 +6,14 @@ enum KAFKA_TOPICS {
   MESSAGE_ALL = 'message.all',
   MESSAGE_DATA = 'message.data',
   TEST_BALANCE_GET = 'testBalance.get',
-  TEST_BALANCE_GENESIS = 'testBalance.genesis',
+  TEST_BALANCE_GENESISES = 'testBalance.genesises',
+  DATA_STORAGE_GENESISES = 'dataStorage.genesises',
   TEST_BALANCE_AVAILABLE = 'testBalance.available',
   NETWORK_DATA_AVAILABLE = 'networkData.available',
   CODE_ALL = 'code.all',
   CODE_DATA = 'code.data',
   MESSAGES_UPDATE_DATA = 'messages.update.data',
-  SERVICE_PARTITION_GET = 'service.partition.get',
-  SERVICES_PARTITION = 'services.partition',
   BLOCKS_STATUS = 'blocks.status',
 }
 
-export { KAFKA_TOPICS };
+export { AMQP_METHODS };
