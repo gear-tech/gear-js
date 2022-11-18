31f0d8304d0e65fc684af26857e8745843a51380
Kolko Valentin <valiant9191@gmail.com>
2022-11-18 14:42:33 +0300

    add new types;

--- apps/rock-paper-scissors/src/types/index.ts
+++ apps/rock-paper-scissors/src/types/index.ts
@@ -4,4 +4,25 @@ type StageType = 'preparation' | 'progress' | 'reveal';
 
 type SVGType = FunctionComponent<SVGProps<SVGSVGElement> & { title?: string | undefined }>;
 
-export type { StageType, SVGType };
+type StateConfigType = {
+    Config: {
+        betSize: string;
+        entryTimeoutMs: string;
+        moveTimeoutMs: string;
+        playersCountLimit: string;
+        revealTimeoutMs: string;
+    }   
+} | undefined
+
+type StateTimeStampType = {
+    CurrentStageTimestamp?: string,
+}
+
+type StateGameStageType = {
+    GameStage?: StageType|undefined
+}
+
+type TimeType = {hours:string,minutes:string, seconds:string}
+
+
+export type { StageType, SVGType, StateConfigType, StateTimeStampType, StateGameStageType, TimeType };
