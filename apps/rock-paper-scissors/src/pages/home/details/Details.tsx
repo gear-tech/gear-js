13c1626e88c329e62f5f9cb97827ea62d65fe1c5
Kolko Valentin <valiant9191@gmail.com>
2022-11-18 14:45:43 +0300

    Update Details

--- apps/rock-paper-scissors/src/pages/home/details/Details.tsx
+++ apps/rock-paper-scissors/src/pages/home/details/Details.tsx
@@ -1,5 +1,5 @@
 import { Hex } from '@gear-js/api';
-import { Button } from '@gear-js/ui';
+// import { Button } from '@gear-js/ui';
 import { BackButton, Detail } from 'components';
 import { SVGType } from 'types';
 import styles from './Details.module.scss';
@@ -8,20 +8,23 @@ type Props = {
   heading: string;
   game: string;
   round: string;
-  contract: Hex;
-  players: string;
-  bet: string;
-  entry: string;
-  move: string;
-  reveal: string;
+  contract: Hex | undefined;
+  bet: string | undefined;
+  entry: string | undefined;
+  move: string | undefined;
+  reveal: string | undefined;
   SVG: SVGType;
+  players: Array<Hex>
+  onBackClick: (arg: string) => void;
 };
 
-function Details({ heading, game, round, contract, players, bet, entry, move, reveal, SVG }: Props) {
+
+
+function Details({ onBackClick, heading, game, round, contract, players, bet, entry, move, reveal, SVG }: Props) {
   return (
     <div className={styles.container}>
       <div className={styles.visual}>
-        <BackButton onClick={() => {}} />
+        <BackButton onClick={() => onBackClick('lobby')} />
         <SVG className={styles.svg} />
       </div>
       <div>
@@ -32,15 +35,18 @@ function Details({ heading, game, round, contract, players, bet, entry, move, re
           <Detail label="Contract address" className={styles.contract}>
             <span className={styles.contractText}>{contract}</span>
           </Detail>
-          <Detail label="Players" text={players} className={styles.players} />
+          <Detail label="Players" text={players.length.toString()} className={styles.players} />
           <Detail label="Bet size" className={styles.bet}>
             <span className={styles.betText}>{bet}</span>
           </Detail>
-          <Detail label="Entry timeout" text={entry} className={styles.entry} />
-          <Detail label="Move timeout" text={move} className={styles.move} />
-          <Detail label="Reveal timeout" text={reveal} className={styles.reveal} />
+          {entry && <Detail label="Entry timeout" text={entry} className={styles.entry} />}
+          {move && <Detail label="Move timeout" text={move} className={styles.move} />}
+          {reveal && <Detail label="Reveal timeout" text={reveal} className={styles.reveal} />}
         </div>
-        <Button text="Register" block />
+        <div className={styles.lobby}>
+          <span className={styles.lobbyTitle}>lobby:</span>
+          {players.map(player => <span className={styles.lobbyText} key={player}>{player}</span>)}
+        </div>
       </div>
     </div>
   );
