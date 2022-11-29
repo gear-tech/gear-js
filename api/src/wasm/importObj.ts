801ca453fbdad4120eadb78c0a9b064e23559669
Dmitry Osipov <dmith.osipov@gmail.com>
2022-11-29 13:00:58 +0300

    update syscalls

--- api/src/wasm/importObj.ts
+++ api/src/wasm/importObj.ts
@@ -29,48 +29,8 @@ export default (
     free: () => {},
     gr_block_height: () => block_height,
     gr_block_timestamp: () => timestamp,
-    gr_exit: () => {},
-    gr_reserve_gas: () => {},
-    gr_unreserve_gas: () => {},
-    gr_gas_available: () => {},
-    gr_program_id: () => {},
-    gr_origin: () => {},
-    gr_random: () => {},
-    gr_leave: () => {},
-    gr_value_available: () => {},
-    gr_wait: () => {},
-    gr_wait_up_to: () => {},
-    gr_wait_for: () => {},
-    gr_wake: () => {},
-    gr_exit_code: () => {},
-    gr_message_id: () => {},
-    gr_read: (at: number, len: number, buffer: number) => {
-      new Uint8Array(memory.buffer).set(inputValue.slice(at, len), buffer);
-    },
-    gr_reply: () => {},
-    gr_reply_wgas: () => {},
-    gr_reply_commit: () => {},
-    gr_reply_commit_wgas: () => {},
-    gr_reservation_reply: () => {},
-    gr_reservation_reply_commit: () => {},
-    gr_reply_push: () => {},
-    gr_reply_to: () => {},
-    gr_send: () => {},
-    gr_send_wgas: () => {},
-    gr_send_commit: () => {},
-    gr_send_commit_wgas: () => {},
-    gr_send_init: () => {},
-    gr_send_push: () => {},
-    gr_reservation_send: () => {},
-    gr_reservation_send_commit: () => {},
-    gr_size: (size_ptr: number) => {
-      const len = CreateType.create('u32', inputValue.byteLength).toU8a();
-      for (let i = 0; i < len.length; i++) {
-        new Uint8Array(memory.buffer)[size_ptr + i] = len[i];
-      }
-    },
-    gr_source: () => {},
-    gr_value: () => {},
+    gr_create_program_wgas: () => {},
+    gr_create_program: () => {},
     gr_debug: (payload: number, len: number) => {
       if (showDebug) {
         console.debug(
@@ -79,13 +39,54 @@ export default (
         );
       }
     },
-    gr_create_program_wgas: () => {},
-    gr_create_program: () => {},
     gr_error: (error: number, len: number) => {
       console.error(
         '[GR_ERROR]',
         CreateType.create('String', new Uint8Array(memory.buffer.slice(error, error + len))).toString(),
       );
     },
+    gr_status_code: () => {},
+    gr_exit: () => {},
+    gr_gas_available: () => {},
+    gr_leave: () => {},
+    gr_message_id: () => {},
+    gr_origin: () => {},
+    gr_program_id: () => {},
+    gr_random: () => {},
+    gr_read: (at: number, len: number, buffer: number) => {
+      new Uint8Array(memory.buffer).set(inputValue.slice(at, len), buffer);
+    },
+    gr_reply_commit_wgas: () => {},
+    gr_reply_commit: () => {},
+    gr_reply_push: () => {},
+    gr_reply_to: () => {},
+    gr_reply_wgas: () => {},
+    gr_reply: () => {},
+    gr_reservation_reply_commit: () => {},
+    gr_reservation_reply: () => {},
+    gr_reservation_send_commit: () => {},
+    gr_reservation_send: () => {},
+    gr_reserve_gas: () => {},
+    gr_send_commit_wgas: () => {},
+    gr_send_commit: () => {},
+    gr_send_init: () => {},
+    gr_send_push: () => {},
+    gr_send_wgas: () => {},
+    gr_send: () => {},
+    gr_size: (size_ptr: number) => {
+      const len = CreateType.create('u32', inputValue.byteLength).toU8a();
+      for (let i = 0; i < len.length; i++) {
+        new Uint8Array(memory.buffer)[size_ptr + i] = len[i];
+      }
+    },
+    gr_source: () => {},
+    gr_system_reserve_gas: () => {},
+    gr_unreserve_gas: () => {},
+    gr_value_available: () => {},
+    gr_value: () => {},
+    gr_wait: () => {},
+    gr_wait_up_to: () => {},
+    gr_wait_for: () => {},
+    gr_wake: () => {},
   },
 });
