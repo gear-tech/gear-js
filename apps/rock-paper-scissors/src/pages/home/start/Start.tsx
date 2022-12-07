91fe15a8544d2149e7431bb40e271f5d0124cb48
Kolko Valentin <valiant9191@gmail.com>
2022-12-07 21:49:47 +0300

    feat: main menu, comment unused

--- apps/rock-paper-scissors/src/pages/home/start/Start.tsx
+++ apps/rock-paper-scissors/src/pages/home/start/Start.tsx
@@ -1,15 +1,15 @@
 import { Button } from '@gear-js/ui';
 import { ReactComponent as CreateSVG } from 'assets/images/icons/create.svg';
-import { Games } from './games';
+// import { Games } from './games';
 import styles from './Start.module.scss';
 
 type Props = {
-  games: any[];
-  ownerGames: any[];
+  // games?: any[];
+  // ownerGames?: any[];
   onCreateClick: (arg: string) => void;
 };
 
-function Start({ games, ownerGames, onCreateClick }: Props) {
+function Start({ onCreateClick }: Props) {
   return (
     <>
       <h2 className={styles.heading}>Rock Paper Scissors Lizard Spock</h2>
@@ -20,8 +20,15 @@ function Start({ games, ownerGames, onCreateClick }: Props) {
         className={styles.button}
         onClick={() => onCreateClick('create')}
       />
-      <Games heading="Games" list={games} />
-      <Games heading="My games" list={ownerGames} />
+      <Button
+        icon={CreateSVG}
+        text="Join game"
+        size="large"
+        className={styles.button}
+        onClick={() => onCreateClick('join')}
+      />
+      {/* <Games heading="Games" list={games} />
+      <Games heading="My games" list={ownerGames} /> */}
     </>
   );
 }
