7519fec203ede0b38e80793677c9287fc5942672
Kolko Valentin <valiant9191@gmail.com>
2022-12-07 21:31:48 +0300

    feat: new types;

--- apps/rock-paper-scissors/src/types/index.ts
+++ apps/rock-paper-scissors/src/types/index.ts
@@ -1,3 +1,4 @@
+import { Hex } from '@gear-js/api';
 import { FunctionComponent, SVGProps } from 'react';
 
 type StageType = 'preparation' | 'progress' | 'reveal';
@@ -6,23 +7,42 @@ type SVGType = FunctionComponent<SVGProps<SVGSVGElement> & { title?: string | un
 
 type StateConfigType = {
     Config: {
-        betSize: string;
-        entryTimeoutMs: string;
-        moveTimeoutMs: string;
-        playersCountLimit: string;
-        revealTimeoutMs: string;
-    }   
-} | undefined
+        betSize: string | undefined;
+        entryTimeoutMs: string | undefined;
+        moveTimeoutMs: string | undefined;
+        playersCountLimit: string | undefined;
+        revealTimeoutMs: string | undefined;
+    }
+}
 
 type StateTimeStampType = {
     CurrentStageTimestamp?: string,
 }
-
-type StateGameStageType = {
-    GameStage?: StageType|undefined
+type StateTimeLeftType = {
+    Deadline?: string,
 }
 
-type TimeType = {hours:string,minutes:string, seconds:string}
+type StateGameStageType = {
+    GameStage?: StageType | undefined | {}
+}
+type StateLobbyType = {
+    LobbyList?: Hex[] | string[] | []
+}
+
+type TimeType = { hours: string, minutes: string, seconds: string }
+
+type UserMoveType = {name?:string, id?:string, SVG?:React.FunctionComponent<React.SVGProps<SVGSVGElement>>}
 
 
-export type { StageType, SVGType, StateConfigType, StateTimeStampType, StateGameStageType, TimeType };
+
+export type {
+    StageType,
+    SVGType,
+    StateConfigType,
+    StateTimeStampType,
+    StateGameStageType,
+    StateLobbyType,
+    StateTimeLeftType,
+    TimeType,
+    UserMoveType
+};
