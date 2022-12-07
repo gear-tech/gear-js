c46ad35883fec773b20483bc304d0a561275617f
Kolko Valentin <valiant9191@gmail.com>
2022-12-07 21:51:01 +0300

    feat: add func to send form;

--- apps/rock-paper-scissors/src/pages/home/reveal/Reveal.tsx
+++ apps/rock-paper-scissors/src/pages/home/reveal/Reveal.tsx
@@ -1,25 +1,35 @@
+import { useState } from 'react';
 import { Button, Input } from '@gear-js/ui';
 import { ActionButton, BackButton } from 'components';
 import { SVGType } from 'types';
 import styles from './Reveal.module.scss';
 
 type Props = {
-  move: { name: string; SVG: SVGType };
+  move: { name: string; SVG?: SVGType; id?: string };
+  onClickMove: (attr: string, pass: string) => void;
+  onRouteChange: (arg: string) => void;
 };
 
-function Reveal({ move }: Props) {
-  const { name, SVG } = move;
+function Reveal({ move, onClickMove, onRouteChange }: Props) {
+  
+  const [passwordValue, setPasswordvalue] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setPasswordvalue(e.target.value);
+
+  const handleSubmit = () => {
+    if (move.id && passwordValue) { onClickMove(move.id, passwordValue) }
+  }
 
   return (
     <div className={styles.container}>
       <h2 className={styles.heading}>Reveal</h2>
       <div className={styles.box}>
-        <Input type="password" label="Password" direction="y" className={styles.input} />
-        <ActionButton name={name} SVG={SVG} isActive />
+        <Input type="password" label="Password" direction="y" className={styles.input} onChange={handleChange} />
+        {move?.name && <ActionButton name={move?.name} SVG={move?.SVG} isActive onClick={()=>{}}  />}
       </div>
       <div className={styles.buttons}>
-        <BackButton onClick={() => {}} />
-        <Button text="Done" size="large" />
+        <BackButton onClick={() => onRouteChange('game')} />
+        <Button onClick={handleSubmit} text="Done" size="large" />
       </div>
     </div>
   );
