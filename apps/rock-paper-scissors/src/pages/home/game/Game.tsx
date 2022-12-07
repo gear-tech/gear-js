06c432630d0a7f4e308563b69b86169891693d3c
Kolko Valentin <valiant9191@gmail.com>
2022-12-07 21:58:13 +0300

    feat: fn route, UI for different users

--- apps/rock-paper-scissors/src/pages/home/game/Game.tsx
+++ apps/rock-paper-scissors/src/pages/home/game/Game.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { Hex } from '@gear-js/api';
 import { Button } from '@gear-js/ui';
 import { BackButton, Detail, Players, Stage } from 'components';
@@ -6,26 +7,49 @@ import { Countdown } from './countdown';
 import styles from './Game.module.scss';
 
 type Props = {
-  onBackClick: (arg: string) => void;
+  onRouteChange: (arg: string) => void;
+  onLeaveGame?: () => void;
+  account?: Hex | undefined;
   heading: string;
-  stage?: StageType;
+  stage?: StageType | '';
   bet: string | undefined;
   game: string;
   round: string;
-  players: Array<Hex>;
+  players?: Array<Hex> | Array<string> | [];
+  finishedPlayers?: Array<Hex> | Array<string> | undefined;
   hoursLeft: string;
   minutesLeft: string;
   secondsLeft: string;
+  admin?: boolean
 };
 
 
-function Game({ onBackClick, players, heading, stage, bet, game, round, hoursLeft, minutesLeft, secondsLeft }: Props) {
+function Game({ onRouteChange, onLeaveGame, account, players, finishedPlayers, heading, stage, bet, game, round, hoursLeft, minutesLeft, secondsLeft, admin }: Props) {
+
+  const handleRouteChange = () => admin ? onRouteChange('detail admin') : onRouteChange('detail')
+
+  const getButton = useMemo(() => {
+    if (stage === "preparation") { return }
+    if (finishedPlayers?.includes(account as Hex)) { return }
+    if (stage === 'progress') {
+      return <Button onClick={() => onRouteChange('move')} text="Make a move" size="large" className={styles.actionButton} />
+    }
+    if (stage === 'reveal') {
+      return <Button onClick={() => onRouteChange('reveal')} text="Reveal" size="large" className={styles.actionButton} />
+    }
+    // console.log('unsupported stage')
+  }, [stage, onRouteChange, finishedPlayers, account])
+
+
   return (
     <div className={styles.container}>
       <div className={styles.players}>
-        <BackButton onClick={() => onBackClick('')} />
-        <Players list={players as Hex[]} heading="Current players" />
-        <Button text="Reveal" size="large" className={styles.actionButton} />
+        <BackButton onClick={() => onRouteChange('')} />
+        <Players
+          finishedPlayers={finishedPlayers as string[]}
+          list={players as string[]}
+          heading="Current players" />
+        {!admin && getButton}
       </div>
       <div className={styles.summary}>
         <h2 className={styles.heading}>{heading}</h2>
@@ -33,19 +57,20 @@ function Game({ onBackClick, players, heading, stage, bet, game, round, hoursLef
           <Detail label="Time left" direction="x" className={styles.time}>
             <Countdown hours={hoursLeft} minutes={minutesLeft} seconds={secondsLeft} />
           </Detail>
-          {stage &&
-            <Detail label="Stage" className={styles.stage}>
-              <Stage value={stage} />
-            </Detail>
-          }
+          <Detail label="Stage" className={styles.stage}>
+            <Stage value={stage} />
+          </Detail>
           <Detail label="Bet size" className={styles.bet}>
             <span className={styles.text}> <span className={styles.bet}>{bet}</span> Unit</span>
           </Detail>
           <Detail label="Current game" text={game} className={styles.game} />
           <Detail label="Current round" text={round} className={styles.round} />
         </div>
-        <Button text="Details" color="light" size="large" onClick={() => onBackClick('detail')} className={styles.detailsButton} />
-        <Button text="Leave the game" color="light" size="large" />
+        <Button text="Details" color="light" size="large" onClick={handleRouteChange} className={styles.detailsButton} />
+        {admin ?
+          <Button text="Edit next game" color="light" size="large" onClick={() => onRouteChange('create')} />
+          :
+          <Button onClick={onLeaveGame} text="Leave the game" color="light" size="large" />}
       </div>
     </div>
   );
