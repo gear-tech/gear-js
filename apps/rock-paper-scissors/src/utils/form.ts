d82ebeb24cf5cf5f7f4906c57c10f1de399faee6
Kolko Valentin <valiant9191@gmail.com>
2022-12-07 21:30:57 +0300

    feat: check value in form, new routes;

--- apps/rock-paper-scissors/src/utils/form.ts
+++ apps/rock-paper-scissors/src/utils/form.ts
@@ -1,22 +1,34 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 const isExists = (value: string) => (!value ? 'Field is required' : null);
 
+const isMinValue = (value: number, minDefaultValue: number) => (value < minDefaultValue ? `Minimal value ${minDefaultValue}` : null)
+
 const useForm = () => {
-    const [form, setForm] = useState('');
-    const routes = ['create', 'lobby', 'detail',]
-  
-    const openCreateForm = () => setForm('create');
-    const openLobbyForm = () => setForm('lobby');
-    const openDetailsForm = () => setForm('detail')
-  
-    const closeForm = () => setForm('');
-    const onClickRouteChange = (param: string): void => {
-      const result = routes.filter(route => route === param)
-      setForm(result[0])
-    }
-  
-    return { form, openCreateForm, closeForm, openLobbyForm, openDetailsForm, onClickRouteChange };
+  const [form, setForm] = useState('');
+  const routes = [
+    'create',
+    'lobby admin',
+    'detail',
+    'detail admin',
+    'join',
+    'Join game',
+    'game',
+    'move',
+    'reveal',
+  ]
+
+  const openCreateForm = () => setForm('create');
+  const openLobbyForm = () => setForm('lobby');
+  const openDetailsForm = () => setForm('detail')
+
+  const closeForm = () => setForm('');
+  const onClickRouteChange = (param: string): void => {
+    const result = routes.filter(route => route === param)
+    setForm(result[0])
   }
 
-export { isExists, useForm };
+  return { form, openCreateForm, closeForm, openLobbyForm, openDetailsForm, onClickRouteChange };
+}
+
+export { isExists, isMinValue, useForm };
--- apps/rock-paper-scissors/src/utils/index.ts
+++ apps/rock-paper-scissors/src/utils/index.ts
@@ -1,7 +1,7 @@
 import type { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
 import { LOCAL_STORAGE } from 'consts';
-import { isExists, useForm } from './form';
+import { isExists, isMinValue, useForm } from './form';
 
 const isLoggedIn = ({ address }: InjectedAccountWithMeta) => localStorage[LOCAL_STORAGE.ACCOUNT] === address;
 
-export { isLoggedIn, isExists, useForm };
+export { isLoggedIn, isExists, isMinValue, useForm };
