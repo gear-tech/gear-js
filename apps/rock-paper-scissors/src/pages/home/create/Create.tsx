686d023d6e8d13c448e871d1f638f17af85ee734
Kolko Valentin <valiant9191@gmail.com>
2022-12-07 21:41:48 +0300

    feat: init values from ms to s, validate new check

--- apps/rock-paper-scissors/src/pages/home/create/Create.tsx
+++ apps/rock-paper-scissors/src/pages/home/create/Create.tsx
@@ -2,11 +2,11 @@ import { useCreateHandler } from '@gear-js/react-hooks';
 import { Button, Input } from '@gear-js/ui';
 import { useForm } from '@mantine/form';
 import { BackButton } from 'components';
-import { isExists } from 'utils';
+import { isExists, isMinValue } from 'utils';
 import styles from './Create.module.scss';
 
 type Props = {
-  onBackClick: (arg:string) => void;
+  onBackClick: (arg: string) => void;
   onSubmit: ReturnType<typeof useCreateHandler>;
   setStateAction: any
 };
@@ -19,20 +19,29 @@ const initialValues = {
   moveTimeoutMs: '',
   revealTimeoutMs: '',
 };
+const transformValues = (values: any) => ({
+  betSize: values.betSize,
+  playersCountLimit: values.playersCountLimit,
+  entryTimeoutMs: `${values.entryTimeoutMs}000`,
+  moveTimeoutMs: `${values.moveTimeoutMs}000`,
+  revealTimeoutMs: `${values.revealTimeoutMs}000`,
+})
 
 const validate = {
   // gameName: isExists,
-  betSize: isExists,
-  playersCountLimit: isExists,
-  entryTimeoutMs: isExists,
-  moveTimeoutMs: isExists,
-  revealTimeoutMs: isExists,
+  betSize: (value: string) => (isExists(value) || isMinValue(Number(value), 1000)),
+  playersCountLimit: (value: string) => (isExists(value) || isMinValue(Number(value), 2)),
+  entryTimeoutMs: (value: string) => (isExists(value) || isMinValue(Number(value), 10)),
+  moveTimeoutMs: (value: string) => (isExists(value) || isMinValue(Number(value), 10)),
+  revealTimeoutMs: (value: string) => (isExists(value) || isMinValue(Number(value), 10)),
 };
 
 function Create({ onBackClick, onSubmit, setStateAction }: Props) {
-  const form = useForm({ initialValues, validate });
+  const form = useForm({ initialValues, validate, transformValues, });
   const { getInputProps } = form;
-  const handleSubmit = form.onSubmit((values) => { onSubmit(values, { onSuccess: (hex) => setStateAction(hex) }) });
+  const handleSubmit = form.onSubmit((values) => {
+    onSubmit(values, { onSuccess: (hex) => { setStateAction(hex); } })
+  });
 
   return (
     <>
@@ -41,11 +50,11 @@ function Create({ onBackClick, onSubmit, setStateAction }: Props) {
         {/* <Input label="Game name" direction="y" {...getInputProps('gameName')} /> */}
         <Input autoComplete="off" type='number' label="Bet size" direction="y" {...getInputProps('betSize')} />
         <Input autoComplete="off" type='number' label="Max players" direction="y" {...getInputProps('playersCountLimit')} />
-        <Input autoComplete="off" type='number' label="Entry timeout" direction="y" {...getInputProps('entryTimeoutMs')} />
-        <Input autoComplete="off" type='number' label="Move timeout" direction="y" {...getInputProps('moveTimeoutMs')} />
-        <Input autoComplete="off" type='number' label="Reveal timeout" direction="y" {...getInputProps('revealTimeoutMs')} />
+        <Input autoComplete="off" type='number' label="Entry timeout (seconds)" direction="y" {...getInputProps('entryTimeoutMs')} />
+        <Input autoComplete="off" type='number' label="Move timeout (seconds)" direction="y" {...getInputProps('moveTimeoutMs')} />
+        <Input autoComplete="off" type='number' label="Reveal timeout (seconds)" direction="y" {...getInputProps('revealTimeoutMs')} />
         <div className={styles.buttons}>
-          <BackButton onClick={()=>onBackClick('')} />
+          <BackButton onClick={() => onBackClick('')} />
           <Button type="submit" text="Create" size="large" />
         </div>
       </form>
