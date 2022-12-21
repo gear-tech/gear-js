8e06c671a64ad855076235e86497821e6fcb3e9e
Kolko Valentin <valiant9191@gmail.com>
2022-12-21 12:28:35 +0300

    rename, change props

--- apps/rock-paper-scissors/src/pages/home/reveal/Reveal.tsx
+++ apps/rock-paper-scissors/src/pages/home/reveal/Reveal.tsx
@@ -1,31 +1,35 @@
 import { useState } from 'react';
 import { Button, Input } from '@gear-js/ui';
 import { ActionButton, BackButton } from 'components';
-import { SVGType } from 'types';
+import { UserMoveType } from 'types';
+import { onSubmitReveal } from 'utils';
 import styles from './Reveal.module.scss';
 
 type Props = {
-  move: { name: string; SVG?: SVGType; id?: string };
-  onClickMove: (attr: string, pass: string) => void;
+  userMove: UserMoveType;
+  payloadSend: any;
   onRouteChange: (arg: string) => void;
 };
 
-function Reveal({ move, onClickMove, onRouteChange }: Props) {
-  
+function Reveal({ userMove, payloadSend, onRouteChange }: Props) {
   const [passwordValue, setPasswordvalue] = useState('');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setPasswordvalue(e.target.value);
 
   const handleSubmit = () => {
-    if (move.id && passwordValue) { onClickMove(move.id, passwordValue) }
-  }
+    if (userMove.id && passwordValue) {
+      onSubmitReveal(onRouteChange, payloadSend, userMove.id, passwordValue);
+    }
+  };
 
   return (
     <div className={styles.container}>
       <h2 className={styles.heading}>Reveal</h2>
       <div className={styles.box}>
         <Input type="password" label="Password" direction="y" className={styles.input} onChange={handleChange} />
-        {move?.name && <ActionButton name={move?.name} SVG={move?.SVG} isActive onClick={()=>{}}  />}
+        {userMove?.name && userMove?.SVG && (
+          <ActionButton name={userMove?.name} SVG={userMove?.SVG} isActive onClick={() => {}} />
+        )}
       </div>
       <div className={styles.buttons}>
         <BackButton onClick={() => onRouteChange('game')} />
