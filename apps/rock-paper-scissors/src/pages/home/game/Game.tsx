cb41ef62e889a479578944d2677efdc2daa58240
Kolko Valentin <valiant9191@gmail.com>
2022-12-21 12:24:42 +0300

    refactor: styles, fn getButton;

--- apps/rock-paper-scissors/src/pages/home/game/Game.tsx
+++ apps/rock-paper-scissors/src/pages/home/game/Game.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react'
 import { Hex } from '@gear-js/api';
 import { Button } from '@gear-js/ui';
 import { BackButton, Detail, Players, Stage } from 'components';
@@ -8,9 +7,8 @@ import styles from './Game.module.scss';
 
 type Props = {
   onRouteChange: (arg: string) => void;
-  account?: Hex | undefined;
   heading: string;
-  stage?: StageType | '';
+  stage: StageType;
   bet: string | undefined;
   game: string;
   round: string;
@@ -19,34 +17,52 @@ type Props = {
   hoursLeft: string;
   minutesLeft: string;
   secondsLeft: string;
-  admin?: boolean
+  admin?: boolean;
+  account?: Hex;
 };
 
+function Game({
+  onRouteChange,
+  players,
+  finishedPlayers,
+  heading,
+  stage,
+  bet,
+  game,
+  round,
+  hoursLeft,
+  minutesLeft,
+  secondsLeft,
+  account,
+  admin,
+}: Props) {
+  const handleRouteChange = () => (admin ? onRouteChange('detail admin') : onRouteChange('detail'));
 
-function Game({ onRouteChange, account, players, finishedPlayers, heading, stage, bet, game, round, hoursLeft, minutesLeft, secondsLeft, admin }: Props) {
-
-  const handleRouteChange = () => admin ? onRouteChange('detail admin') : onRouteChange('detail')
-
-  const getButton = useMemo(() => {
-    if (stage === "preparation") return;
+  const getButton = () => {
+    if (stage === 'preparation') return;
     if (finishedPlayers?.includes(account as Hex)) return;
     if (stage === 'progress') {
-      return <Button onClick={() => onRouteChange('move')} text="Make a move" size="large" className={styles.actionButton} />
-    };
+      return (
+        <Button onClick={() => onRouteChange('move')} text="Make a move" size="large" className={styles.actionButton} />
+      );
+    }
     if (stage === 'reveal') {
-      return <Button onClick={() => onRouteChange('reveal')} text="Reveal" size="large" className={styles.actionButton} />
-    };
-  }, [onRouteChange, stage, finishedPlayers, account]);
-  
+      return (
+        <Button onClick={() => onRouteChange('reveal')} text="Reveal" size="large" className={styles.actionButton} />
+      );
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.players}>
         <BackButton onClick={() => onRouteChange('')} />
         <Players
           finishedPlayers={finishedPlayers as string[]}
-          list={players as string[]}
-          heading="Current players" />
-        {!admin && getButton}
+          list={(players as Hex[]) || []}
+          heading="Current players"
+        />
+        {!admin && getButton()}
       </div>
       <div className={styles.summary}>
         <h2 className={styles.heading}>{heading}</h2>
@@ -58,15 +74,21 @@ function Game({ onRouteChange, account, players, finishedPlayers, heading, stage
             <Stage value={stage} />
           </Detail>
           <Detail label="Bet size" className={styles.bet}>
-            <span className={styles.text}> <span className={styles.bet}>{bet}</span> Unit </span>
+            <span className={styles.text}>
+              <span className={styles.bet}>{bet}</span> Unit{' '}
+            </span>
           </Detail>
           <Detail label="Current game" text={game} className={styles.game} />
           <Detail label="Current round" text={round} className={styles.round} />
         </div>
-        <Button text="Details" color="light" size="large" onClick={handleRouteChange} className={styles.detailsButton} />
-        {admin &&
-          <Button text="Edit next game" color="light" size="large" onClick={() => onRouteChange('create')} />
-        }
+        <Button
+          text="Details"
+          color="light"
+          size="large"
+          onClick={handleRouteChange}
+          className={styles.detailsButton}
+        />
+        {admin && <Button text="Edit next game" color="light" size="large" onClick={() => onRouteChange('create')} />}
       </div>
     </div>
   );
