cc20f4d696422969416581820a738568418fb16c
Kolko Valentin <valiant9191@gmail.com>
2022-12-21 12:35:49 +0300

    ref, add fn buttonText

--- apps/rock-paper-scissors/src/pages/home/round-result/RoundResult.tsx
+++ apps/rock-paper-scissors/src/pages/home/round-result/RoundResult.tsx
@@ -4,16 +4,17 @@ import { Players } from 'components';
 import styles from './RoundResult.module.scss';
 
 type Props = {
+  onClickRoute: (route:string) => void;
   name: string;
   game: string;
   round: string;
-  winners?:[string, `0x${string}`][]|[];
+  winners?: [string, `0x${string}`][] | [];
   loosers?: Hex[];
+  nextRound?: boolean;
 };
 
-function RoundResult({ name, game, round, winners, loosers }: Props) {
-  const hexId=winners?.map(el=>el[0])
-  // const moves=winners?.map(el=>el[1])
+function RoundResult({ name, game, round, winners, loosers, nextRound, onClickRoute }: Props) {
+  const getButtonText=nextRound ? 'Next Round' : 'Close';
   return (
     <div className={styles.container}>
       <h2 className={styles.heading}>
@@ -21,9 +22,9 @@ function RoundResult({ name, game, round, winners, loosers }: Props) {
         <span>Game #{game}</span>
       </h2>
       <h3 className={styles.subheading}>Round {round} result</h3>
-      <Players heading="Advance to the next round" list={hexId} className={styles.players} center />
-      <Players heading="Losers" list={loosers} center />
-      <Button text="Close" size="large" className={styles.button} />
+      <Players heading="Advance to the next round" list={winners as []} className={styles.players} center />
+      <Players heading="Losers" list={loosers as Hex[] || []} center />
+      <Button text={getButtonText} size="large" className={styles.button} onClick={()=>onClickRoute('game')} />
     </div>
   );
 }
