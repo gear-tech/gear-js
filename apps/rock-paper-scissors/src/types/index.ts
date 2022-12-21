17a380797ce66df845e1a678e819d1135bdc2e25
Kolko Valentin <valiant9191@gmail.com>
2022-12-21 12:31:17 +0300

    refactor types;

--- apps/rock-paper-scissors/src/types/index.ts
+++ apps/rock-paper-scissors/src/types/index.ts
@@ -6,51 +6,47 @@ type StageType = 'preparation' | 'progress' | 'reveal';
 type SVGType = FunctionComponent<SVGProps<SVGSVGElement> & { title?: string | undefined }>;
 
 type StateConfigType = {
-    Config: {
-        betSize: string | undefined;
-        entryTimeoutMs: string | undefined;
-        moveTimeoutMs: string | undefined;
-        playersCountLimit: string | undefined;
-        revealTimeoutMs: string | undefined;
-    }
-}
+  Config: {
+    betSize: string | undefined;
+    entryTimeoutMs: string | undefined;
+    moveTimeoutMs: string | undefined;
+    playersCountLimit: string | undefined;
+    revealTimeoutMs: string | undefined;
+  };
+};
 
-type StateTimeLeftType = {
-    Deadline?: string,
-}
+type StateTimeLeftType = { Deadline?: string };
 
-type StateGameStageType = {
-    GameStage?: StageType | undefined | {}
-}
-type StateLobbyType = {
-    LobbyList?: Hex[] | string[] | []
-}
-type StateRoundType = {
-    CurrentRound?: number | string | undefined
-}
-type StateWinnerType = {
-    Winner?: Hex | undefined
-}
+type StateGameStageType = { GameStage?: StageType | undefined | {} };
 
-type TimeType = { hours: string, minutes: string, seconds: string }
+type StateLobbyType = { LobbyList?: Hex[] };
 
-type PlayersMoveType = { PlayerMoves: Hex[] }
+type StateRoundType = { CurrentRound?: number | string | undefined };
 
-type UserMoveType = { name?: string, id?: string, SVG?: React.FunctionComponent<React.SVGProps<SVGSVGElement>> }
+type StateWinnerType = { Winner?: Hex | undefined };
 
+type TimeType = { hours: string; minutes: string; seconds: string };
 
+type PlayersMoveType = { PlayerMoves: Hex[] };
 
+type UserMoveType = { name?: string; id?: string; SVG?: React.FunctionComponent<React.SVGProps<SVGSVGElement>> };
+
+type GameStageType = {
+  InProgress?: { finishedPlayers: Hex[] };
+  Reveal?: { finishedPlayers: Hex[] };
+};
 
 export type {
-    StageType,
-    SVGType,
-    StateConfigType,
-    StateGameStageType,
-    StateLobbyType,
-    StateTimeLeftType,
-    TimeType,
-    PlayersMoveType,
-    UserMoveType,
-    StateRoundType,
-    StateWinnerType
+  StageType,
+  SVGType,
+  StateConfigType,
+  StateGameStageType,
+  StateLobbyType,
+  StateTimeLeftType,
+  TimeType,
+  PlayersMoveType,
+  UserMoveType,
+  StateRoundType,
+  StateWinnerType,
+  GameStageType,
 };
