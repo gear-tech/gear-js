009e76cfc71fff6154c0ecb67bfb34c58dea93d8
Kolko Valentin <valiant9191@gmail.com>
2022-12-26 11:01:06 +0300

    refactor

--- apps/rock-paper-scissors/src/utils/sendMessage.ts
+++ apps/rock-paper-scissors/src/utils/sendMessage.ts
@@ -1,49 +1,55 @@
-import { Hex } from "@gear-js/api";
-import { stringToU8a } from "@polkadot/util";
+import { Hex } from '@gear-js/api';
+import { stringToU8a } from '@polkadot/util';
 import { blake2AsHex } from '@polkadot/util-crypto';
-import { UserMoveType } from "types";
+import { UserMoveType } from 'types';
 
-const onClickRegister = (routeChange: (arg: string) => void, payloadSend: any, lobbyList: Hex[], accountHex: Hex, betSize: string) => {
-    if (lobbyList?.includes(accountHex as never)) {
-        return routeChange('game');
-    }
-    const replacedBetSize = betSize?.replace(',', '')
-    payloadSend(
-        { Register: null },
-        {
-            onSuccess: () => {
-                routeChange('game');
-            },
-            value: replacedBetSize,
-        },
-    );
+const onClickRegister = (
+  routeChange: (arg: string) => void,
+  payloadSend: any,
+  lobbyList: Hex[],
+  accountHex: Hex,
+  betSize: string,
+) => {
+  if (lobbyList?.includes(accountHex as never)) {
+    return routeChange('game');
+  }
+  const replacedBetSize = betSize?.replace(',', '');
+  payloadSend(
+    { Register: null },
+    {
+      onSuccess: () => {
+        routeChange('game');
+      },
+      value: replacedBetSize,
+    },
+  );
 };
 
 const onSubmitMove = (routeChange: (arg: string) => void, payloadSend: any, userMove: UserMoveType, pass: string) => {
-    if (pass && Object.keys(userMove).length > 0) {
-        const outputPass = userMove.id + pass;
-        const payload = blake2AsHex(outputPass, 256);
-        payloadSend(
-            { MakeMove: payload },
-            {
-                onSuccess: () => {
-                    routeChange('game');
-                },
-            },
-        );
-    }
+  if (pass && Object.keys(userMove).length > 0) {
+    const outputPass = userMove.id + pass;
+    const payload = blake2AsHex(outputPass, 256);
+    payloadSend(
+      { MakeMove: payload },
+      {
+        onSuccess: () => {
+          routeChange('game');
+        },
+      },
+    );
+  }
 };
 
 const onSubmitReveal = (routeChange: (arg: string) => void, payloadSend: any, userMove: string, pass: string) => {
-    const outputPass = stringToU8a(`${userMove}${pass}`);
-    payloadSend(
-        { Reveal: Array.from(outputPass) },
-        {
-            onSuccess: () => {
-                routeChange('round result');
-            },
-        },
-    );
+  const outputPass = stringToU8a(`${userMove}${pass}`);
+  payloadSend(
+    { Reveal: Array.from(outputPass) },
+    {
+      onSuccess: () => {
+        routeChange('round result');
+      },
+    },
+  );
 };
 
 // const onEndGame = () => {
@@ -52,4 +58,4 @@ const onSubmitReveal = (routeChange: (arg: string) => void, payloadSend: any, us
 //     { onSuccess: () => { console.log('Game OvEr'); onClickRouteChange('') } })
 // };
 
-export { onSubmitReveal, onSubmitMove, onClickRegister }
+export { onSubmitReveal, onSubmitMove, onClickRegister };
