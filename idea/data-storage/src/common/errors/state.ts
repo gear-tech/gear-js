a61727d1acb453e817939b4d1ceee1bff31ec2b2
Almazatun <atunstyle@gmail.com>
2023-01-12 13:23:24 +0600

    add state exception StateAlreadyExists

--- idea/data-storage/src/common/errors/state.ts
+++ idea/data-storage/src/common/errors/state.ts
@@ -3,3 +3,7 @@ import { GearJsonRPCError } from './base';
 export class StateNotFound extends GearJsonRPCError {
   name = 'StateNotFound';
 }
+
+export class StateAlreadyExists extends GearJsonRPCError {
+  name = 'StateAlreadyExists';
+}
