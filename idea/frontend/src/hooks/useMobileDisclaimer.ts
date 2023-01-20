10506ebef959ac8aba0ed38ea5c00a2fb4ffe00a
Nikita Yutanov <nikitayutanov@gmail.com>
2023-01-20 18:45:46 +0300

    Fix display if user already visited

--- idea/frontend/src/hooks/useMobileDisclaimer.ts
+++ idea/frontend/src/hooks/useMobileDisclaimer.ts
@@ -1,10 +1,16 @@
 import { useState } from 'react';
+import { LocalStorage } from 'shared/config';
 import { isMobileDevice } from 'shared/helpers';
 
 const useMobileDisclaimer = () => {
-  const [isMobileDisclaimerVisible, setIsMobileDisclaimerVisible] = useState(isMobileDevice());
+  const [isMobileDisclaimerVisible, setIsMobileDisclaimerVisible] = useState(
+    isMobileDevice() && !localStorage[LocalStorage.IsNewUser],
+  );
 
-  const closeMobileDisclaimer = () => setIsMobileDisclaimerVisible(false);
+  const closeMobileDisclaimer = () => {
+    setIsMobileDisclaimerVisible(false);
+    localStorage.setItem(LocalStorage.IsNewUser, 'false');
+  };
 
   return { isMobileDisclaimerVisible, closeMobileDisclaimer };
 };
