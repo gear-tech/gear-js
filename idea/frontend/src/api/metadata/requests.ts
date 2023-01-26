8289702761e209d067a9664d334228c46e2b7275
Nikita Yutanov <nikitayutanov@gmail.com>
2023-01-26 15:31:55 +0300

    Fix get metadata request (#1165)

--- idea/frontend/src/api/metadata/requests.ts
+++ idea/frontend/src/api/metadata/requests.ts
@@ -4,6 +4,6 @@ import { IMeta } from 'entities/metadata';
 
 const addMetadata = (params: IMeta) => rpcService.callRPC(RpcMethods.AddMetadata, params);
 
-const fetchMetadata = (programId: string) => rpcService.callRPC<IMeta>(RpcMethods.GetMetadata, { programId });
+const fetchMetadata = (programId: string) => rpcService.callRPC<IMeta>(RpcMethods.GetMetadata, { id: programId });
 
 export { addMetadata, fetchMetadata };
