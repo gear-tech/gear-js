c19f8e15ab0624afb28904eafb43720ec793662c
Nikita Yutanov <nikitayutanov@gmail.com>
2023-01-30 18:45:32 +0300

    Change codehash link on program page

--- idea/frontend/src/pages/code/hooks/useMetadata.ts
+++ idea/frontend/src/pages/code/hooks/useMetadata.ts
@@ -1,13 +1,18 @@
 import { Hex, ProgramMetadata, getProgramMetadata } from '@gear-js/api';
+import { useAlert } from '@gear-js/react-hooks';
 import { useState, useEffect } from 'react';
 
 import { fetchCodeMetadata } from 'api';
 
 const useMetadata = (codeId: Hex) => {
+  const alert = useAlert();
+
   const [metadata, setMetadata] = useState<ProgramMetadata>();
 
   useEffect(() => {
-    fetchCodeMetadata(codeId).then(({ result }) => setMetadata(getProgramMetadata(result.hex)));
+    fetchCodeMetadata(codeId)
+      .then(({ result }) => setMetadata(getProgramMetadata(result.hex)))
+      .catch(({ message }: Error) => alert.error(message));
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
