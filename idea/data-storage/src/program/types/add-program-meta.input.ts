8f21f31f3cdcc35068c735a37da3bf4286bed306
Almaz Pasonov <atunstyle@gmail.com>
2023-02-06 21:32:38 +0600

    Update meta service feature `addMetaByProgram` (#1184)

---
+++ idea/data-storage/src/program/types/add-program-meta.input.ts
@@ -0,0 +1,6 @@
+import { Meta } from '../../database/entities';
+
+export interface AddProgramMetaInput {
+  name: string;
+  meta: Meta;
+}
--- idea/data-storage/src/program/types/index.ts
+++ idea/data-storage/src/program/types/index.ts
@@ -1,4 +1,5 @@
 import { CreateProgramInput } from './create-program.input';
 import { UpdateProgramDataInput } from './update-program-data.input';
+import { AddProgramMetaInput } from './add-program-meta.input';
 
-export { CreateProgramInput, UpdateProgramDataInput };
+export { CreateProgramInput, UpdateProgramDataInput, AddProgramMetaInput };
