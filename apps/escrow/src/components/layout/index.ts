f51650571e32c48a6c19baede38192326c11a6c1
Eugene Way <relax211@gmail.com>
2023-03-07 11:08:40 +0100

    Delete escrow app (#1220)
    Co-authored-by: Dmitry Osipov <dmith.osipov@gmail.com>

--- apps/escrow/src/components/layout/index.ts
+++
@@ -1,5 +0,0 @@
-import { Header } from './header';
-import { Footer } from './footer';
-import { Box } from './box';
-
-export { Header, Footer, Box };
--- apps/escrow/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-import { useEscrow, useEscrowMessage, useWallets } from './api';
-import { useWalletId } from './wallet';
-import { useProgram } from './program';
-import { useWasm } from './context';
-
-export { useEscrow, useWallets, useEscrowMessage, useWalletId, useProgram, useWasm };
