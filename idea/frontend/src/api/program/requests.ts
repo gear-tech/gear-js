fdad4fbdf4561126301b139cd456e57e0c60cfff
Nikita Yutanov <nikitayutanov@gmail.com>
2023-04-17 16:51:15 +0300

    Fix local node add name request (#1265)
    Co-authored-by: Dmitry Osipov <dmith.osipov@gmail.com>

--- idea/frontend/src/api/program/requests.ts
+++ idea/frontend/src/api/program/requests.ts
@@ -8,8 +8,8 @@ import { FetchProgramsParams, ProgramPaginationModel } from './types';
 
 const fetchProgram = (id: string) => rpcService.callRPC<IProgram>(RpcMethods.GetProgram, { id });
 
-const addProgramName = (params: { id: HexString; name: string }) =>
-  rpcService.callRPC(RpcMethods.AddProgramName, params);
+const addProgramName = (params: { id: HexString; name: string }, isDevChain: boolean | undefined) =>
+  isDevChain ? Promise.resolve() : rpcService.callRPC(RpcMethods.AddProgramName, params);
 
 const fetchPrograms = (params: FetchProgramsParams) =>
   rpcService.callRPC<ProgramPaginationModel>(RpcMethods.GetAllPrograms, params);
