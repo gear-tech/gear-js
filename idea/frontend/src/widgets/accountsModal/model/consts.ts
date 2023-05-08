c1a13c80d7889a539dd9262cd4a3e7a7e05b3f41
Nikita Yutanov <nikitayutanov@gmail.com>
2023-05-08 12:46:06 +0300

    Add enkrypt wallet support (#1284)

--- idea/frontend/src/widgets/accountsModal/model/consts.ts
+++ idea/frontend/src/widgets/accountsModal/model/consts.ts
@@ -1,11 +1,13 @@
 import { ReactComponent as PolkadotSVG } from 'shared/assets/images/wallets/polkadot.svg';
 import { ReactComponent as SubwalletSVG } from 'shared/assets/images/wallets/subwallet.svg';
 import { ReactComponent as TalismanSVG } from 'shared/assets/images/wallets/talisman.svg';
+import { ReactComponent as EnkryptSVG } from 'shared/assets/images/wallets/enkrypt.svg';
 
 const WALLET = {
   'polkadot-js': { name: 'Polkadot JS', icon: PolkadotSVG },
   'subwallet-js': { name: 'SubWallet', icon: SubwalletSVG },
   talisman: { name: 'Talisman', icon: TalismanSVG },
+  enkrypt: { name: 'Enkrypt', icon: EnkryptSVG },
 };
 
 const WALLETS = Object.keys(WALLET);
