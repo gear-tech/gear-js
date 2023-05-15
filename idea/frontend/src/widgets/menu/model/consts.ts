631f926564ed7148fe427bd64b95f6f2eaacad95
Nikita Yutanov <nikitayutanov@gmail.com>
2023-05-16 01:15:41 +0300

    Update nodes

--- idea/frontend/src/widgets/menu/model/consts.ts
+++ idea/frontend/src/widgets/menu/model/consts.ts
@@ -5,34 +5,58 @@ import { ReactComponent as NetworkVaraSVG } from 'shared/assets/images/logos/net
 const DEVELOPMENT_SECTION = 'development';
 
 const GENESIS = {
-  VARA: '0x69599490fc00e8c5636ec255f4eee61d1ca950dd87df7a32cd92b6c8f61dbe28',
+  VARA_TESTNET: '0x69599490fc00e8c5636ec255f4eee61d1ca950dd87df7a32cd92b6c8f61dbe28',
+  VARA: '0xfe1b4c55fd4d668101126434206571a7838a8b6b93a6d1b95d607e78e6c53763',
 };
 
+const VARA_LOGO = { SVG: VaraSVG, SHORT_SVG: ShortVaraSVG, NETWORK: NetworkVaraSVG };
+
 const LOGO = {
-  [GENESIS.VARA]: { SVG: VaraSVG, SHORT_SVG: ShortVaraSVG, NETWORK: NetworkVaraSVG },
+  [GENESIS.VARA_TESTNET]: VARA_LOGO,
+  [GENESIS.VARA]: VARA_LOGO,
 };
 
 const NODE_SECTIONS = [
   {
-    caption: 'test network',
+    caption: 'Vara Stable Testnet',
     nodes: [
       {
         isCustom: false,
-        address: 'wss://rpc-node.gear-tech.io:443',
+        address: 'wss://testnet.vara.rs',
+        SVG: NetworkVaraSVG,
       },
       {
         isCustom: false,
-        address: 'wss://rpc-node.gear-tech.io:443',
-        SVG: LOGO[GENESIS.VARA].NETWORK,
+        address: 'wss://archive-testnet.vara.rs',
+        SVG: NetworkVaraSVG,
       },
     ],
   },
   {
-    caption: 'workshop',
+    caption: 'Gear Staging Testnet V7',
     nodes: [
       {
         isCustom: false,
-        address: 'wss://node-workshop.gear.rs:443',
+        address: 'wss://rpc-node.gear-tech.io',
+      },
+      {
+        isCustom: false,
+        address: 'wss://archive-node.gear-tech.io',
+      },
+    ],
+  },
+  {
+    caption: 'vara network',
+    nodes: [
+      {
+        isCustom: false,
+        address: 'wss://rpc.vara-network.io',
+        SVG: NetworkVaraSVG,
+      },
+      {
+        isCustom: false,
+        address: 'wss://archive-rpc.vara-network.io',
+        SVG: NetworkVaraSVG,
       },
     ],
   },
