7110c88aeb2c729a74a1cb485cdb627749e58c4e
Nikita Yutanov <nikitayutanov@gmail.com>
2023-05-15 21:29:02 +0300

    Add network logo display

--- idea/frontend/src/widgets/menu/model/consts.ts
+++ idea/frontend/src/widgets/menu/model/consts.ts
@@ -1,3 +1,14 @@
+import { ReactComponent as VaraSVG } from 'shared/assets/images/logos/vara.svg';
+import { ReactComponent as ShortVaraSVG } from 'shared/assets/images/logos/shortcut/vara.svg';
+
 const DEVELOPMENT_SECTION = 'development';
 
-export { DEVELOPMENT_SECTION };
+const GENESIS = {
+  VARA: '0x69599490fc00e8c5636ec255f4eee61d1ca950dd87df7a32cd92b6c8f61dbe28',
+};
+
+const LOGO = {
+  [GENESIS.VARA]: { SVG: VaraSVG, SHORT_SVG: ShortVaraSVG },
+};
+
+export { DEVELOPMENT_SECTION, LOGO };
--- idea/frontend/src/widgets/menu/ui/logo/Logo.tsx
+++ idea/frontend/src/widgets/menu/ui/logo/Logo.tsx
@@ -1,27 +1,34 @@
-import { CSSTransition } from 'react-transition-group';
+import { useApi } from '@gear-js/react-hooks';
 import { Link } from 'react-router-dom';
 
-import { AnimationTimeout, routes } from 'shared/config';
-import { ReactComponent as GearsSVG } from 'shared/assets/images/logos/gears.svg';
-import { ReactComponent as IdeaLogoSVG } from 'shared/assets/images/logos/idea.svg';
-import { ReactComponent as GearLogoSVG } from 'shared/assets/images/logos/gearText.svg';
+import { routes } from 'shared/config';
+import { ReactComponent as IdeaSVG } from 'shared/assets/images/logos/idea.svg';
+import { ReactComponent as GearSVG } from 'shared/assets/images/logos/gear.svg';
+import { ReactComponent as ShortGearSVG } from 'shared/assets/images/logos/shortcut/gear.svg';
 
+import { LOGO } from '../../model/consts';
 import styles from './Logo.module.scss';
 
 type Props = {
   isOpen: boolean;
 };
 
-const Logo = ({ isOpen }: Props) => (
-  <Link to={routes.programs} className={styles.logoWrapper}>
-    <CSSTransition in={isOpen} timeout={AnimationTimeout.Default} unmountOnExit className={styles.ideaLogo}>
-      <IdeaLogoSVG />
-    </CSSTransition>
-    <GearsSVG className={styles.gearsLogo} />
-    <CSSTransition in={isOpen} timeout={AnimationTimeout.Default} unmountOnExit className={styles.gearLogo}>
-      <GearLogoSVG />
-    </CSSTransition>
-  </Link>
-);
+const Logo = ({ isOpen }: Props) => {
+  const { api, isApiReady } = useApi();
+
+  const genesis = isApiReady ? api.genesisHash.toHex() : undefined;
+
+  const logo = genesis ? LOGO[genesis] : undefined;
+  const SVG = logo?.SVG || GearSVG;
+  const ShortSVG = logo?.SHORT_SVG || ShortGearSVG;
+
+  return (
+    <Link to={routes.programs} className={styles.logoWrapper}>
+      {isOpen && <IdeaSVG className={styles.ideaSVG} />}
+      {!isOpen && <ShortSVG />}
+      {isOpen && <SVG />}
+    </Link>
+  );
+};
 
 export { Logo };
