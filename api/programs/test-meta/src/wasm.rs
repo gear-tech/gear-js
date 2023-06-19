373c83667f7124424cabba747df07aebe50e6622
Dmitry Osipov <dmith.osipov@gmail.com>
2023-06-19 11:31:15 +0400

    rm metahash

--- api/programs/test-meta/src/wasm.rs
+++ api/programs/test-meta/src/wasm.rs
@@ -6,7 +6,7 @@ static mut STATE: Vec<Wallet> = Vec::new();
 
 #[no_mangle]
 unsafe extern "C" fn init() {
-    let message_in: BTreeSet<u8> = msg::load().expect("Unable to decode message");
+    let _: BTreeSet<u8> = msg::load().expect("Unable to decode message");
 
     let mut res: BTreeMap<String, u8> = BTreeMap::new();
 
@@ -47,7 +47,7 @@ unsafe extern "C" fn handle() {
     let action: Action = msg::load().unwrap();
 
     match action {
-        Action::One(option_string) => msg::reply(EmptyStruct { empty: () }, 1000).unwrap(),
+        Action::One(_) => msg::reply(EmptyStruct { empty: () }, 1000).unwrap(),
         _ => msg::reply(EmptyStruct { empty: () }, 0).unwrap(),
     };
 }
@@ -55,10 +55,4 @@ unsafe extern "C" fn handle() {
 #[no_mangle]
 extern "C" fn state() {
     msg::reply(unsafe { STATE.clone() }, 0).expect("Error in state");
-}
-
-#[no_mangle]
-extern "C" fn metahash() {
-    let metahash: [u8; 32] = include!("../.metahash");
-    msg::reply(metahash, 0).expect("Failed to share metahash");
 }
