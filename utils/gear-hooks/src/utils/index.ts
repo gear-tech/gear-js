9ec4fa1ac72f8ba9fccb46860ee518a06804bf15
Nikita Yutanov <nikitayutanov@gmail.com>
2023-07-14 18:51:56 +0300

    Add network balance unit

--- utils/gear-hooks/src/utils/index.ts
+++ utils/gear-hooks/src/utils/index.ts
@@ -1,19 +1,6 @@
-import { decodeAddress, GasInfo } from '@gear-js/api';
+import { GasInfo } from '@gear-js/api';
 import type { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
-import { Balance } from '@polkadot/types/interfaces';
 import { bnToBn } from '@polkadot/util';
-import { Account } from 'types';
-
-const getBalance = (balance: Balance) => {
-  const [value, unit] = balance.toHuman().split(' ');
-  return { value, unit };
-};
-
-const getAccount = (account: InjectedAccountWithMeta, balance: Balance): Account => ({
-  ...account,
-  balance: getBalance(balance),
-  decodedAddress: decodeAddress(account.address),
-});
 
 const isLoggedIn = ({ address }: InjectedAccountWithMeta) => localStorage.account === address;
 
@@ -22,4 +9,4 @@ const getAutoGasLimit = ({ waited, min_limit }: GasInfo) =>
 
 const withoutCommas = (value: string) => value.replace(/,/g, '');
 
-export { getBalance, getAccount, isLoggedIn, getAutoGasLimit, withoutCommas };
+export { isLoggedIn, getAutoGasLimit, withoutCommas };
