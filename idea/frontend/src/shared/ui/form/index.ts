f94e447c6a994774b1f6dbca8547f0f3a032c3d9
Nikita Yutanov <nikitayutanov@gmail.com>
2023-08-02 17:34:15 +0300

    Add value transfer unit

--- idea/frontend/src/shared/ui/form/index.ts
+++ idea/frontend/src/shared/ui/form/index.ts
@@ -2,6 +2,6 @@ import formStyles from './Form.module.scss';
 import { FormText } from './formText';
 import { FormInput } from './formInput';
 import { FormTextarea } from './formTextarea';
-import { ValueField } from './value-field';
+import { ValueField } from './value-field/ValueField';
 
 export { formStyles, FormText, FormInput, FormTextarea, ValueField };
