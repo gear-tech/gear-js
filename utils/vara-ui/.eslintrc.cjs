8fb886ffd234e7889784f1b13880f751ea089766
Nikita Yutanov <nikitayutanov@gmail.com>
2023-08-08 14:41:26 +0300

    Init storybook

--- utils/vara-ui/.eslintrc.cjs
+++ utils/vara-ui/.eslintrc.cjs
@@ -1,10 +1,14 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -12,7 +16,9 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {
+        allowConstantExport: true,
+      },
     ],
   },
-}
+};
--- utils/vara-ui/src/main.tsx
+++ utils/vara-ui/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const container = document.getElementById('root');
+const root = createRoot(container as HTMLElement);
+
+root.render(
+  <StrictMode>
+    <div>App</div>
+  </StrictMode>,
+);
