219c19ef08e70cd1e991f22eee625530e1a3ee6b
Dmitry Osipov <dmith.osipov@gmail.com>
2023-08-21 10:12:19 +0400

    fix: create input payload

--- api/src/State.ts
+++ api/src/State.ts
@@ -56,7 +56,7 @@ export class GearProgramState extends GearProgramStorage {
    */
   async read<T extends Codec = Codec>(args: ReadStateArgs, meta: ProgramMetadata, type?: number): Promise<T> {
     const payload =
-      meta.version === MetadataVersion.V1Rust
+      meta.version === MetadataVersion.V2Rust
         ? meta.createType((meta.types.state as HumanTypesRepr).input!, args.payload)
         : '0x';
 
