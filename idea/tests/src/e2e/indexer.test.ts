69983d326cda4be81f1946440abb3d88fe788e71
Dmitry Osipov <dmith.osipov@gmail.com>
2023-08-23 11:40:43 +0400

    update tests

--- idea/tests/src/e2e/indexer.test.ts
+++ idea/tests/src/e2e/indexer.test.ts
@@ -25,7 +25,7 @@ import { getAccounts, sleep } from '../utils';
 import { getAllMessages, getMessageData, getMessagePayload, getMessagesByDates } from './messages';
 import { getCodeData, getCodes, getCodesByDates } from './code';
 import { networkDataAvailable } from './network-data-available';
-import { blocksStatus } from './block';
+import request from './request';
 
 let genesis: HexString;
 let prepared: IPrepared;
@@ -33,7 +33,7 @@ let api: GearApi;
 let alice: KeyringPair;
 let test_meta_id: HexString;
 
-const programs = [];
+const programs: { programId: string; codeId: string; metahash?: string; hasState: boolean }[] = [];
 const codes = [];
 const sentMessages = [];
 const receivedMessages = [];
@@ -90,7 +90,9 @@ async function listenToEvents() {
   });
 }
 
-describe('upload programs', () => {
+const finalizationPromises = [];
+
+describe('prepare', () => {
   test('upload test_meta', async () => {
     const code = readFileSync(path.join(PATH_TO_PROGRAMS, 'test_meta.opt.wasm'));
 
@@ -101,20 +103,27 @@ describe('upload programs', () => {
     test_meta_id = programId;
     programs.push({ programId, codeId, metahash, hasState: true });
     codes.push({ codeId, metahash, hasState: true });
-    const [mqid, mqsource, mqdestination]: [string, string, string] = await new Promise((resolve, reject) =>
-      api.program.signAndSend(alice, ({ events }) => {
-        events.forEach(({ event }) => {
-          if (event.method === 'ExtrinsicFailed') {
-            reject(new Error(api.getExtrinsicFailedError(event).docs.join('. ')));
-          } else if (event.method === 'MessageQueued') {
-            const {
-              data: { id, source, destination },
-            } = event as MessageQueued;
-            resolve([id.toHex(), source.toHex(), destination.toHex()]);
-          }
-        });
-      }),
-    );
+    const [mqid, mqsource, mqdestination]: [string, string, string] = await new Promise((resolve, reject) => {
+      finalizationPromises.push(
+        new Promise((finResolve) => {
+          api.program.signAndSend(alice, ({ events, status }) => {
+            if (status.isFinalized) {
+              finResolve(0);
+            }
+            events.forEach(({ event }) => {
+              if (event.method === 'ExtrinsicFailed') {
+                reject(new Error(api.getExtrinsicFailedError(event).docs.join('. ')));
+              } else if (event.method === 'MessageQueued') {
+                const {
+                  data: { id, source, destination },
+                } = event as MessageQueued;
+                resolve([id.toHex(), source.toHex(), destination.toHex()]);
+              }
+            });
+          });
+        }),
+      );
+    });
 
     sentMessages.push({ id: mqid, source: mqsource, destination: mqdestination, entry: 'init', payload });
   });
@@ -124,20 +133,29 @@ describe('upload programs', () => {
     const { programId, codeId } = api.program.upload({ code, gasLimit: 200_000_000_000 });
     programs.push({ programId, codeId, metahash: null, hasState: false });
     codes.push({ codeId, metahash: null, hasState: false });
-    const [mqid, mqsource, mqdestination]: [string, string, string] = await new Promise((resolve, reject) =>
-      api.program.signAndSend(alice, ({ events }) => {
-        events.forEach(({ event }) => {
-          if (event.method === 'ExtrinsicFailed') {
-            reject(new Error(api.getExtrinsicFailedError(event).docs.join('. ')));
-          } else if (event.method === 'MessageQueued') {
-            const {
-              data: { id, source, destination },
-            } = event as MessageQueued;
-            resolve([id.toHex(), source.toHex(), destination.toHex()]);
-          }
-        });
-      }),
-    );
+    const [mqid, mqsource, mqdestination]: [string, string, string] = await new Promise((resolve, reject) => {
+      finalizationPromises.push(
+        new Promise((finResolve) => {
+          api.program.signAndSend(alice, ({ events, status }) => {
+            if (status.isFinalized) {
+              finResolve(0);
+            }
+            if (status.isInBlock) {
+              events.forEach(({ event }) => {
+                if (event.method === 'ExtrinsicFailed') {
+                  reject(new Error(api.getExtrinsicFailedError(event).docs.join('. ')));
+                } else if (event.method === 'MessageQueued') {
+                  const {
+                    data: { id, source, destination },
+                  } = event as MessageQueued;
+                  resolve([id.toHex(), source.toHex(), destination.toHex()]);
+                }
+              });
+            }
+          });
+        }),
+      );
+    });
 
     sentMessages.push({ id: mqid, source: mqsource, destination: mqdestination, entry: 'init', payload: '0x' });
   });
@@ -146,76 +164,133 @@ describe('upload programs', () => {
     const payload = meta.createType(meta.types.handle.input, { One: 'Alice' }).toHex();
     const tx = api.message.send({ destination: test_meta_id, gasLimit: 2_000_000_000, payload, value: 1000 }, meta);
 
-    await new Promise((resolve, reject) =>
-      tx.signAndSend(alice, ({ events }) => {
-        events.forEach(({ event }) => {
-          if (event.method === 'ExtrinsicFailed') {
-            reject(new Error(api.getExtrinsicFailedError(event).docs.join('. ')));
-          } else if (event.method === 'MessageQueued') {
-            const {
-              data: { id, source, destination },
-            } = event as MessageQueued;
-            sentMessages.push({
-              id: id.toHex(),
-              source: source.toHex(),
-              destination: destination.toHex(),
-              entry: 'handle',
-              payload,
+    await new Promise((resolve, reject) => {
+      finalizationPromises.push(new Promise((finResolve) => {
+        tx.signAndSend(alice, ({ events, status }) => {
+          if (status.isFinalized) {
+            finResolve(0);
+          }
+          if (status.isInBlock) {
+            events.forEach(({ event }) => {
+              console.log(event.toJSON());
+              if (event.method === 'ExtrinsicFailed') {
+                reject(new Error(api.getExtrinsicFailedError(event).docs.join('. ')));
+              } else if (event.method === 'MessageQueued') {
+                const {
+                  data: { id, source, destination },
+                } = event as MessageQueued;
+                sentMessages.push({
+                  id: id.toHex(),
+                  source: source.toHex(),
+                  destination: destination.toHex(),
+                  entry: 'handle',
+                  payload,
+                });
+                resolve(0);
+              }
             });
-            resolve(0);
           }
         });
-      }),
-    );
+      }));
+    });
   });
 
   test('send batch of messages to test_meta', async () => {
     const txs = [];
-    const payload_0 = meta.createType(meta.types.handle.input, { Two: [[8, 16]] }).toHex();
-    txs.push(api.message.send({ destination: test_meta_id, payload: payload_0, gasLimit: 200_000_000_000 }));
-    const payload_1 = meta
-      .createType(meta.types.handle.input, {
-        Four: { array8: new Array(8).fill(0), array32: new Array(32).fill(1), actor: decodeAddress(alice.address) },
-      })
-      .toHex();
-    txs.push(api.message.send({ destination: test_meta_id, payload: payload_1, gasLimit: 200_000_000_000 }));
+    const payloads = [
+      meta.createType(meta.types.handle.input, { Two: [[8, 16]] }).toHex(),
+      meta
+        .createType(meta.types.handle.input, {
+          Four: { array8: new Array(8).fill(0), array32: new Array(32).fill(1), actor: decodeAddress(alice.address) },
+        })
+        .toHex(),
+    ];
+
+    txs.push(api.message.send({ destination: test_meta_id, payload: payloads[0], gasLimit: 200_000_000_000 }));
+    txs.push(api.message.send({ destination: test_meta_id, payload: payloads[1], gasLimit: 200_000_000_000 }));
 
     const tx = api.tx.utility.batchAll(txs);
 
-    await new Promise((resolve, reject) =>
-      tx.signAndSend(alice, ({ events }) => {
-        events.forEach(({ event }) => {
-          if (event.method === 'ExtrinsicFailed') {
-            reject(new Error(api.getExtrinsicFailedError(event).docs.join('. ')));
-          } else if (event.method === 'MessageQueued') {
-            console.log(event.index.toJSON());
-            const {
-              data: { id, source, destination },
-            } = event as MessageQueued;
-            sentMessages.push({
-              id: id.toHex(),
-              source: source.toHex(),
-              destination: destination.toHex(),
-              entry: 'handle',
-              // payload,
-            });
-            resolve(0);
-          }
-        });
-      }),
-    );
+    let index = -1;
+
+    await new Promise((resolve, reject) => {
+      finalizationPromises.push(
+        new Promise((finResolve) => {
+          tx.signAndSend(alice, ({ events, status }) => {
+            if (status.isFinalized) {
+              finResolve(0);
+            }
+            if (status.isInBlock) {
+              events.forEach(({ event }) => {
+                if (event.method === 'ExtrinsicFailed') {
+                  reject(new Error(api.getExtrinsicFailedError(event).docs.join('. ')));
+                } else if (event.method === 'MessageQueued') {
+                  index++;
+                  const {
+                    data: { id, source, destination },
+                  } = event as MessageQueued;
+                  sentMessages.push({
+                    id: id.toHex(),
+                    source: source.toHex(),
+                    destination: destination.toHex(),
+                    entry: 'handle',
+                    payload: payloads[index],
+                  });
+                } else if (event.method === 'ExtrinsicSuccess') {
+                  resolve(0);
+                }
+              });
+            }
+          });
+        }),
+      );
+    });
+
+    expect(index).toBe(1);
+  });
+
+  test('wait for finalization', async () => {
+    await Promise.all(finalizationPromises);
   });
 });
 
-describe.skip('Indexer methods', () => {
+describe('methods', () => {
   test(API_GATEWAY_METHODS.NETWORK_DATA_AVAILABLE, async () => {
-    expect(await networkDataAvailable(genesis)).toBeTruthy();
+    const response = await request('networkData.available', {
+      genesis,
+    });
+
+    expect(response).toHaveProperty('result');
+    expect(response.result).toBeTruthy();
   });
 
   test(INDEXER_METHODS.BLOCKS_STATUS, async () => {
-    expect(await blocksStatus(genesis)).toBeTruthy();
+    const response = await request('blocks.status', { genesis });
+
+    expect(response).toHaveProperty('result');
+    expect(response.result).toHaveProperty('number');
+    expect(response.result).toHaveProperty('hash');
+    expect(response.result).toHaveProperty('timestamp');
+    expect(response.result).toHaveProperty('genesis');
   });
 
+  test(INDEXER_METHODS.PROGRAM_ALL, async () => {
+    const response = await request('program.all', { genesis });
+    expect(response).toHaveProperty('result');
+    console.log(response.result);
+    expect(response.result.count).toBe(programs.length);
+    for (const p of programs) {
+      const receivedProgram = response.result.programs.find(({ id }) => id === p.programId);
+      expect(receivedProgram).toBeDefined();
+      expect(receivedProgram.code.id).toEqual(p.codeId);
+      expect(receivedProgram.metahash).toEqual(p.metahash);
+    }
+  });
+
+  test(INDEXER_METHODS.PROGRAM_ALL, async () => {});
+});
+
+describe.skip('Indexer methods', () => {
   test(INDEXER_METHODS.PROGRAM_ALL, async () => {
     expect(await getAllPrograms(genesis, Object.keys(prepared.programs) as HexString[])).toBeTruthy();
     expect(await getAllProgramsByOwner(genesis, prepared.programs as IPreparedPrograms)).toBeTruthy();
