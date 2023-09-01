e625fe88d082daed0d6ac54b690a101fcd176a03
Dmitry Osipov <dmith.osipov@gmail.com>
2023-09-01 15:36:59 +0400

    small refactoring

--- idea/api-gateway/src/middleware/validate-json-rpc-request.middleware.ts
+++ idea/api-gateway/src/middleware/validate-json-rpc-request.middleware.ts
@@ -1,9 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { IRpcRequest, JSONRPC_ERRORS, logger } from '@gear-js/common';
 
-export async function validateJsonRpcRequestMiddleware(req: Request, res: Response, next: NextFunction) {
-  const body: IRpcRequest = req.body;
-
+export async function validateJsonRpcRequestMiddleware({ body }: Request, res: Response, next: NextFunction) {
   if (Array.isArray(body)) {
     for (const request of body) {
       if (!isValidRequestParams(request)) {
@@ -25,16 +23,15 @@ function isValidRequestParams({ id, method, jsonrpc, params }: IRpcRequest): boo
 
 function getInvalidParamsResponse(req: IRpcRequest) {
   logger.info('Invalid params error', { req });
-  const response: any = {
-    jsonrpc: '2.0',
-  };
+
   const error = JSONRPC_ERRORS.InvalidParams.name;
 
-  response['id'] = req.id ? req.id : null;
-  response['error'] = {
-    message: JSONRPC_ERRORS[error].message,
-    code: JSONRPC_ERRORS[error].code,
+  return {
+    jsonrpc: '2.0',
+    id: req.id || null,
+    error: {
+      message: JSONRPC_ERRORS[error].message,
+      code: JSONRPC_ERRORS[error].code,
+    },
   };
-
-  return response;
 }
