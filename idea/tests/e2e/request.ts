3e6cae3fa6aa49192bcaef90383059e3bd729120
Dmitry Osipov <dmith.osipov@gmail.com>
2023-09-01 18:54:36 +0400

    fix: return invalid request

--- idea/tests/e2e/request.ts
+++ idea/tests/e2e/request.ts
@@ -13,6 +13,21 @@ export default async function (method: string, params: any) {
   return response.json();
 }
 
+export async function invalidRequest(method: string, params: any) {
+  const response = await fetch(base.gear.api, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      jsonrpc: '2.0',
+      method,
+      params,
+    }),
+  });
+  return response.json();
+}
+
 export async function batchRequest(method: string, params: any) {
   const response = await fetch(base.gear.api, {
     method: 'POST',
