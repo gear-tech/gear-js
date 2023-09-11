33f5c36adba4892c343b8d71b0f6908116cc93ed
Nikita Yutanov <nikitayutanov@gmail.com>
2023-09-11 18:40:30 +0300

    Fix dynamic gas multiplier (#1386)

--- idea/frontend/src/hooks/useGasMultiplier.ts
+++ idea/frontend/src/hooks/useGasMultiplier.ts
@@ -1,14 +1,19 @@
 import { useApi } from '@gear-js/react-hooks';
+
 import { useBalanceMultiplier } from './useBalanceMultiplier';
 
 function useGasMultiplier() {
   const { api, isApiReady } = useApi();
-  const valuePerGas = isApiReady ? api.valuePerGas.toString() : '0';
+  // '1000' is an old runtime fallback
+  const valuePerGas = isApiReady ? api.valuePerGas?.toString() || '1000' : '0';
 
   const { balanceMultiplier } = useBalanceMultiplier();
   const gasMultiplier = balanceMultiplier.dividedBy(valuePerGas);
 
-  return gasMultiplier;
+  // TODO: find a way to calculate logarithm without number
+  const gasDecimals = Math.floor(Math.log10(gasMultiplier.toNumber()));
+
+  return { gasMultiplier, gasDecimals };
 }
 
 export { useGasMultiplier };
