34afd36fc464e14aea11ceafbff75e98480305bc
Nikita Yutanov <nikitayutanov@gmail.com>
2023-09-12 12:12:13 +0300

    Fix gas value fallback

--- idea/frontend/src/hooks/useGasMultiplier.ts
+++ idea/frontend/src/hooks/useGasMultiplier.ts
@@ -1,11 +1,19 @@
 import { useApi } from '@gear-js/react-hooks';
+import { useMemo } from 'react';
 
 import { useBalanceMultiplier } from './useBalanceMultiplier';
 
 function useGasMultiplier() {
-  const { api, isApiReady } = useApi();
+  const { api } = useApi();
+
   // '1000' is an old runtime fallback
-  const valuePerGas = isApiReady ? api.valuePerGas?.toString() || '1000' : '0';
+  const valuePerGas = useMemo(() => {
+    try {
+      return api.valuePerGas.toString();
+    } catch {
+      return '1000';
+    }
+  }, [api]);
 
   const { balanceMultiplier } = useBalanceMultiplier();
   const gasMultiplier = balanceMultiplier.dividedBy(valuePerGas);
