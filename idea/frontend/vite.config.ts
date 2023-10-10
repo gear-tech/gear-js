08359eede03d164fb6435fc084f168a9e2eb01ff
Nikita Yutanov <nikitayutanov@gmail.com>
2023-10-10 18:54:25 +0300

    Disable runtime eslint

--- idea/frontend/vite.config.ts
+++ idea/frontend/vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import path from 'path';
+// import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
