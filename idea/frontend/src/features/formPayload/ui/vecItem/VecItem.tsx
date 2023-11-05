4876c77b52c215e199a32cc9623094d043090f46
Nikita Yutanov <nikitayutanov@gmail.com>
2023-11-06 01:03:37 +0300

    Migrate payload form fields

--- idea/frontend/src/features/formPayload/ui/vecItem/VecItem.tsx
+++ idea/frontend/src/features/formPayload/ui/vecItem/VecItem.tsx
@@ -1,16 +1,16 @@
-import { useField } from 'react-final-form';
 import { Textarea } from '@gear-js/ui';
+import { useFormContext } from 'react-hook-form';
 
 import styles from '../FormPayload.module.scss';
 import { getItemLabel } from '../../helpers';
 import { PayloadItemProps } from '../../model/types';
 
 const VecItem = ({ title, levelName, typeStructure }: PayloadItemProps) => {
-  const { input } = useField(levelName);
+  const { register } = useFormContext();
 
   const itemLabel = getItemLabel(typeStructure.name, title);
 
-  return <Textarea {...input} rows={8} label={itemLabel} direction="y" className={styles.field} />;
+  return <Textarea {...register(levelName)} rows={8} label={itemLabel} direction="y" className={styles.field} />;
 };
 
 export { VecItem };
