624d305406b7c1a376f8166bae88e60ad63a5ce2
Nikita Yutanov <nikitayutanov@gmail.com>
2023-11-06 01:03:50 +0300

    Migrate transfer balance modal

--- idea/frontend/src/features/balance/ui/transfer-balance-modal/transfer-balance-modal.tsx
+++ idea/frontend/src/features/balance/ui/transfer-balance-modal/transfer-balance-modal.tsx
@@ -1,8 +1,7 @@
 import { decodeAddress } from '@gear-js/api';
 import { useAccount, useBalanceFormat } from '@gear-js/react-hooks';
 import { Button, Checkbox, Modal } from '@gear-js/ui';
-import { useState } from 'react';
-import { Form } from 'react-final-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
 import CloseSVG from '@/shared/assets/images/actions/close.svg?react';
@@ -13,8 +12,9 @@ import { useBalanceTransfer } from '@/hooks';
 import SubmitSVG from '../../assets/submit.svg?react';
 import styles from './transfer-balance-modal.module.scss';
 
-const initialValues = { address: '', value: '' };
+const defaultValues = { address: '', value: '', keepAlive: true };
 
+// TODOFORM:
 const validationSchema = yup.object().shape({
   address: yup
     .string()
@@ -32,9 +32,10 @@ const TransferBalanceModal = ({ close }: Props) => {
   const { getChainBalanceValue } = useBalanceFormat();
   const transferBalance = useBalanceTransfer();
 
-  const [keepAlive, setKeepAlive] = useState(true);
+  const methods = useForm({ defaultValues });
+  const { register } = methods;
 
-  const onSubmit = ({ address, value }: typeof initialValues) => {
+  const handleSubmit = ({ address, value, keepAlive }: typeof defaultValues) => {
     if (!account) return;
 
     const chainValue = getChainBalanceValue(value).toFixed();
@@ -46,26 +47,20 @@ const TransferBalanceModal = ({ close }: Props) => {
 
   return (
     <Modal heading="Transfer Balance" size="large" close={close}>
-      <Form initialValues={initialValues} onSubmit={onSubmit} validate={getValidation(validationSchema)}>
-        {({ handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <div className={styles.inputs}>
-              <FormInput name="address" label="Address" direction="y" block />
-              <ValueField name="value" label="Value:" direction="y" block />
-              <Checkbox
-                label="Keep Alive"
-                checked={keepAlive}
-                onChange={() => setKeepAlive((prevValue) => !prevValue)}
-              />
-            </div>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(handleSubmit)}>
+          <div className={styles.inputs}>
+            <FormInput name="address" label="Address" direction="y" block />
+            <ValueField name="value" label="Value:" direction="y" block />
+            <Checkbox label="Keep Alive" {...register('keepAlive')} />
+          </div>
 
-            <div className={styles.buttons}>
-              <Button type="submit" text="Send" size="large" icon={SubmitSVG} />
-              <Button icon={CloseSVG} color="light" size="large" text="Close" onClick={close} />
-            </div>
-          </form>
-        )}
-      </Form>
+          <div className={styles.buttons}>
+            <Button type="submit" text="Send" size="large" icon={SubmitSVG} />
+            <Button icon={CloseSVG} color="light" size="large" text="Close" onClick={close} />
+          </div>
+        </form>
+      </FormProvider>
     </Modal>
   );
 };
