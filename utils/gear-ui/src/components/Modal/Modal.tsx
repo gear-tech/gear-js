4473ea4b089db5f1f237618ec8e4388b393c0f3c
Nikita Yutanov <nikitayutanov@gmail.com>
2023-11-23 20:49:04 +0300

    Add modal footer and fix height with overflow

--- utils/gear-ui/src/components/Modal/Modal.tsx
+++ utils/gear-ui/src/components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { ReactNode, useEffect, useState, MouseEvent } from 'react';
+import { ReactNode, useEffect, useState, MouseEvent, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import { Button } from '../Button/Button';
 import { ReactComponent as xSVG } from './images/x.svg';
@@ -9,12 +9,34 @@ type Props = {
   heading: string;
   close: () => void;
   children?: ReactNode;
+  footer?: ReactNode;
   className?: string;
   size?: 'normal' | 'large';
 };
 
-const Modal = ({ heading, close, children, className, size = 'normal' }: Props) => {
+function useHeight() {
+  const [height, setHeight] = useState(0);
+
+  const ref = useCallback((node: HTMLDivElement | null) => {
+    if (node) setHeight(node.getBoundingClientRect().height);
+  }, []);
+
+  return [height, ref] as const;
+}
+
+function useMaxHeight() {
+  const [headerHeight, headerRef] = useHeight();
+  const [footerHeight, footerRef] = useHeight();
+
+  const padding = 32;
+  const bodyStyle = { maxHeight: `calc(100vh - ${headerHeight + footerHeight + 2 * padding}px)` };
+
+  return { bodyStyle, headerRef, footerRef };
+}
+
+const Modal = ({ heading, close, children, footer, className, size = 'normal' }: Props) => {
   const [root, setRoot] = useState<HTMLDivElement>();
+  const { bodyStyle, headerRef, footerRef } = useMaxHeight();
 
   const modalClassName = clsx(styles.modal, styles[size]);
   const bodyClassName = clsx(styles.body, className);
@@ -37,14 +59,21 @@ const Modal = ({ heading, close, children, className, size = 'normal' }: Props) 
   const component = (
     <div className={styles.overlay} onClick={handleOverlayClick} data-testid="overlay">
       <div className={modalClassName} data-testid="modal">
-        <header className={styles.header}>
+        <header className={styles.header} ref={headerRef}>
           <h3 className={styles.heading}>{heading}</h3>
           <Button icon={xSVG} color="transparent" onClick={close} />
         </header>
+
         {children && (
-          <div className={bodyClassName} data-testid="body">
+          <div className={bodyClassName} data-testid="body" style={bodyStyle}>
             {children}
           </div>
+        )}
+
+        {footer && (
+          <footer className={styles.footer} ref={footerRef}>
+            {footer}
+          </footer>
         )}
       </div>
     </div>
