9a728c96faddf0fb6644cb50b8a62932c444e047
Dmitry Osipov <dmith.osipov@gmail.com>
2023-11-27 14:27:03 +0100

    remove comments

--- idea/indexer/src/common/helpers/get-dates-filter.ts
+++ idea/indexer/src/common/helpers/get-dates-filter.ts
@@ -1,16 +1,11 @@
-import { Between, LessThanOrEqual, MoreThanOrEqual } from 'typeorm';
-
 export function getDatesFilter(fromDate: string, toDate: string) {
   if (fromDate && toDate) {
-    // return Between(new Date(fromDate), new Date(toDate));
     return { fromDate, toDate };
   }
   if (fromDate) {
-    // return MoreThanOrEqual(new Date(fromDate));
     return { fromDate };
   }
   if (toDate) {
-    // return LessThanOrEqual(new Date(toDate));
     return { toDate };
   }
   return undefined;
