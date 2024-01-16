29cab14b6e0d70806b5b562fed06d2b26e403b9c
Nikita Yutanov <nikitayutanov@gmail.com>
2024-01-16 16:53:05 +0300

    Add getVaraAddress helper and fix useDeriveBalancesAll export

--- utils/gear-hooks/src/utils/index.ts
+++ utils/gear-hooks/src/utils/index.ts
@@ -1,6 +1,8 @@
-import { GasInfo, GearApi } from '@gear-js/api';
+import { GasInfo, GearApi, HexString } from '@gear-js/api';
+import { Keyring } from '@polkadot/api';
 import { Event } from '@polkadot/types/interfaces';
 import { bnToBn } from '@polkadot/util';
+import { VARA_SS58_FORMAT } from 'consts';
 
 const getAutoGasLimit = ({ waited, min_limit }: GasInfo) =>
   waited ? min_limit.add(min_limit.mul(bnToBn(0.1))) : min_limit;
@@ -14,4 +16,6 @@ const getExtrinsicFailedMessage = (api: GearApi, event: Event) => {
   return `${errorMethod}: ${formattedDocs}`;
 };
 
-export { getAutoGasLimit, withoutCommas, getExtrinsicFailedMessage };
+const getVaraAddress = (value: HexString) => new Keyring().encodeAddress(value, VARA_SS58_FORMAT);
+
+export { getAutoGasLimit, withoutCommas, getExtrinsicFailedMessage, getVaraAddress };
