c55c3ebf4dda2abe4c8e9344293cfec50f30d33b
Nikita Yutanov <nikitayutanov@gmail.com>
2024-04-10 13:05:01 +0300

    Simplify isProgramExists validation

--- idea/frontend/src/features/voucher/hooks/use-program-id-schema.ts
+++ idea/frontend/src/features/voucher/hooks/use-program-id-schema.ts
@@ -6,22 +6,17 @@ import { z } from 'zod';
 function useProgramIdSchema(list: HexString[]) {
   const { api, isApiReady } = useApi();
 
-  const isProgramExists = async (id: HexString) => {
+  const isProgramExists = (id: HexString) => {
     if (!isApiReady) throw new Error('API is not initialized');
 
-    try {
-      await api.programStorage.getProgram(id);
-      return true;
-    } catch {
-      return false;
-    }
+    return api.program.exists(id);
   };
 
   return z
     .custom<HexString>()
     .refine((value) => isHex(value), 'Value should be hex')
     .refine((value) => !list.includes(value), 'Program ID already exists')
-    .refine(async (value) => isProgramExists(value), 'Program not found in the storage');
+    .refine((value) => isProgramExists(value), 'Program not found in the storage');
 }
 
 export { useProgramIdSchema };
