cc6dbdad544b86a31872ae05d8e2305ed6804a7c
Nikita Yutanov <nikitayutanov@gmail.com>
2024-04-10 13:42:34 +0300

    Drop hooks V110 support

--- utils/gear-hooks/src/hooks/api/voucher/index.ts
+++ utils/gear-hooks/src/hooks/api/voucher/index.ts
@@ -2,8 +2,6 @@ import { useIsVoucherExists, useIsAccountVoucherExists } from './use-is-voucher-
 import { useVoucher, useAccountVoucher } from './use-voucher';
 import { useVouchers, useAccountVouchers } from './use-vouchers';
 import { useVoucherStatus, useGetVoucherStatus } from './use-voucher-status';
-import { useAccountVoucherBalanceDeprecated, useVoucherBalanceDeprecated } from './use-voucher-balance-deprecated';
-import { useAccountVoucherDeprecated, useVoucherDeprecated } from './use-voucher-deprecated';
 import { useIsAnyVoucherActive, useIsAnyAccountVoucherActive } from './use-is-any-voucher-active';
 import { useIssuedVouchers, useAccountIssuedVouchers } from './use-issued-vouchers';
 
@@ -16,10 +14,6 @@ export {
   useAccountVouchers,
   useVoucherStatus,
   useGetVoucherStatus,
-  useVoucherBalanceDeprecated,
-  useVoucherDeprecated,
-  useAccountVoucherBalanceDeprecated,
-  useAccountVoucherDeprecated,
   useIsAnyVoucherActive,
   useIsAnyAccountVoucherActive,
   useIssuedVouchers,
