a53d80a02222d7bbdb879d46d389be7e7b3bfc38
Nikita Yutanov <nikitayutanov@gmail.com>
2024-06-06 02:07:19 +0300

    Add fixed size array field

--- idea/frontend/src/features/sails/ui/fixed-size-array-field/fixed-size-array-field.tsx
+++ idea/frontend/src/features/sails/ui/fixed-size-array-field/fixed-size-array-field.tsx
@@ -1,13 +1,24 @@
 import { Fieldset } from '@/shared/ui';
 
 import { TypeDef } from '../../types';
+import { getLabel, getNestedName, getType } from '../../utils';
 
 type Props = {
   def: TypeDef;
+  name: string;
+  label: string;
+  renderField: (def: TypeDef, name: string, label: string) => JSX.Element | undefined;
 };
 
-function FixedSizeArrayField({ def }: Props) {
-  return <Fieldset legend={'Fixed Size Array'}>fixed size array</Fieldset>;
+function FixedSizeArrayField({ def, name, label, renderField }: Props) {
+  const arrayDef = def.asFixedSizeArray;
+
+  const renderFields = () =>
+    new Array<typeof arrayDef>(arrayDef.len)
+      .fill(arrayDef)
+      .map((field, index) => renderField(field.def, '', getNestedName(name, index.toString())));
+
+  return <Fieldset legend={getLabel(label, getType(def))}>{renderFields()}</Fieldset>;
 }
 
 export { FixedSizeArrayField };
