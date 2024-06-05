1434e61269a3f592d2f30a95a8175b2e446e1ab8
Nikita Yutanov <nikitayutanov@gmail.com>
2024-06-06 00:48:02 +0300

    Add result field

--- idea/frontend/src/features/sails/ui/result-field/result-field.tsx
+++ idea/frontend/src/features/sails/ui/result-field/result-field.tsx
@@ -1,12 +1,35 @@
 import { Select } from '@gear-js/ui';
+import { useState } from 'react';
+
+import { Fieldset } from '@/shared/ui';
+
 import { TypeDef } from '../../types';
+import { getLabel, getNestedName, getType } from '../../utils';
 
 type Props = {
   def: TypeDef;
+  name: string;
+  label: string;
+  renderField: (def: TypeDef, label: string, name: string) => JSX.Element | undefined;
 };
 
-function ResultField({ def }: Props) {
-  return <Select options={[]} />;
+const OPTIONS = [
+  { label: 'Ok', value: 'ok' },
+  { label: 'Err', value: 'err' },
+] as const;
+
+type Result = typeof OPTIONS[number]['value'];
+
+function ResultField({ def, name, label, renderField }: Props) {
+  const [result, setResult] = useState<Result>(OPTIONS[0].value);
+
+  return (
+    <Fieldset legend={getLabel(label, getType(def))}>
+      <Select options={OPTIONS} value={result} onChange={({ target }) => setResult(target.value as Result)} />
+
+      {renderField(def.asResult[result].def, '', getNestedName(name, result))}
+    </Fieldset>
+  );
 }
 
 export { ResultField };
