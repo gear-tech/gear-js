a11c35de082ce9593fe22b6d9c33169b3627e651
Nikita Yutanov <nikitayutanov@gmail.com>
2024-06-05 15:02:23 +0300

    Add services selection

---
+++ idea/frontend/src/features/sails/ui/fields/fields.tsx
@@ -0,0 +1,51 @@
+import { Sails } from 'sails-js';
+
+import { generateRandomId } from '@/shared/helpers';
+
+import { EnumField } from '../enum-field';
+import { UserDefinedField } from '../user-defined-field';
+import { StructField } from '../struct-field';
+import { OptionalField } from '../optional-field';
+import { ResultField } from '../result-field';
+import { VecField } from '../vec-field';
+import { MapField } from '../map-field';
+import { FixedSizeArrayField } from '../fixed-size-array-field';
+import { PrimitiveField } from '../primitive-field';
+import { ISailsFuncArg, TypeDef } from '../../types';
+import { getLabel, getType } from '../../utils';
+
+type Props = {
+  sails: Sails;
+  args: ISailsFuncArg[];
+};
+
+function Fields({ sails, args }: Props) {
+  const getFieldComponent = (def: TypeDef) => {
+    if (def.isEnum) return EnumField;
+    if (def.isStruct) return StructField;
+    if (def.isOptional) return OptionalField;
+    if (def.isResult) return ResultField;
+    if (def.isVec) return VecField;
+    if (def.isMap) return MapField;
+    if (def.isFixedSizeArray) return FixedSizeArrayField;
+    if (def.isUserDefined) return UserDefinedField;
+    if (def.isPrimitive) return PrimitiveField;
+
+    throw new Error('Unknown field type: ' + JSON.stringify(def));
+  };
+
+  const renderField = (def: TypeDef, name: string = '') => {
+    if (!sails) throw new Error('Sails is not defined');
+    if (!def) return; // in case of empty enum variant, EnumVariant.def sails-js type is inaccurate at the moment
+
+    const key = generateRandomId();
+    const label = getLabel(name, getType(def));
+    const Field = getFieldComponent(def);
+
+    return <Field key={key} def={def} sails={sails} label={label} renderField={renderField} />;
+  };
+
+  return args.map(({ typeDef, name }) => renderField(typeDef, name));
+}
+
+export { Fields };
--- idea/frontend/src/features/sails/ui/payload-form/payload-form.tsx
+++ idea/frontend/src/features/sails/ui/payload-form/payload-form.tsx
@@ -3,21 +3,10 @@ import { Select } from '@gear-js/ui';
 import { useState, useEffect } from 'react';
 
 import { Fieldset } from '@/shared/ui';
-import { generateRandomId } from '@/shared/helpers';
 
 import { useParsedIdl } from '../../hooks';
-import { ISailsFuncArg, TypeDef } from '../../types';
-import { EnumField } from '../enum-field';
-import { UserDefinedField } from '../user-defined-field';
-import { StructField } from '../struct-field';
-import { OptionalField } from '../optional-field';
-import { ResultField } from '../result-field';
-import { VecField } from '../vec-field';
-import { MapField } from '../map-field';
-import { FixedSizeArrayField } from '../fixed-size-array-field';
-import { PrimitiveField } from '../primitive-field';
+import { Fields } from '../fields';
 import styles from './payload-form.module.scss';
-import { getLabel, getType } from '../../utils';
 
 type Props = {
   programId: HexString;
@@ -25,9 +14,19 @@ type Props = {
 
 function PayloadForm({ programId }: Props) {
   const { sails, idl } = useParsedIdl(programId);
+  const { services } = idl || {};
+  console.log('idl: ', idl);
 
-  const [serviceName] = Object.keys(idl?.services || {});
-  const functions = idl?.services[serviceName].functions || {};
+  const serviceNames = Object.keys(services || {});
+  const [defaultServiceName] = serviceNames;
+  const [serviceName, setServiceName] = useState<string>();
+
+  useEffect(() => {
+    setServiceName(defaultServiceName);
+  }, [defaultServiceName]);
+
+  const service = services && serviceName ? services[serviceName] : undefined;
+  const { functions } = service || {};
   const functionNames = Object.keys(functions || {});
   const [defaultFunctionName] = functionNames;
 
@@ -37,48 +36,29 @@ function PayloadForm({ programId }: Props) {
     setFunctionName(defaultFunctionName);
   }, [defaultFunctionName]);
 
-  const getFieldComponent = (def: TypeDef) => {
-    if (def.isEnum) return EnumField;
-    if (def.isStruct) return StructField;
-    if (def.isOptional) return OptionalField;
-    if (def.isResult) return ResultField;
-    if (def.isVec) return VecField;
-    if (def.isMap) return MapField;
-    if (def.isFixedSizeArray) return FixedSizeArrayField;
-    if (def.isUserDefined) return UserDefinedField;
-    if (def.isPrimitive) return PrimitiveField;
-
-    throw new Error('Unknown field type: ' + JSON.stringify(def));
-  };
-
-  const renderField = (def: TypeDef, name: string = '') => {
-    if (!sails) throw new Error('Sails is not defined');
-    if (!def) return; // in case of empty enum variant, EnumVariant.def sails-js type is inaccurate at the moment
-
-    const key = generateRandomId();
-    const label = getLabel(name, getType(def));
-    const Field = getFieldComponent(def);
-
-    return <Field key={key} def={def} sails={sails} label={label} renderField={renderField} />;
-  };
-
-  const renderFields = (args: ISailsFuncArg[]) =>
-    args.map(({ typeDef, name, type }) => {
-      // console.log('type: ', type);
-
-      return renderField(typeDef, name);
-    });
-
   return (
     <form className={styles.form}>
-      <Fieldset legend={serviceName}>
+      <Fieldset>
         <Select
+          label="Service"
+          direction="y"
+          options={serviceNames.map((name) => ({ label: name, value: name }))}
+          value={serviceName}
+          onChange={({ target }) => {
+            setServiceName(target.value);
+            setFunctionName(Object.keys(services?.[target.value].functions || {})[0]);
+          }}
+        />
+
+        <Select
+          label="Function"
+          direction="y"
           options={functionNames.map((name) => ({ label: name, value: name }))}
           value={functionName}
           onChange={({ target }) => setFunctionName(target.value)}
         />
 
-        {functionName && renderFields(functions[functionName].args)}
+        {sails && functions && functionName && <Fields sails={sails} args={functions[functionName].args} />}
       </Fieldset>
     </form>
   );
