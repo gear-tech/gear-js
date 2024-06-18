5f8ee70f8b4708b891b50e92f8d07e4f76e7a6b6
Nikita Yutanov <nikitayutanov@gmail.com>
2024-06-18 16:47:00 +0300

    Replace ga with gtm

--- idea/frontend/src/index.tsx
+++ idea/frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import ReactGA from 'react-ga4';
+import TagManager from 'react-gtm-module';
 
 import { App } from '@/app/App';
 
 import { GTM_ID } from './shared/config';
 
-if (GTM_ID) ReactGA.initialize(GTM_ID);
+if (GTM_ID) TagManager.initialize({ gtmId: GTM_ID });
 
 const container = document.getElementById('root');
 const root = createRoot(container as HTMLElement);
