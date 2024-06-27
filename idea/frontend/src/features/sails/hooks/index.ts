ca76d5ef43f4d8265725bef587e775a42d5c8260
Nikita Yutanov <nikitayutanov@gmail.com>
2024-06-27 18:08:00 +0300

    Remove leftover assets

--- idea/frontend/src/features/sails/hooks/index.ts
+++ idea/frontend/src/features/sails/hooks/index.ts
@@ -1,7 +1,5 @@
-import { useIdl } from './use-idl';
-import { useParsedIdl } from './use-parsed-idl';
 import { useConstructor, useService, useSetPayloadValue } from './payload';
 import { useSails } from './use-sails';
 import { useSailsWithFile } from './use-sails-with-file';
 
-export { useIdl, useParsedIdl, useConstructor, useService, useSetPayloadValue, useSails, useSailsWithFile };
+export { useConstructor, useService, useSetPayloadValue, useSails, useSailsWithFile };
--- idea/frontend/src/features/sails/index.ts
+++ idea/frontend/src/features/sails/index.ts
@@ -1,19 +1,9 @@
-import { useIdl, useSails, useSailsWithFile, useConstructor, useService } from './hooks';
+import { useSails, useSailsWithFile, useConstructor, useService } from './hooks';
 import { PayloadForm, IDL } from './ui';
 import { addIdl } from './api';
 import { getResetPayloadValue } from './utils';
 import { PayloadValue, PayloadValueSchema } from './types';
 
-export {
-  useIdl,
-  PayloadForm,
-  IDL,
-  useSails,
-  useSailsWithFile,
-  useConstructor,
-  useService,
-  addIdl,
-  getResetPayloadValue,
-};
+export { PayloadForm, IDL, useSails, useSailsWithFile, useConstructor, useService, addIdl, getResetPayloadValue };
 
 export type { PayloadValue, PayloadValueSchema };
