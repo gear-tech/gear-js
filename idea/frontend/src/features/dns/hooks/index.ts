30ac0a8b213f01be672c48ad6f6ad775cdf4b4b1
Vadim Tokar <vtokar@gear.foundation>
2024-07-02 12:47:03 +0400

    fix typo

--- idea/frontend/src/features/dns/hooks/index.ts
+++ idea/frontend/src/features/dns/hooks/index.ts
@@ -2,7 +2,7 @@ import { useDnsSort } from './use-dns-sort';
 import { useDns } from './use-dns';
 import { useDnsFilters } from './use-dns-filters';
 import { useDnsActions } from './use-dns-actions';
-import { useInitDnsProgram } from './use-itit-dns-program';
+import { useInitDnsProgram } from './use-init-dns-program';
 import { useDnsSchema } from './use-dns-schema';
 
 export { useDns, useDnsFilters, useDnsSort, useDnsActions, useInitDnsProgram, useDnsSchema };
