acffa6f52335374b571f55fc75d6de0e1a2487be
Nikita Yutanov <nikitayutanov@gmail.com>
2024-07-02 18:07:20 +0300

    Add useProgram hook

---
+++ utils/gear-hooks/src/hooks/sails/use-program.ts
@@ -0,0 +1,38 @@
+import { GearApi, HexString } from '@gear-js/api';
+import { UseQueryOptions, useQuery } from '@tanstack/react-query';
+import { useContext } from 'react';
+
+import { ApiContext } from 'context';
+
+type Program<T> = {
+  new (api: GearApi, programId?: HexString): T;
+};
+
+type QueryOptions<T> = Omit<UseQueryOptions<T, undefined, T, (string | undefined)[]>, 'queryKey' | 'queryFn'>;
+
+type UseProgramParameters<T> = {
+  library: Program<T>;
+  id: HexString | undefined;
+  query?: QueryOptions<T>;
+};
+
+function useProgram<T>({ library, id, query }: UseProgramParameters<T>) {
+  const { api, isApiReady } = useContext(ApiContext);
+
+  const getProgram = () => {
+    if (!isApiReady) throw new Error('API is not initialized');
+    if (!id) throw new Error('Program ID is not found');
+
+    return new library(api, id);
+  };
+
+  return useQuery({
+    ...query,
+    queryKey: ['program', id, api?.provider.endpoint],
+    queryFn: getProgram,
+    enabled: isApiReady && Boolean(id) && (query?.enabled ?? true),
+  });
+}
+
+export { useProgram };
+export type { UseProgramParameters };
