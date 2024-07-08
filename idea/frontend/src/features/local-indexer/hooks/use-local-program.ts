0e9a8b252d121fcc7117aeae98bd0451a8b56150
Nikita Yutanov <nikitayutanov@gmail.com>
2024-07-09 01:20:19 +0300

    Fix metadata request on each local program

--- idea/frontend/src/features/local-indexer/hooks/use-local-program.ts
+++ idea/frontend/src/features/local-indexer/hooks/use-local-program.ts
@@ -1,53 +1,45 @@
 import { useApi } from '@gear-js/react-hooks';
 import { HexString } from '@polkadot/util/types';
-import { ProgramMetadata } from '@gear-js/api';
 
 import { useProgramStatus } from '@/features/program';
-import { isState, useMetadata } from '@/features/metadata';
 
 import { PROGRAMS_LOCAL_FORAGE } from '../consts';
 import { LocalProgram } from '../types';
 
 function useLocalProgram() {
   const { api, isApiReady } = useApi();
-
-  const { getMetadata } = useMetadata();
   const { getProgramStatus } = useProgramStatus();
+
+  // TODO: useMetadataHash hook or util?
+  const getMetadataHash = async (id: HexString) => {
+    if (!isApiReady) throw new Error('API is not initialized');
+
+    try {
+      return await api.code.metaHash(id);
+    } catch {
+      return null;
+    }
+  };
+
+  const getCodeId = async (id: HexString) => {
+    if (!isApiReady) throw new Error('API is not initialized');
+
+    // cuz error on terminated program
+    try {
+      return await api.program.codeHash(id);
+    } catch {
+      return null;
+    }
+  };
 
   const getChainProgram = async (id: HexString) => {
     if (!isApiReady) return Promise.reject(new Error('API is not initialized'));
 
     const name = id;
     const status = await getProgramStatus(id);
-
-    let codeId: HexString | null;
-    let metahash: HexString | null;
-    let metaHex: HexString | null | undefined;
-
-    // cuz error on terminated program
-    try {
-      codeId = await api.program.codeHash(id);
-    } catch {
-      codeId = null;
-    }
-
-    try {
-      metahash = await api.code.metaHash(codeId || id);
-    } catch {
-      metahash = null;
-    }
-
-    // metadata is retrived via useMetadata, so no need to log errors here
-    try {
-      metaHex = metahash ? (await getMetadata(metahash)).result.hex : undefined;
-    } catch {
-      metaHex = null;
-    }
-
-    // TODO: on Programs page each program can make a request to backend,
-    // is there a way to optimize it?
-    const metadata = metaHex ? ProgramMetadata.from(metaHex) : undefined;
-    const hasState = isState(metadata);
+    const codeId = await getCodeId(id);
+    const metahash = await getMetadataHash(id);
+    const hasState = false;
 
     return { id, name, status, codeId, metahash, hasState };
   };
