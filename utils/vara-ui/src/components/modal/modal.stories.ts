4cd5f93aff0b6a18672eda275dd77821f5463371
Nikita Yutanov <nikitayutanov@gmail.com>
2024-09-27 17:35:14 +0300

    feat(vara-ui): mobile modal

--- utils/vara-ui/src/components/modal/modal.stories.ts
+++ utils/vara-ui/src/components/modal/modal.stories.ts
@@ -4,6 +4,9 @@ import { Modal } from './modal';
 type Type = typeof Modal;
 type Story = StoryObj<Type>;
 
+const LONG_TEXT =
+  'Text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text';
+
 const meta: Meta<Type> = {
   title: 'Modal',
   component: Modal,
@@ -20,11 +23,28 @@ const Default: Story = {
 const Scroll: Story = {
   args: {
     heading: 'Heading',
-    children:
-      'Text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text',
+    children: LONG_TEXT,
     footer: 'Footer',
   },
 };
 
+const Mobile: Story = {
+  args: {
+    heading: 'Mobile Modal',
+    children: 'Some modal text',
+    close: () => {},
+  },
+  parameters: { viewport: { defaultViewport: 'mobile1' } },
+};
+
+const MobileScroll: Story = {
+  args: {
+    heading: 'Mobile Modal',
+    children: LONG_TEXT,
+    close: () => {},
+  },
+  parameters: { viewport: { defaultViewport: 'mobile1' } },
+};
+
 export default meta;
-export { Default, Scroll };
+export { Default, Scroll, Mobile, MobileScroll };
