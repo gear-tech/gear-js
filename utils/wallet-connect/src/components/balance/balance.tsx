cdc8a1fffde6642a1232e076e923a865c83317de
Nikita Yutanov <nikitayutanov@gmail.com>
2024-10-01 18:13:13 +0300

    Add the ability to hide balance

---
+++ utils/wallet-connect/src/components/balance/balance.tsx
@@ -0,0 +1,33 @@
+import { useAccount, useApi, useBalanceFormat, useDeriveBalancesAll } from '@gear-js/react-hooks';
+import cx from 'clsx';
+
+import { ReactComponent as VaraSVG } from '../../assets/vara.svg';
+import styles from './balance.module.css';
+
+type Props = {
+  variant: 'gear' | 'vara';
+};
+
+function Balance({ variant }: Props) {
+  const { isApiReady } = useApi();
+  const { account } = useAccount();
+  const { getFormattedBalance } = useBalanceFormat();
+
+  const { data: balances } = useDeriveBalancesAll({ address: account?.decodedAddress, watch: true });
+  const balance = isApiReady && balances ? getFormattedBalance(balances.freeBalance) : undefined;
+
+  if (!balance) return null;
+
+  return (
+    <div className={styles.balance}>
+      <VaraSVG />
+
+      <p className={cx(styles.text, styles[variant])}>
+        <span className={styles.value}>{balance.value}</span>
+        <span className={styles.unit}>{balance.unit}</span>
+      </p>
+    </div>
+  );
+}
+
+export { Balance };
--- utils/wallet-connect/src/components/wallet/wallet.tsx
+++ utils/wallet-connect/src/components/wallet/wallet.tsx
@@ -1,25 +1,21 @@
-import { useAccount, useApi, useBalanceFormat, useDeriveBalancesAll } from '@gear-js/react-hooks';
-import cx from 'clsx';
+import { useAccount } from '@gear-js/react-hooks';
 import { useState } from 'react';
 
-import { ReactComponent as VaraSVG } from '../../assets/vara.svg';
+import { Balance } from '../balance';
 import { WalletModal } from '../wallet-modal';
 import { UI_CONFIG } from '../ui-config';
 import styles from './wallet.module.css';
 
 type Props = {
   variant?: 'gear' | 'vara';
-  // temp solution to support responsiveness in MenuHandler, until it's supported here
+  displayBalance?: boolean;
+
+  // temp solution to support responsiveness in dApps MenuHandler, until it's supported here
   accountButtonClassName?: string;
 };
 
-function Wallet({ variant = 'vara', accountButtonClassName }: Props) {
-  const { isApiReady } = useApi();
+function Wallet({ variant = 'vara', displayBalance = true, accountButtonClassName }: Props) {
   const { account, isAccountReady } = useAccount();
-
-  const { getFormattedBalance } = useBalanceFormat();
-  const { data: balances } = useDeriveBalancesAll({ address: account?.decodedAddress, watch: true });
-  const balance = isApiReady && balances ? getFormattedBalance(balances.freeBalance) : undefined;
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const openModal = () => setIsModalOpen(true);
@@ -31,16 +27,7 @@ function Wallet({ variant = 'vara', accountButtonClassName }: Props) {
   return (
     <>
       <div className={styles.wallet}>
-        {balance && (
-          <div className={styles.balance}>
-            <VaraSVG />
-
-            <p className={cx(styles.text, styles[variant])}>
-              <span className={styles.value}>{balance.value}</span>
-              <span className={styles.unit}>{balance.unit}</span>
-            </p>
-          </div>
-        )}
+        {displayBalance && <Balance variant={variant} />}
 
         {account ? (
           <div className={accountButtonClassName}>
