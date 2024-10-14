6bddf3c0ac579a08743b11615b96b967fb72f62d
Nikita Yutanov <nikitayutanov@gmail.com>
2024-10-14 15:02:51 +0300

    fix(idea/frontend): specific network dns requests

--- idea/frontend/src/features/dns/hooks/use-dns-schema.ts
+++ idea/frontend/src/features/dns/hooks/use-dns-schema.ts
@@ -1,24 +1,30 @@
+import { useApi } from '@gear-js/react-hooks';
 import { z } from 'zod';
 
 import { useProgramIdSchema } from '@/hooks';
+
 import { FIELD_NAME, NAME_SCHEMA } from '../consts';
 import { getSingleDns } from '../utils';
 
 const useDnsSchema = (isEditMode?: boolean) => {
+  const { api, isApiReady } = useApi();
   const programIdSchema = useProgramIdSchema([]);
 
-  const dnsNameSchema = isEditMode
-    ? NAME_SCHEMA
-    : NAME_SCHEMA.refine(async (name) => {
-        const result = await getSingleDns({ name }).catch(() => {
-          // empty
-        });
-        return !result;
-      }, 'dDNS name already exists');
+  const getNameSchema = () => {
+    if (!isApiReady) return z.string();
+    const genesis = api.genesisHash.toHex();
+
+    return isEditMode
+      ? NAME_SCHEMA
+      : NAME_SCHEMA.refine(async (name) => {
+          const result = await getSingleDns({ name, genesis }).catch(() => {});
+          return !result;
+        }, 'dDNS name already exists');
+  };
 
   const dnsSchema = z.object({
     [FIELD_NAME.DNS_ADDRESS]: programIdSchema,
-    [FIELD_NAME.DNS_NAME]: dnsNameSchema,
+    [FIELD_NAME.DNS_NAME]: getNameSchema(),
   });
 
   return dnsSchema;
--- idea/frontend/src/features/dns/hooks/use-single-dns.ts
+++ idea/frontend/src/features/dns/hooks/use-single-dns.ts
@@ -1,12 +1,23 @@
+import { useApi } from '@gear-js/react-hooks';
 import { HexString } from '@gear-js/api';
 import { useQuery } from '@tanstack/react-query';
 
 import { getSingleDns } from '../utils';
 
 function useSingleDns(address: HexString) {
+  const { api, isApiReady } = useApi();
+
+  const getQuery = () => {
+    if (!isApiReady) throw new Error('Genesis hash is not found');
+    const genesis = api.genesisHash.toHex();
+
+    return getSingleDns({ address, genesis });
+  };
+
   return useQuery({
     queryKey: ['single-dns', address],
-    queryFn: () => getSingleDns({ address }),
+    queryFn: getQuery,
+    enabled: Boolean(isApiReady),
   });
 }
 
