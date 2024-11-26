ee066ecea91e96e772c52eb7b2e45ed879846e7a
Eugene Kotsarev <gimmethenick@gmail.com>
2024-11-26 18:27:28 +0300

    chore: bump packages cause' of vulnerabilities

--- utils/vara-ui/vite.config.ts
+++ utils/vara-ui/vite.config.ts
@@ -6,7 +6,7 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), dts()],
+  plugins: [react(), svgr(), dts({ exclude: ['**/*.stories.tsx'] })],
 
   build: {
     lib: {
