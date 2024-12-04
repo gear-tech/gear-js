e84f47559a17f2d53351fac37300a20bdb2fbf7b
Eugene Kotsarev <gimmethenick@gmail.com>
2024-12-04 16:38:39 +0300

    feat(vara-ui): upd button

---
+++ utils/vara-ui/src/components/button/button.stories.tsx
@@ -0,0 +1,59 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Button } from './button';
+import ArrowIcon from '../../assets/images/arrow-right.svg?react';
+import LightningIcon from '../../assets/images/lightning.svg?react';
+import { buttonColors, buttonSizes } from './helpers.ts';
+
+type Type = typeof Button;
+type Story = StoryObj<Type>;
+
+const meta: Meta<Type> = {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Click me!',
+    color: 'primary',
+    size: 'default',
+    disabled: false,
+    isLoading: false,
+    block: false,
+    noWrap: false,
+  },
+  argTypes: {
+    disabled: { control: 'boolean' },
+    isLoading: { control: 'boolean' },
+    block: { control: 'boolean' },
+    noWrap: { control: 'boolean' },
+    size: {
+      options: buttonSizes,
+      control: { type: 'select' },
+    },
+    color: {
+      options: buttonColors,
+      control: { type: 'select' },
+    },
+  },
+};
+
+export const Default: Story = {};
+export const Border: Story = { args: { color: 'border' } };
+export const Grey: Story = { args: { color: 'grey' } };
+export const Transparent: Story = { args: { color: 'transparent' } };
+export const Dark: Story = { args: { color: 'dark' } };
+export const Light: Story = { args: { color: 'light' } };
+export const Destructive: Story = { args: { color: 'destructive' } };
+export const NoText: Story = { args: { color: 'primary', children: undefined, icon: ArrowIcon } };
+export const WithChildrenAndIcons: Story = {
+  args: {
+    color: 'primary',
+    children: (
+      <>
+        <LightningIcon />
+        Hello!
+        <LightningIcon />
+      </>
+    ),
+  },
+};
+
+export default meta;
--- utils/vara-ui/src/components/button/button.tsx
+++ utils/vara-ui/src/components/button/button.tsx
@@ -1,12 +1,13 @@
 import { ButtonHTMLAttributes, FunctionComponent, ReactNode, SVGProps, forwardRef } from 'react';
 import cx from 'clsx';
 import styles from './button.module.scss';
+import type { IButtonColors, IButtonSizes } from './helpers.ts';
 
 type BaseProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   text?: string;
   icon?: FunctionComponent<SVGProps<SVGSVGElement> & { title?: string | undefined }>;
-  color?: 'primary' | 'dark' | 'light' | 'grey' | 'border' | 'transparent';
-  size?: 'default' | 'small';
+  color?: IButtonColors;
+  size?: IButtonSizes;
   isLoading?: boolean;
   block?: boolean;
   noWrap?: boolean;
@@ -52,7 +53,7 @@ const Button = forwardRef<HTMLButtonElement, Props>((props, ref) => {
       className={cx(
         styles.button,
         styles[color],
-        color !== 'transparent' && styles[size],
+        styles[size],
         disabled && styles.disabled,
         isLoading && styles.loading,
         !text && !children && styles.noText,
