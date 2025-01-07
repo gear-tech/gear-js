8e4fa151e5a74f4feb437482de7198d18a4434f4
Nikita Yutanov <nikitayutanov@gmail.com>
2025-01-07 17:21:47 +0300

    refactor dark mode mixin

--- utils/vara-ui/src/components/alert/index.ts
+++ utils/vara-ui/src/components/alert/index.ts
@@ -1,6 +1,5 @@
-import { Alert, AlertProps, AlertTypes, AlertVariants } from './alert';
+import { Alert, AlertProps } from './alert';
 import alertStyles from './alert.module.scss';
-import { alertIcons } from './icons.tsx';
 
-export { Alert, alertStyles, alertIcons };
-export type { AlertProps, AlertTypes, AlertVariants };
+export { Alert, alertStyles };
+export type { AlertProps };
