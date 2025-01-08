d3b184a57b6a5edfc2220430be6af78900b3a30a
Nikita Yutanov <nikitayutanov@gmail.com>
2025-01-08 18:30:21 +0300

    refactor switch

--- utils/vara-ui/src/components/checkbox/checkbox.tsx
+++ utils/vara-ui/src/components/checkbox/checkbox.tsx
@@ -13,15 +13,9 @@ type Props = Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> & {
 const Checkbox = forwardRef<HTMLInputElement, Props>(
   ({ label, className, type = 'checkbox', size = 'default', error, ...attrs }, ref) => {
     return (
-      <label className={cx(styles.label, className, styles[size])}>
+      <label className={cx(styles.label, className, styles[type], styles[size])}>
         <span className={styles.inputWrapper}>
-          <input
-            type="checkbox"
-            className={cx(styles.input, styles[type])}
-            ref={ref}
-            aria-invalid={Boolean(error)}
-            {...attrs}
-          />
+          <input type="checkbox" className={styles.input} ref={ref} aria-invalid={Boolean(error)} {...attrs} />
         </span>
 
         {label}
