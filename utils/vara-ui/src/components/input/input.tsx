0774c8c5d1dc7a8c6ea60c7d8614c26b2a5d8a63
Nikita Yutanov <nikitayutanov@gmail.com>
2025-01-13 23:16:42 +0300

    add label container component

--- utils/vara-ui/src/components/input/input.tsx
+++ utils/vara-ui/src/components/input/input.tsx
@@ -1,30 +1,21 @@
-import { InputHTMLAttributes, ReactNode, forwardRef, FunctionComponent, SVGProps } from 'react';
-import cx from 'clsx';
+import { InputHTMLAttributes, forwardRef, FunctionComponent, SVGProps } from 'react';
 
+import { LabelContainer, LabelContainerProps } from '../label-container';
 import styles from './input.module.scss';
 
-type Props = Omit<InputHTMLAttributes<HTMLInputElement>, 'id' | 'size'> & {
-  icon?: FunctionComponent<SVGProps<SVGSVGElement> & { title?: string | undefined }>;
-  size?: 'small' | 'medium' | 'large';
-  label?: string;
-  error?: ReactNode;
-  block?: boolean;
-};
+type Props = Omit<InputHTMLAttributes<HTMLInputElement>, 'id' | 'size'> &
+  LabelContainerProps & {
+    icon?: FunctionComponent<SVGProps<SVGSVGElement> & { title?: string | undefined }>;
+  };
 
 const Input = forwardRef<HTMLInputElement, Props>(
-  ({ icon: Icon, className, label, error, type = 'text', size = 'medium', block, ...attrs }, ref) => {
+  ({ icon: Icon, className, label, error, type = 'text', size, block, ...attrs }, ref) => {
     return (
-      <label className={cx(styles.container, styles[size], className, block && styles.block)}>
-        {label && <span className={styles.label}>{label}</span>}
+      <LabelContainer className={className} label={label} error={error} size={size} block={block}>
+        {Icon && <Icon className={styles.icon} />}
 
-        <span className={styles.inputWrapper}>
-          {Icon && <Icon className={styles.icon} />}
-
-          <input className={styles.input} type={type} ref={ref} aria-invalid={Boolean(error)} {...attrs} />
-        </span>
-
-        {error && <span className={styles.error}>{error}</span>}
-      </label>
+        <input className={styles.input} type={type} ref={ref} aria-invalid={Boolean(error)} {...attrs} />
+      </LabelContainer>
     );
   },
 );
