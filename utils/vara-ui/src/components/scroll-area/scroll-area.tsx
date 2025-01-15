30f01f490381e978dd6850001c7fc6914121319e
Nikita Yutanov <nikitayutanov@gmail.com>
2025-01-16 01:07:30 +0300

    add `as` prop

--- utils/vara-ui/src/components/scroll-area/scroll-area.tsx
+++ utils/vara-ui/src/components/scroll-area/scroll-area.tsx
@@ -1,14 +1,22 @@
 import cx from 'clsx';
-import { PropsWithChildren } from 'react';
+import { ComponentPropsWithoutRef, ElementType, PropsWithChildren } from 'react';
 
 import styles from './scroll-area.module.scss';
 
-type Props = PropsWithChildren & {
-  className?: string;
-};
+type Props<T extends ElementType> = PropsWithChildren &
+  ComponentPropsWithoutRef<T> & {
+    as?: T;
+    className?: string;
+  };
 
-function ScrollArea({ children, className }: Props) {
-  return <div className={cx(styles.container, className)}>{children}</div>;
+function ScrollArea<T extends ElementType = 'div'>({ as, children, className, ...attrs }: Props<T>) {
+  const Element = as || 'div';
+
+  return (
+    <Element className={cx(styles.container, className)} {...attrs}>
+      {children}
+    </Element>
+  );
 }
 
 export { ScrollArea };
