c77c3167ab28a2c33539dbb66f7f3bccbc1ac1d5
Nikita Yutanov <nikitayutanov@gmail.com>
2025-01-16 17:28:10 +0300

    add logic

--- utils/vara-ui/src/components/tooltip/tooltip.tsx
+++ utils/vara-ui/src/components/tooltip/tooltip.tsx
@@ -1,11 +1,56 @@
-import { PropsWithChildren } from 'react';
+import { cloneElement, CSSProperties, ReactNode, ReactElement, useState } from 'react';
 
-import styles from './tooltip.module.css';
+import { useRootPortal } from '../../hooks';
+import { getPosition } from './get-position';
+import styles from './tooltip.module.scss';
 
-type Props = PropsWithChildren;
+type Props = {
+  children: ReactElement;
+  value?: ReactNode;
+  position?:
+    | 'top-start'
+    | 'top'
+    | 'top-end'
+    | 'right-start'
+    | 'right'
+    | 'right-end'
+    | 'bottom-start'
+    | 'bottom'
+    | 'bottom-end'
+    | 'left-start'
+    | 'left'
+    | 'left-end';
+};
 
-function Tooltip() {
-  return null;
+function TooltipComponent({ value, style }: { value: ReactNode; style: CSSProperties }) {
+  return useRootPortal(
+    'tooltip-root',
+    <div className={styles.tooltip} style={style}>
+      {value}
+    </div>,
+  );
+}
+
+function Tooltip({ value, position = 'top', children }: Props) {
+  const [style, setStyle] = useState<CSSProperties>();
+
+  const handleMouseOver = (event: MouseEvent) => {
+    const anchor = event.target as HTMLElement | null;
+    if (!anchor) return;
+
+    setStyle(getPosition(anchor, position));
+  };
+
+  return (
+    <>
+      {cloneElement(children, {
+        onMouseOver: handleMouseOver,
+        onMouseLeave: () => setStyle(undefined),
+      })}
+
+      {style && <TooltipComponent value={value} style={style} />}
+    </>
+  );
 }
 
 export { Tooltip };
