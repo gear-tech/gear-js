1dc2880bf92c7ae3a706bc9c426da0c84f60f59f
Dmitry Osipov <dmith.osipov@gmail.com>
2025-01-22 17:44:14 +0700

    fix: hooks build

--- utils/gear-hooks/rollup.config.mjs
+++ utils/gear-hooks/rollup.config.mjs
@@ -1,15 +1,24 @@
 import packageJson from './package.json' with { type: 'json' };
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default [
   {
     input: 'src/index.ts',
-    output: [ { file: packageJson.main, format: 'esm', inlineDynamicImports: true },
+    output: [{ file: packageJson.main, format: 'esm', inlineDynamicImports: true }],
+    plugins: [
+      peerDepsExternal(),
+      resolve({
+        resolveOnly: (module) => !module.includes('polkadot'),
+      }),
+      commonjs(),
+      typescript({
+        tsconfig: 'tsconfig.json',
+      }),
+      terser(),
     ],
-    plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), terser()],
   },
 ];
