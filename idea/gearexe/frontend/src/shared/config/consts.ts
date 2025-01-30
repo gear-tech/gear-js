75287f6b0044f2964d68da94145d36b0e2dbc7ae
Vadim Tokar <vtokar@gear.foundation>
2025-01-30 14:56:04 +0400

    update envs

--- idea/gearexe/frontend/src/shared/config/consts.ts
+++ idea/gearexe/frontend/src/shared/config/consts.ts
@@ -1,6 +1,6 @@
 export const PROJECT_ID = import.meta.env
   .VITE_WALLET_CONNECT_PROJECT_ID as string;
-export const GEAR_API_NODE = import.meta.env.VITE_GEAR_API_NODE as string;
+export const GEAR_API_NODE = import.meta.env.VITE_GEAR_EXE_API_NODE as string;
 export const ETH_CHAIN_ID = 17000; // (0x4268) Holesky
 export const ETH_NODE_ADDRESS = import.meta.env.VITE_ETH_NODE_ADDRESS;
 
