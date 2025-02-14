ba72b84512825fd21711a0c0cb5ce3cfc92377eb
Nikita Yutanov <nikitayutanov@gmail.com>
2025-02-14 16:01:46 +0300

    replace idea configs

--- idea/gear/frontend/vite.config.ts
+++ idea/gear/frontend/vite.config.ts
@@ -1,12 +1,3 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import checker from 'vite-plugin-checker';
+import { viteAppConfig } from '@gear-js/frontend-configs';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr(), nodePolyfills(), checker({ typescript: true })],
-
-  resolve: { alias: { '@': '/src' } },
-});
+export default viteAppConfig;
