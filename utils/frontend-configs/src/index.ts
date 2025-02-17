564b109d6cc7fe419e7c897cf0b8721c52ce1c09
Nikita Yutanov <nikitayutanov@gmail.com>
2025-02-18 00:39:30 +0300

    add type safe prettier config

--- utils/frontend-configs/src/index.ts
+++ utils/frontend-configs/src/index.ts
@@ -1,4 +1,5 @@
 import { eslintConfig } from './eslint.js';
 import { viteConfigs } from './vite.js';
+import { prettierConfig } from './prettier.js';
 
-export { eslintConfig, viteConfigs };
+export { eslintConfig, viteConfigs, prettierConfig };
