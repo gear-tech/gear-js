d2a856b56701559220b220f28a79ef6b960c1229
Nikita Yutanov <nikitayutanov@gmail.com>
2025-02-21 18:01:26 +0300

    fix types build

--- utils/gear-ui/vitest.setup.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom/vitest';
-import { cleanup } from '@testing-library/react';
-import { afterEach, vi } from 'vitest';
-
-afterEach(() => {
-  vi.clearAllMocks();
-  cleanup();
-});
