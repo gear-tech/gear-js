1b3451c77f56ea2990d7d9a799884cdcc2ff1310
Nikita Yutanov <nikitayutanov@gmail.com>
2025-03-19 21:06:16 +0300

    chore(frontend-configs): release `0.1.1` and bump deps

--- utils/frontend-configs/src/eslint.ts
+++ utils/frontend-configs/src/eslint.ts
@@ -1,7 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import react from 'eslint-plugin-react';
-// @ts-expect-error - WiP: https://github.com/facebook/react/issues/30119
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
