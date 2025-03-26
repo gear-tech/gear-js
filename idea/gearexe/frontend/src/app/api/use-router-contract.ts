8201881b1e940fb80208cc1ef5f87567ee775b26
Vadim Tokar <vtokar@gear.foundation>
2025-03-26 10:29:52 +0700

    add ci envs

--- idea/gearexe/frontend/src/app/api/use-router-contract.ts
+++ idea/gearexe/frontend/src/app/api/use-router-contract.ts
@@ -1,9 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
-import { ethers } from 'ethers';
 import { getRouterContract } from 'gearexe';
 import { useAccount } from 'wagmi';
 
-import { ETH_NODE_ADDRESS, ROUTER_CONTRACT_ADDRESS } from '@/shared/config';
+import { ROUTER_CONTRACT_ADDRESS } from '@/shared/config';
 
 import { useEthersSigner } from './ethers/signer';
 
@@ -13,16 +12,16 @@ const useRouterContract = () => {
 
   const { data: routerContract, isLoading } = useQuery({
     queryKey: ['getRouterContract', signer],
-    // queryFn: () => getRouterContract(ROUTER_CONTRACT_ADDRESS, signer),
-    // TODO: Temporary solution. MetaMask doesn't support blob transactions.
-    queryFn: () =>
-      getRouterContract(
-        ROUTER_CONTRACT_ADDRESS,
-        new ethers.Wallet(
-          import.meta.env.VITE_SEQUENCER_PRIVATE_KEY as string,
-          new ethers.WebSocketProvider(ETH_NODE_ADDRESS),
-        ),
-      ),
+    queryFn: () => getRouterContract(ROUTER_CONTRACT_ADDRESS, signer),
+    // TODO: Temporary solution. MetaMask doesn't support blob transactions. Used for testing.
+    // queryFn: () =>
+    //   getRouterContract(
+    //     ROUTER_CONTRACT_ADDRESS,
+    //     new ethers.Wallet(
+    //       import.meta.env.VITE_SEQUENCER_PRIVATE_KEY as string,
+    //       new ethers.WebSocketProvider(ETH_NODE_ADDRESS),
+    //     ),
+    //   ),
     enabled: Boolean(signer),
   });
 
