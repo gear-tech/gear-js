a6a97424dee76bbe348b37a5e3ee32b555160e64
Vadim Tokar <vtokar@gear.foundation>
2025-04-21 13:20:07 +0300

    feat(gear-idea/frontend): use replyCode

--- idea/gear/frontend/src/hooks/useEventSubscriptions/helpers.tsx
+++ idea/gear/frontend/src/hooks/useEventSubscriptions/helpers.tsx
@@ -1,4 +1,4 @@
-import { UserMessageSent, Transfer, encodeAddress } from '@gear-js/api';
+import { UserMessageSent, Transfer, encodeAddress, ReplyCode, GearApi } from '@gear-js/api';
 import { AlertContainerFactory } from '@gear-js/react-hooks';
 import { HexString } from '@polkadot/util/types';
 import { generatePath } from 'react-router-dom';
@@ -6,7 +6,12 @@ import { generatePath } from 'react-router-dom';
 import { routes } from '@/shared/config';
 import { CustomLink } from '@/shared/ui/customLink';
 
-const messageSentEventsHandler = (event: UserMessageSent, address: HexString, alert: AlertContainerFactory) => {
+const messageSentEventsHandler = (
+  event: UserMessageSent,
+  address: HexString,
+  alert: AlertContainerFactory,
+  api: GearApi,
+) => {
   const { message, method, section } = event.data;
   const { payload, destination, details, id } = message;
 
@@ -17,17 +22,29 @@ const messageSentEventsHandler = (event: UserMessageSent, address: HexString, al
   const messageId = id.toHex();
   const alertOptions = { title: `${section}.${method}` };
 
-  const isError = details.isSome && !details.unwrap().code.isSuccess;
+  const code = details.unwrap().code;
+  const isError = details.isSome && !code.isSuccess;
+  const replyCode = new ReplyCode(code.toU8a(), api.specVersion);
 
   // eslint-disable-next-line @typescript-eslint/unbound-method -- TODO(#1800): resolve eslint comments
   const showAlert = isError ? alert.error : alert.success;
+
+  const getErrorReason = () => {
+    if (replyCode.errorReason.executionReason.isUserspacePanic) {
+      return payload.toHuman() as string;
+    }
+    if (replyCode.errorReason.isExecution) {
+      return replyCode.errorReason.executionReason.explanation;
+    }
+    return replyCode.errorReason.explanation;
+  };
 
   showAlert(
     <>
       <p>
         ID: <CustomLink to={generatePath(routes.message, { messageId })} text={messageId} />
       </p>
-      {isError && <p>{payload.toHuman() as string}</p>}
+      {isError && <p>{getErrorReason()}</p>}
     </>,
     alertOptions,
   );
