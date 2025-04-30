be17bd17caa1c887225727d836ce152ae8692cc2
Dmitri Osipov <dmith.osipov@gmail.com>
2025-04-30 13:44:21 +0200

    fix(gear-idea-faucet): accept vara-encoded addresses (#1910)

--- idea/gear/faucet/tests/testnet.test.ts
+++ idea/gear/faucet/tests/testnet.test.ts
@@ -6,8 +6,11 @@ import { FaucetApp } from '../src/main';
 import { repos } from './__mocks__/db';
 import { RequestStatus } from '../src/database';
 import { hash } from '../src/services/db/last-seen';
+import { Keyring } from '@polkadot/api';
+import { decodeAddress } from '@gear-js/api';
 
-const VARA_USER_ADDRESS = '0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d';
+const ALICE = '0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d';
+const BOB = 'kGim5ByTuPokQf21odiQskRXcVEwaunk5PwC4dmGz8M6zuwkq';
 const VARA_GENESIS = '0x0000000000000000000000000000000000000000000000000000000000000000';
 
 describe('Testnet requests', () => {
@@ -29,7 +32,7 @@ describe('Testnet requests', () => {
   });
 
   it('should return an error about missing fields', async () => {
-    let res = await req(VARA_USER_ADDRESS);
+    let res = await req(ALICE);
 
     expect(res.statusCode).toBe(400);
     expect(res.body).toEqual({ error: 'Address and genesis are required' });
@@ -41,7 +44,7 @@ describe('Testnet requests', () => {
   });
 
   it('should return UnsupportedTarget error', async () => {
-    const res = await req(VARA_USER_ADDRESS, '0x1234');
+    const res = await req(ALICE, '0x1234');
 
     expect(res.statusCode).toBe(400);
     expect(res.body).toEqual({ error: '0x1234 is not supported' });
@@ -54,8 +57,8 @@ describe('Testnet requests', () => {
     expect(res.body).toEqual({ error: 'Invalid account address' });
   });
 
-  it('should return ok', async () => {
-    const res = await req(VARA_USER_ADDRESS, VARA_GENESIS);
+  it('should return ok (Alice)', async () => {
+    const res = await req(ALICE, VARA_GENESIS);
 
     expect(res.statusCode).toBe(200);
 
@@ -63,7 +66,7 @@ describe('Testnet requests', () => {
 
     expect(faucetRequestData).toHaveProperty('1');
     expect(faucetRequestData[1]).toHaveProperty('id', 1);
-    expect(faucetRequestData[1]).toHaveProperty('address', VARA_USER_ADDRESS);
+    expect(faucetRequestData[1]).toHaveProperty('address', ALICE);
     expect(faucetRequestData[1]).toHaveProperty('target', VARA_GENESIS);
     expect(faucetRequestData[1]).toHaveProperty('status', RequestStatus.Pending);
 
@@ -77,11 +80,56 @@ describe('Testnet requests', () => {
     expect(faucetRequestData[1].status).toBe(RequestStatus.Completed);
 
     let userLastSeenData = repos.UserLastSeen._data();
-    expect(userLastSeenData).toHaveProperty(hash(VARA_USER_ADDRESS, VARA_GENESIS));
+    expect(userLastSeenData).toHaveProperty(hash(ALICE, VARA_GENESIS));
   });
 
-  it('should return request limit error', async () => {
-    const res = await req(VARA_USER_ADDRESS, VARA_GENESIS);
+  it('should return request limit error (Alice)', async () => {
+    const addr = new Keyring({ ss58Format: 137 }).encodeAddress(ALICE);
+    let res = await req(addr, VARA_GENESIS);
+
+    expect(res.statusCode).toBe(403);
+    expect(res.body).toEqual({ error: 'The limit for requesting test balance has been reached.' });
+
+    res = await req(ALICE, VARA_GENESIS);
+
+    expect(res.statusCode).toBe(403);
+    expect(res.body).toEqual({ error: 'The limit for requesting test balance has been reached.' });
+  });
+
+  it('should return ok (Bob)', async () => {
+    const res = await req(BOB, VARA_GENESIS);
+
+    expect(res.statusCode).toBe(200);
+
+    let faucetRequestData = repos.FaucetRequest._data();
+
+    expect(faucetRequestData).toHaveProperty('1');
+    expect(faucetRequestData[2]).toHaveProperty('id', 2);
+    expect(faucetRequestData[2]).toHaveProperty('address', decodeAddress(BOB));
+    expect(faucetRequestData[2]).toHaveProperty('target', VARA_GENESIS);
+    expect(faucetRequestData[2]).toHaveProperty('status', RequestStatus.Pending);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    faucetRequestData = repos.FaucetRequest._data();
+    expect(faucetRequestData[2].status).toBe(RequestStatus.Processing);
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    faucetRequestData = repos.FaucetRequest._data();
+    expect(faucetRequestData[2].status).toBe(RequestStatus.Completed);
+
+    let userLastSeenData = repos.UserLastSeen._data();
+    expect(userLastSeenData).toHaveProperty(hash(decodeAddress(BOB), VARA_GENESIS));
+  });
+
+  it('should return request limit error (Bob)', async () => {
+    const addr = decodeAddress(BOB);
+    let res = await req(addr, VARA_GENESIS);
+
+    expect(res.statusCode).toBe(403);
+    expect(res.body).toEqual({ error: 'The limit for requesting test balance has been reached.' });
+
+    res = await req(BOB, VARA_GENESIS);
 
     expect(res.statusCode).toBe(403);
     expect(res.body).toEqual({ error: 'The limit for requesting test balance has been reached.' });
