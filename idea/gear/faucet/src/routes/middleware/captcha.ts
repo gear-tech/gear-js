c9e525d0a50c9b6666994e83f944753ebb73fcab
Dmitri Osipov <dmith.osipov@gmail.com>
2025-05-04 10:32:34 +0200

    add some logs

--- idea/gear/faucet/src/routes/middleware/captcha.ts
+++ idea/gear/faucet/src/routes/middleware/captcha.ts
@@ -2,10 +2,12 @@ import { Response, Request, NextFunction } from 'express';
 import { verify } from 'hcaptcha';
 
 import config from '../../config';
+import { logger } from 'gear-idea-common';
 
 const SECRET = config.server.captchaSecret;
 
 async function verifyCaptcha(token: string): Promise<boolean> {
+  logger.debug('verify captcha', { token });
   if (!token) {
     return false;
   }
@@ -14,12 +16,14 @@ async function verifyCaptcha(token: string): Promise<boolean> {
     return true;
   }
 
-  const verfied = await verify(SECRET, token);
+  const verifResponse = await verify(SECRET, token);
+  logger.debug('verify captcha', { verifResponse });
 
-  return verfied.success;
+  return verifResponse.success;
 }
 
 export function captchaMiddleware({ body: { token } }: Request, res: Response, next: NextFunction) {
+  logger.debug('captcha', { token });
   verifyCaptcha(token).then((result) => {
     if (result) {
       next();
--- idea/gear/faucet/src/routes/middleware/log.ts
+++ idea/gear/faucet/src/routes/middleware/log.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import { logger } from 'gear-idea-common';
 
 export function requestLoggerMiddleware(req: Request, _: Response, next: NextFunction) {
-  logger.info(`${req.path}`, req.body);
+  if (req.body) {
+    logger.info(`${req.path}`, { token: req.body.token?.slice(0, 10), ...req.body });
+  }
   return next();
 }
