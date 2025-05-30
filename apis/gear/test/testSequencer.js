5281e62663887dc5bb44ccadf059d1752337de3b
Dmitri Osipov <dmith.osipov@gmail.com>
2025-05-30 13:56:24 +0200

    fix: test sequencer

--- apis/gear/test/testSequencer.js
+++ apis/gear/test/testSequencer.js
@@ -2,29 +2,28 @@ import * as Sequencer from '@jest/test-sequencer';
 
 export default class CustomSequencer extends Sequencer.default.default {
   sort(tests) {
+    if (tests.length <= 1) return tests;
+
     const copyTests = Array.from(tests);
-    const result = new Array(copyTests.length);
 
-    const apiTest = tests.find(({ path }) => path.includes('GearApi'));
-    const codeTest = tests.find(({ path }) => path.includes('Code'));
+    const apiTest = copyTests.find(({ path }) => path.endsWith('GearApi.test.ts'));
+    const codeTest = copyTests.find(({ path }) => path.endsWith('Code.test.ts'));
+    const messageTest = copyTests.find(({ path }) => path.endsWith('Message.test.ts'));
 
-    let counter = 0;
+    const remainingTests = copyTests.filter(
+      (test) =>
+        !test.path.endsWith('GearApi.test.ts') &&
+        !test.path.endsWith('Code.test.ts') &&
+        !test.path.endsWith('Message.test.ts'),
+    );
 
-    if (apiTest && tests.length > 1) counter++;
-    if (codeTest && tests.length > 1) counter++;
+    const result = [];
 
-    for (const test of copyTests) {
-      if (test.path.includes('GearApi') && tests.length > 1) {
-        result[0] = test;
-        continue;
-      } else if (test.path.includes('Code') && tests.length > 1) {
-        result[1] = test;
-        continue;
-      } else {
-        result[counter] = test;
-        counter++;
-      }
-    }
+    if (apiTest) result.push(apiTest);
+    if (codeTest) result.push(codeTest);
+    if (messageTest) result.push(messageTest);
+
+    result.push(...remainingTests);
 
     return result;
   }
