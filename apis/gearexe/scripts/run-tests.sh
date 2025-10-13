6d884638e2d082e77cccb2b8cbf79aec153e436b
Dmitri Osipov <dmith.osipov@gmail.com>
2025-10-13 15:07:46 +0200

    fix: test script

--- apis/gearexe/scripts/run-tests.sh
+++ apis/gearexe/scripts/run-tests.sh
@@ -69,21 +69,11 @@ compile_contracts() {
 # Function to run tests
 run_tests() {
     log_info "Running test suite..."
-    cd $ROOT_DIR
-
-    log_info "Locating Jest binary..."
-    jest_bin=$(yarn bin jest)
-    if [ $? -ne 0 ]; then
-        log_error "Failed to locate Jest binary"
-        exit 1
-    fi
-    log_success "Jest binary found at: $jest_bin"
 
     cd $PROJECT_DIR
     log_info "Starting test execution in sequential mode (--runInBand)..."
     log_info "Test output follows:"
     echo "--------------------------------------------------------"
-    # yarn node --no-warnings --experimental-vm-modules $jest_bin --runInBand
     npx jest --runInBand
     test_result=$?
     echo "--------------------------------------------------------"
