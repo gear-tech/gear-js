316ffa559983c36b38289bcf2cb446e9523d9885
Nikita Yutanov <nikitayutanov@gmail.com>
2025-12-03 19:06:42 +0400

    fix history nav

--- idea/gear/frontend/src/features/program/hooks/use-program-tab.ts
+++ idea/gear/frontend/src/features/program/hooks/use-program-tab.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { PROGRAM_TAB_ID, PROGRAM_TAB_SEARCH_PARAM, PROGRAM_TABS } from '../consts';
@@ -9,35 +9,41 @@ const DEFAULT_TAB_ID = PROGRAM_TABS[0].id;
 const getSearchParamsTabId = (searchParams: URLSearchParams) => {
   const tabId = searchParams.get(PROGRAM_TAB_SEARCH_PARAM)?.toLowerCase();
 
-  if (!tabId || !PROGRAM_TABS.some(({ id }) => id === tabId)) return;
+  if (!tabId || !PROGRAM_TABS.some(({ id }) => id === tabId)) return DEFAULT_TAB_ID;
 
   return tabId as ProgramTabId;
 };
 
 function useProgramTab() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [tabId, setTabId] = useState(getSearchParamsTabId(searchParams) || DEFAULT_TAB_ID);
+  const searchParamsId = useMemo(() => getSearchParamsTabId(searchParams), [searchParams]);
 
-  const isMessages = tabId === PROGRAM_TAB_ID.MESSAGES;
-  const isEvents = tabId === PROGRAM_TAB_ID.EVENTS;
-  const isVouchers = tabId === PROGRAM_TAB_ID.VOUCHERS;
-  const isMetadata = tabId === PROGRAM_TAB_ID.METADATA;
+  const [id, setId] = useState(searchParamsId);
 
-  const handleChange = (id: ProgramTabId) => {
-    setTabId(id);
+  const isMessages = id === PROGRAM_TAB_ID.MESSAGES;
+  const isEvents = id === PROGRAM_TAB_ID.EVENTS;
+  const isVouchers = id === PROGRAM_TAB_ID.VOUCHERS;
+  const isMetadata = id === PROGRAM_TAB_ID.METADATA;
+
+  useEffect(() => {
+    setId(searchParamsId);
+  }, [searchParamsId]);
+
+  const handleChange = (_id: ProgramTabId) => {
+    setId(_id);
 
     setSearchParams((prevParams) => {
-      if (id === DEFAULT_TAB_ID) {
+      if (_id === DEFAULT_TAB_ID) {
         prevParams.delete(PROGRAM_TAB_SEARCH_PARAM);
       } else {
-        prevParams.set(PROGRAM_TAB_SEARCH_PARAM, id);
+        prevParams.set(PROGRAM_TAB_SEARCH_PARAM, _id);
       }
 
       return prevParams;
     });
   };
 
-  return { id: tabId, isMessages, isEvents, isVouchers, isMetadata, handleChange };
+  return { id, isMessages, isEvents, isVouchers, isMetadata, handleChange };
 }
 
 export { useProgramTab };
