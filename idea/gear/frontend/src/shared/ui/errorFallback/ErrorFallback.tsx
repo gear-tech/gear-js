508c7db3341843e5cef45e04ff42339b9be6e3d2
Nikita Yutanov <nikitayutanov@gmail.com>
2025-12-05 15:57:57 +0400

    fix(idea/frontend): `Router` remount loops forced by `ErrorBoundary` (#2225)

--- idea/gear/frontend/src/shared/ui/errorFallback/ErrorFallback.tsx
+++ idea/gear/frontend/src/shared/ui/errorFallback/ErrorFallback.tsx
@@ -1,17 +1,39 @@
 import { FallbackProps } from 'react-error-boundary';
+import { useLocation } from 'react-router-dom';
+
+import { useChangeEffect } from '@/hooks';
 
 import { BackButton } from '../backButton';
 import { Subheader } from '../subheader';
 
 import styles from './ErrorFallback.module.scss';
 
-const ErrorFallback = ({ error }: FallbackProps) => (
-  <>
-    <Subheader title="An unexpected error occured:" />
-    {/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- TODO(#1800): resolve eslint comments */}
-    <p className={styles.error}>{error.message}</p>
-    <BackButton />
-  </>
-);
+const getErrorMessage = (error: unknown) => {
+  if (error instanceof Error) return error.message;
+
+  try {
+    const json = JSON.stringify(error, null, 2);
+
+    return json.length > 500 ? json.slice(0, 500) + '...' : json;
+  } catch {
+    return "Can't retrieve error message";
+  }
+};
+
+const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+  const { pathname } = useLocation();
+
+  useChangeEffect(() => {
+    resetErrorBoundary();
+  }, [pathname]);
+
+  return (
+    <>
+      <Subheader title="An unexpected error occurred:" />
+      <p className={styles.error}>{getErrorMessage(error)}</p>
+      <BackButton />
+    </>
+  );
+};
 
 export { ErrorFallback };
