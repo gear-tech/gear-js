e1b9e3f021bbdc65d71e3192867a4ee3a77997e9
Vadim Tokar <vtokar@gear.foundation>
2025-12-09 14:01:12 +0300

    fix build

--- idea/vara-eth/frontend/src/pages/program/program.tsx
+++ idea/vara-eth/frontend/src/pages/program/program.tsx
@@ -27,14 +27,12 @@ const Program = () => {
   const { data: program, isLoading } = useGetProgramByIdQuery(programId || '');
   const { data: programState, refetch } = useReadContractState(programId);
 
-  const { value, decimals } = useWrappedVaraBalance(programId);
+  const { decimals } = useWrappedVaraBalance(programId);
   const isActive = true;
   const programName = 'Program name';
   const codeId = program?.codeId || '';
   const blockHash = program?.createdAtTx || '';
   const blockDateTime = program?.createdAtBlock ? `Block ${program.createdAtBlock}` : '';
-
-  const wvaraBalance = value !== undefined && decimals ? formatBalance(value, decimals) : null;
 
   const executableBalance =
     programState && decimals ? formatBalance(BigInt(programState.executableBalance), decimals) : null;
