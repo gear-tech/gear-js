31223ab3bb2762642baaa13405b1be97ab8dd9a6
Vadim Tokar <vtokar@gear.foundation>
2025-12-09 13:41:33 +0300

    fix(vara-eth-frontend): remove unused ui

--- idea/vara-eth/frontend/src/components/ui/balance/balance.tsx
+++ idea/vara-eth/frontend/src/components/ui/balance/balance.tsx
@@ -1,16 +1,13 @@
-import { clsx } from 'clsx';
-
 import styles from './balance.module.scss';
 
 type Props = {
   value: string | null;
   units: string;
-  withDivider?: boolean;
 };
 
-const Balance = ({ value, units, withDivider }: Props) => {
+const Balance = ({ value, units }: Props) => {
   return (
-    <span className={clsx(styles.wrapper, withDivider && styles.divider)}>
+    <span className={styles.wrapper}>
       <span className={styles.value}>{value === null ? '-' : value}</span> {units}
     </span>
   );
