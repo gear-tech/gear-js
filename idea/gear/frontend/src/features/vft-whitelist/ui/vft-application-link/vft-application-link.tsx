15c837387f08ce6b3737469cb9851ad2e71e9a8b
Nikita Yutanov <nikitayutanov@gmail.com>
2025-12-18 18:22:06 +0400

    add vft application link

---
+++ idea/gear/frontend/src/features/vft-whitelist/ui/vft-application-link/vft-application-link.tsx
@@ -0,0 +1,15 @@
+import styles from './vft-application-link.module.scss';
+
+function VftApplicationLink() {
+  return (
+    <a
+      href="https://forms.clickup.com/18331098/f/hfdeu-23333/1U9CQG5ZBWXQ2IFW19"
+      target="_blank"
+      rel="noreferrer"
+      className={styles.link}>
+      Want to list your program?
+    </a>
+  );
+}
+
+export { VftApplicationLink };
