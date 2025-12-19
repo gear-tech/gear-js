e46dff3806446fa32e41a09cad0d0b3a16d83f6d
Dmitri Osipov <dmith.osipov@gmail.com>
2025-12-19 14:44:42 +0200

    fix: update util fns

--- apis/vara-eth/src/provider/util.ts
+++ apis/vara-eth/src/provider/util.ts
@@ -1,23 +1,15 @@
 import type { HttpUrl, IVaraEthProvider, IVaraEthValidatorPoolProvider, WsUrl } from '../types/index.js';
 
 export function isWsUrl(url: string): url is WsUrl {
-  if (url.startsWith('wss://')) return true;
-  if (url.startsWith('ws://')) return true;
-  return false;
+  return url.startsWith('wss://') || url.startsWith('ws://');
 }
 
 export function isHttpUrl(url: string): url is HttpUrl {
-  if (url.startsWith('https://')) return true;
-  if (url.startsWith('http://')) return true;
-  return false;
+  return url.startsWith('https://') || url.startsWith('http://');
 }
 
 export function isPoolProvider(
   provider: IVaraEthProvider | IVaraEthValidatorPoolProvider,
 ): provider is IVaraEthValidatorPoolProvider {
-  if ('isPool' in provider && provider.isPool === true) {
-    return true;
-  }
-
-  return false;
+  return 'isPool' in provider && provider.isPool === true;
 }
