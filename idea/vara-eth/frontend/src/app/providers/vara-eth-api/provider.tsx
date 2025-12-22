cec5c255cb5d84da97b3cc0253e9ae3b65168160
Vadim Tokar <vtokar@gear.foundation>
2025-12-22 06:22:45 +0300

    feat(vara-eth-idea/frontend): improve program page

--- idea/vara-eth/frontend/src/app/providers/vara-eth-api/provider.tsx
+++ idea/vara-eth/frontend/src/app/providers/vara-eth-api/provider.tsx
@@ -1,21 +1,24 @@
 import { VaraEthApi, WsVaraEthProvider } from '@vara-eth/api';
 import { PropsWithChildren, useEffect, useMemo, useState } from 'react';
 
+import { useEthereumClient } from '@/app/api';
 import { VARA_ETH_NODE_ADDRESS } from '@/shared/config';
 
 import { ApiContext } from './context';
 
 const VaraEthApiProvider = ({ children }: PropsWithChildren) => {
   const [api, setApi] = useState<VaraEthApi>();
+  const { data: ethereumClient } = useEthereumClient();
 
   useEffect(() => {
-    const instance = new VaraEthApi(new WsVaraEthProvider(VARA_ETH_NODE_ADDRESS));
+    if (!ethereumClient) return;
+    const instance = new VaraEthApi(new WsVaraEthProvider(VARA_ETH_NODE_ADDRESS), ethereumClient);
     setApi(instance);
 
     return () => {
       void instance.provider.disconnect();
     };
-  }, []);
+  }, [ethereumClient]);
 
   const value = useMemo(() => (api ? { api, isApiReady: true as const } : { api, isApiReady: false as const }), [api]);
 
