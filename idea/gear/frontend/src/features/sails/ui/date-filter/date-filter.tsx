86a9058e3ced78f7c6bf2e186bd67775d74f2009
Nikita Yutanov <nikitayutanov@gmail.com>
2025-12-25 21:08:16 +0400

    add calendar

--- idea/gear/frontend/src/features/sails/ui/date-filter/date-filter.tsx
+++ idea/gear/frontend/src/features/sails/ui/date-filter/date-filter.tsx
@@ -1,9 +1,26 @@
 import { Popover as BasePopover } from '@base-ui/react';
 import { Input } from '@gear-js/ui';
+import { DayFlag, DayPicker, SelectionState, UI } from 'react-day-picker';
 
 import CalendarSVG from './calendar.svg?react';
 import styles from './date-filter.module.scss';
-import 'react-day-picker/style.css';
+
+const CALENDAR_CLASS_NAMES = {
+  [UI.Months]: styles.calendar,
+  [UI.MonthCaption]: styles.heading,
+  [UI.PreviousMonthButton]: styles.buttonPrev,
+  [UI.NextMonthButton]: styles.buttonNext,
+  [UI.Chevron]: styles.buttonIcon,
+  [UI.Weekdays]: styles.weekdays,
+  [UI.MonthGrid]: styles.month,
+  [UI.Day]: styles.day,
+  [UI.DayButton]: styles.dayButton,
+  [SelectionState.range_start]: styles.dayStart,
+  [SelectionState.range_middle]: styles.dayMiddle,
+  [SelectionState.range_end]: styles.dayEnd,
+  [SelectionState.selected]: styles.daySelected,
+  [DayFlag.outside]: styles.dayOutside,
+};
 
 function DateFilter() {
   return (
@@ -12,14 +29,16 @@ function DateFilter() {
         nativeButton={false}
         render={
           <div>
-            <Input icon={CalendarSVG} className={styles.input} readOnly />
+            <Input placeholder="Date" icon={CalendarSVG} className={styles.input} readOnly />
           </div>
         }
       />
 
       <BasePopover.Portal>
-        <BasePopover.Positioner sideOffset={8} className={styles.positioner}>
-          <BasePopover.Popup className={styles.popup}></BasePopover.Popup>
+        <BasePopover.Positioner sideOffset={8} collisionAvoidance={{ side: 'none' }} className={styles.positioner}>
+          <BasePopover.Popup className={styles.popup}>
+            <DayPicker mode="range" navLayout="around" classNames={CALENDAR_CLASS_NAMES} showOutsideDays />
+          </BasePopover.Popup>
         </BasePopover.Positioner>
       </BasePopover.Portal>
     </BasePopover.Root>
