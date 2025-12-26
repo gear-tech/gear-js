c2872a522174283c9b36eec2c58a6815b4b521b1
Nikita Yutanov <nikitayutanov@gmail.com>
2025-12-26 20:32:36 +0400

    add program events date filter

--- idea/gear/frontend/src/features/sails/ui/program-events/program-events.tsx
+++ idea/gear/frontend/src/features/sails/ui/program-events/program-events.tsx
@@ -2,7 +2,7 @@ import { HexString } from '@gear-js/api';
 import { parseAsString } from 'nuqs';
 import { Sails } from 'sails-js';
 
-import { Filters } from '@/features/filters';
+import { DateRangeInput, Filters } from '@/features/filters';
 import { useSearchParamsStates } from '@/hooks';
 import CardPlaceholderSVG from '@/shared/assets/images/placeholders/card.svg?react';
 import { List, ProgramTabLayout, Skeleton } from '@/shared/ui';
@@ -19,22 +19,33 @@ type Props = {
 
 const FILTER_NAME = {
   SAILS: 'sails',
+  DATE_FROM: 'from',
+  DATE_TO: 'to',
 } as const;
 
 const DEFAULT_VALUE = {
   SAILS: '' as string,
+  DATE_FROM: '' as string,
+  DATE_TO: '' as string,
 } as const;
 
 const DEFAULT_FILTER_VALUES = {
   [FILTER_NAME.SAILS]: DEFAULT_VALUE.SAILS,
+  [FILTER_NAME.DATE_FROM]: DEFAULT_VALUE.DATE_FROM,
+  [FILTER_NAME.DATE_TO]: DEFAULT_VALUE.DATE_TO,
 } as const;
 
 function useFilters(sails: Sails | undefined) {
   const [filters, setFilters] = useSearchParamsStates({
     [FILTER_NAME.SAILS]: parseAsString.withDefault(DEFAULT_VALUE.SAILS),
+
+    // TODO: validate date strings
+    [FILTER_NAME.DATE_FROM]: parseAsString.withDefault(DEFAULT_VALUE.DATE_FROM),
+    [FILTER_NAME.DATE_TO]: parseAsString.withDefault(DEFAULT_VALUE.DATE_TO),
   });
 
   const validFilters = {
+    ...filters,
     [FILTER_NAME.SAILS]: getValidSailsFilterValue(sails, 'events', filters[FILTER_NAME.SAILS], DEFAULT_VALUE.SAILS),
   };
 
@@ -43,9 +54,11 @@ function useFilters(sails: Sails | undefined) {
 
 function ProgramEvents({ programId, sails }: Props) {
   const [filterValues, setFilterValues] = useFilters(sails);
+
+  const { from, to } = filterValues;
   const { serviceName, functionName } = getParsedSailsFilterValue(filterValues[FILTER_NAME.SAILS]);
 
-  const events = useEvents({ source: programId, service: serviceName, name: functionName });
+  const events = useEvents({ source: programId, service: serviceName, name: functionName, from, to });
 
   const renderList = () => (
     <List
@@ -65,6 +78,8 @@ function ProgramEvents({ programId, sails }: Props) {
 
     return (
       <Filters initialValues={DEFAULT_FILTER_VALUES} values={filterValues} onSubmit={setFilterValues}>
+        <DateRangeInput fromName={FILTER_NAME.DATE_FROM} toName={FILTER_NAME.DATE_TO} onSubmit={setFilterValues} />
+
         <SailsFilter
           label="Sails Events"
           services={sails.services}
