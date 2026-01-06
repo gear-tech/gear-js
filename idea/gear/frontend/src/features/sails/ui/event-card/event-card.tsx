a12a261f4bbcba837196ce4b08173c4862b2ab16
Nikita Yutanov <nikitayutanov@gmail.com>
2026-01-07 00:16:03 +0400

    handle program event decoding errors

--- idea/gear/frontend/src/features/sails/ui/event-card/event-card.tsx
+++ idea/gear/frontend/src/features/sails/ui/event-card/event-card.tsx
@@ -9,6 +9,7 @@ import { useIsVftProgram, VftEventPayload } from '@/features/vft-whitelist';
 import ArrowSVG from '@/shared/assets/images/actions/arrowRight.svg?react';
 import CodeSVG from '@/shared/assets/images/actions/code.svg?react';
 import { absoluteRoutes } from '@/shared/config';
+import { getErrorMessage } from '@/shared/helpers';
 import { PreformattedBlock, UILink } from '@/shared/ui';
 import { TimestampBlock } from '@/shared/ui/timestampBlock';
 
@@ -24,16 +25,32 @@ type Props = {
 };
 
 function EventCard({ programId, event, sails }: Props) {
-  const { service, name, blockNumber, blockHash, payload } = event;
+  const { service: serviceName, name, blockNumber, blockHash, payload } = event;
 
   const { data: isVft } = useIsVftProgram(programId);
+  const isVftPayload = isVft && serviceName?.toLowerCase() === 'vft' && name;
+
   const [isOpen, setIsOpen] = useState(false);
 
-  const getDecodedPayload = () => {
-    if (!sails || !service || !name || !payload) return payload;
+  const decodePayload = () => {
+    if (!sails || !serviceName || !name || !payload) return { payload };
 
-    return sails.services[service].events[name].decode(payload) as AnyJson;
+    const service = sails.services[serviceName];
+
+    if (!service) return { payload, error: `Sails Service '${serviceName}' is not found` };
+
+    const serviceEvent = service.events[name];
+
+    if (!serviceEvent) return { payload, error: `Sails Event '${name}' is not found` };
+
+    try {
+      return { payload: serviceEvent.decode(payload) as AnyJson };
+    } catch (error) {
+      return { payload, error: getErrorMessage(error) };
+    }
   };
+
+  const decoded = isOpen ? decodePayload() : undefined;
 
   return (
     <div className={styles.card}>
@@ -42,7 +59,7 @@ function EventCard({ programId, event, sails }: Props) {
           type="button"
           onClick={() => setIsOpen((prevValue) => !prevValue)}
           className={clsx(styles.button, isOpen && styles.open)}>
-          <span>{service && name ? `${service}.${name}` : service || name}</span>
+          <span>{serviceName && name ? `${serviceName}.${name}` : serviceName || name || 'Unknown Event'}</span>
 
           <ArrowSVG />
         </button>
@@ -63,12 +80,17 @@ function EventCard({ programId, event, sails }: Props) {
         </div>
       </header>
 
-      {isOpen &&
-        (isVft && service?.toLowerCase() === 'vft' && name ? (
-          <VftEventPayload name={name.toLowerCase()} programId={programId} decoded={getDecodedPayload()} />
-        ) : (
-          <PreformattedBlock text={getDecodedPayload()} />
-        ))}
+      {decoded && (
+        <div>
+          {isVftPayload && !decoded.error ? (
+            <VftEventPayload name={name.toLowerCase()} programId={programId} decoded={decoded.payload} />
+          ) : (
+            <PreformattedBlock text={decoded.payload} />
+          )}
+
+          {decoded.error && <p className={styles.error}>Can&apos;t decode payload: {decoded.error}</p>}
+        </div>
+      )}
     </div>
   );
 }
