f5a70d90f3e3ff569dde7b2b69481ae43f4b0a0d
Nikita Yutanov <nikitayutanov@gmail.com>
2026-01-21 19:02:00 +0400

    init package

--- idea/vara-eth/frontend/src/main.tsx
+++ idea/vara-eth/frontend/src/main.tsx
@@ -1,7 +1,10 @@
+import { someFunctionToExport } from '@gear-js/sails-payload-form';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './app/app';
+
+console.log(someFunctionToExport());
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- idea/vara-eth/frontend/vite.config.ts
+++ idea/vara-eth/frontend/vite.config.ts
@@ -1,6 +1,11 @@
 import { viteConfigs } from '@gear-js/frontend-configs';
+import { resolve } from 'path';
 import { mergeConfig } from 'vite';
 
 export default mergeConfig(viteConfigs.app, {
+  resolve: {
+    alias: { '@gear-js/sails-payload-form': resolve(__dirname, '../../../utils/sails-payload-form/src/index.ts') },
+  },
+
   css: { preprocessorOptions: { scss: { additionalData: '@use "@/shared/utils/styles/_breakpoints" as *;' } } },
 });
