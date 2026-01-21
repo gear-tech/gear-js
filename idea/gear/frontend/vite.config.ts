dfbba7de36152ee142298a0b68576782f6f291b7
Nikita Yutanov <nikitayutanov@gmail.com>
2026-01-21 23:03:44 +0400

    cleanup configs

--- idea/gear/frontend/vite.config.ts
+++ idea/gear/frontend/vite.config.ts
@@ -1,9 +1,3 @@
 import { viteConfigs } from '@gear-js/frontend-configs';
-import { resolve } from 'path';
-import { mergeConfig } from 'vite';
 
-export default mergeConfig(viteConfigs.app, {
-  resolve: {
-    alias: { '@gear-js/sails-payload-form': resolve(__dirname, '../../../utils/sails-payload-form/src/index.ts') },
-  },
-});
+export default viteConfigs.app;
