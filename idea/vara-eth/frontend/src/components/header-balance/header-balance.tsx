584d259f286b9445ba90b9800ff79174952ac9d8
Nikita Yutanov <nikitayutanov@gmail.com>
2026-02-12 12:41:59 +0400

    add header balance skeletons

--- idea/vara-eth/frontend/src/components/header-balance/header-balance.tsx
+++ idea/vara-eth/frontend/src/components/header-balance/header-balance.tsx
@@ -3,7 +3,9 @@ import { useAccount, useBalance } from 'wagmi';
 import { useWrappedVaraBalance } from '@/app/api';
 import EthSVG from '@/assets/icons/eth-coin.svg?react';
 import VaraSVG from '@/assets/icons/vara-coin.svg?react';
-import { formatBalance } from '@/shared/utils';
+import { formatBalance, isUndefined } from '@/shared/utils';
+
+import { Skeleton } from '../ui';
 
 import styles from './header-balance.module.scss';
 
@@ -15,20 +17,22 @@ const HeaderBalance = () => {
     address: ethAccount.address,
   });
 
-  const splittedWVara = !isPending ? formatBalance(value, decimals).split('.') : null;
-
-  const splittedEth = ethBalance ? formatBalance(ethBalance.value, ethBalance.decimals).split('.') : null;
+  const splittedWVara = !isPending ? formatBalance(value, decimals).split('.') : undefined;
+  const splittedEth = ethBalance ? formatBalance(ethBalance.value, ethBalance.decimals).split('.') : undefined;
 
   return (
     <div className={styles.container}>
       <div className={styles.item}>
         <VaraSVG />
+
         <div>
           <div className={styles.name}>WVARA</div>
-          {splittedWVara !== null && (
+
+          {isUndefined(splittedWVara) ? (
+            <Skeleton width="4rem" className={styles.value} />
+          ) : (
             <div className={styles.value}>
               {splittedWVara[0]}
-
               {splittedWVara[1] && <span className={styles.decimal}>.{splittedWVara[1]}</span>}
             </div>
           )}
@@ -37,12 +41,16 @@ const HeaderBalance = () => {
 
       <div className={styles.item}>
         <EthSVG />
+
         <div>
           <div className={styles.name}>ETH</div>
-          {splittedEth && (
+
+          {isUndefined(splittedEth) ? (
+            <Skeleton width="4rem" className={styles.value} />
+          ) : (
             <div className={styles.value}>
               {splittedEth[0]}
-              <span className={styles.decimal}>.{splittedEth[1]}</span>
+              {splittedEth[1] && <span className={styles.decimal}>.{splittedEth[1]}</span>}
             </div>
           )}
         </div>
