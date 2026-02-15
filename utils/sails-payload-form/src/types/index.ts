7deef1adbd6ea1f53371b2504b208fcc8a2ba14e
Nikita Yutanov <nikitayutanov@gmail.com>
2026-02-15 07:09:30 +0400

    fix(sails-payload-form): remove `@gear-js/api` dependency (#2344)

--- utils/sails-payload-form/src/types/index.ts
+++ utils/sails-payload-form/src/types/index.ts
@@ -1,12 +1,12 @@
-import { HexString } from '@gear-js/api';
-
 import { RESULT } from '../consts';
 
 import { FieldProps } from './field-props';
 import { ISailsFuncArg } from './sails';
 
+type HexString = `0x${string}`;
+
 type Result = (typeof RESULT)[keyof typeof RESULT];
 type PayloadValue = string | boolean | null | Array<PayloadValue> | { [key: string]: PayloadValue };
 type FormattedPayloadValue = { encoded: HexString; decoded: Record<string, unknown> };
 
-export type { FieldProps, ISailsFuncArg, Result, PayloadValue, FormattedPayloadValue };
+export type { HexString, FieldProps, ISailsFuncArg, Result, PayloadValue, FormattedPayloadValue };
