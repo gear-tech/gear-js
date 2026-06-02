e6e4bab38c1efa2100d2db7de7df5f931d05e622
Vadim Tokar <52737608+vraja-nayaka@users.noreply.github.com>
2026-06-02 14:30:35 +0300

    feat(vara-eth/frontend): add chainId deps for api (#2511)

--- idea/vara-eth/frontend/src/app/api/use-wrapped-vara-balance.ts
+++ idea/vara-eth/frontend/src/app/api/use-wrapped-vara-balance.ts
@@ -1,8 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
+import { useAtomValue } from 'jotai';
 import type { Hex } from 'viem';
 import { useConnection } from 'wagmi';
 
 import { useApi } from '@/app/api';
+import { nodeAtom } from '@/app/store';
 
 type WrappedVaraBalance =
   | {
@@ -19,24 +21,25 @@ type WrappedVaraBalance =
 const useWrappedVaraBalance = (address?: Hex) => {
   const ethAccount = useConnection();
   const { data: api } = useApi();
+  const { ethChainId } = useAtomValue(nodeAtom);
 
   const targetAddress = address || ethAccount.address;
 
   const value = useQuery({
-    queryKey: ['wrappedVaraBalance', targetAddress],
+    queryKey: ['wrappedVaraBalance', ethChainId, targetAddress],
     queryFn: () => {
       if (!targetAddress) throw new Error('Address is required');
       return api?.eth.wvara.balanceOf(targetAddress);
     },
     select: (response) => (response !== undefined ? BigInt(response) : undefined),
-    enabled: Boolean(api && targetAddress),
+    enabled: Boolean(api && targetAddress && ethChainId),
   });
 
   const decimals = useQuery({
-    queryKey: ['wrappedVaraDecimals'],
+    queryKey: ['wrappedVaraDecimals', ethChainId],
     queryFn: () => api?.eth.wvara.decimals(),
     select: (response) => Number(response),
-    enabled: Boolean(api),
+    enabled: Boolean(api && ethChainId),
   });
 
   return {
